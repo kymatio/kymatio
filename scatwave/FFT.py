9646eaf9256575ba455fb93d2b616ca5bcb721a8
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-04-19 17:29:35 +0200

    add differentiable scattering

---
+++ scatwave/FFT.py
@@ -0,0 +1,109 @@
+import torch
+from torch.autograd import Function
+from collections import defaultdict, namedtuple
+from skcuda import cublas, cufft
+import numpy as np
+
+
+class FFTcache(object):
+
+    def __init__(self):
+        self.fft_cache=defaultdict(lambda: None)
+
+    def buildCache(self, input, type):
+        k = input.ndimension() - 3
+        n = np.asarray([input.size(k), input.size(k+1)], np.int32)
+        batch = input.nelement() // (2*input.size(k) * input.size(k + 1))
+        idist = input.size(k) * input.size(k + 1)
+        istride = 1
+        ostride = istride
+        odist = idist
+        rank = 2
+        print rank, n.ctypes.data, n.ctypes.data, istride, idist, n.ctypes.data, ostride, odist, type, batch
+        plan = cufft.cufftPlanMany(rank, n.ctypes.data, n.ctypes.data, istride, idist, n.ctypes.data, ostride, odist, type, batch)
+        self.fft_cache[(input.size(),type,input.get_device())] = plan
+
+    def __del__(self):
+        for keys in self.fft_cache:
+            try:
+                cufft.cufftDestroy(self.fft_cache[keys])
+            except:
+                pass
+
+    def c2c(self, input, inverse=False):
+        assert input.is_contiguous()
+        output = input.new(input.size())
+        flag = cufft.CUFFT_INVERSE if inverse else cufft.CUFFT_FORWARD
+        ffttype = cufft.CUFFT_C2C if isinstance(input, torch.cuda.FloatTensor) else cufft.CUFFT_Z2Z
+        if (self.fft_cache[(input.size(), ffttype, input.get_device())] is None):
+            self.buildCache(input, ffttype)
+        cufft.cufftExecC2C(self.fft_cache[(input.size(), ffttype, input.get_device())],
+                           input.data_ptr(), output.data_ptr(), flag)
+        return output
+
+    def c2r(self, input):
+        output = input.new(input.size()[:-1])
+        if(self.fft_cache[(input.size(), cufft.CUFFT_C2R, input.get_device())] is None):
+            self.buildCache(input, cufft.CUFFT_C2R)
+        cufft.cufftExecC2R(self.fft_cache[(input.size(), cufft.CUFFT_C2R, input.get_device())],
+                           input.data_ptr(), output.data_ptr())
+        return output
+
+    def r2c(self, input):
+        output = input.new(input.size() + torch.Size((2,)))
+        if(self.fft_cache[(input.size(), cufft.CUFFT_R2C, input.get_device())] is None):
+            self.buildCache(input, cufft.CUFFT_R2C)
+        cufft.cufftExecR2C(self.fft_cache[(input.size(), cufft.CUFFT_R2C, input.get_device())],
+                           input.data_ptr(), output.data_ptr())
+        return output
+
+
+fft = FFTcache()
+
+class FFT_C2C(torch.autograd.Function):
+
+    def forward(self, input):
+        return fft.c2c(input, inverse=False)
+
+    def backward(self, grad_output):
+        return fft.c2c(grad_output, inverse=True)
+
+
+class FFT_iC2C(torch.autograd.Function):
+
+    def forward(self, input):
+        return fft.c2c(input, inverse=True)
+
+    def backward(self, grad_output):
+        return fft.c2c(grad_output, inverse=False)
+
+class FFT_C2R(torch.autograd.Function):
+
+    def forward(self, input):
+        return fft.c2r(input)
+
+    def backward(self, grad_output):
+        return fft.r2c(grad_output)
+
+
+class FFT_R2C(torch.autograd.Function):
+
+    def forward(self, input):
+        return fft.r2c(input)
+
+    def backward(self, grad_output):
+        return fft.c2r(grad_output)
+
+
+def fft_c2c(input):
+    return FFT_C2C()(input)
+
+def ifft_c2c(input):
+    return FFT_iC2C()(input)
+
+def ifft_c2r(input):
+    # return FFT_C2R()(input)
+    return FFT_C2C()(input).select(input.dim()-1, 0)
+
+def fft_r2c(input):
+    return FFT_R2C()(input)
---
+++ scatwave/differentiable.py
@@ -0,0 +1,131 @@
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+from scatwave.FFT import fft_c2c, ifft_c2r, ifft_c2c
+from scatwave.filters_bank import filters_bank
+from scatwave.scattering import Scattering
+
+
+def prepare_padding_size(M, N, J):
+    M_padded = ((M + 2 ** (J))//2**J+1)*2**J
+    N_padded = ((N + 2 ** (J))//2**J+1)*2**J
+
+    return M_padded, N_padded
+
+
+def cast(Psi, Phi, _type):
+    for key, item in enumerate(Psi):
+        for key2, item2 in Psi[key].iteritems():
+            if torch.is_tensor(item2):
+                Psi[key][key2] = Variable(item2.type(_type))
+    Phi = [Variable(v.type(_type)) for v in Phi]
+    return Psi, Phi
+
+
+def pad(input, J):
+    out_ = F.pad(input, (2**J,) * 4, mode='reflect').unsqueeze(input.dim())
+    return torch.cat([out_, Variable(input.data.new(out_.size()).zero_())], 4)
+
+
+def unpad(input):
+    return input[..., 1:-1, 1:-1]
+
+
+def cdgmm(A, B):
+    C = Variable(A.data.new(A.size()))
+
+    A_r = A[..., 0].contiguous().view(-1, A.size(-2)*A.size(-3))
+    A_i = A[..., 1].contiguous().view(-1, A.size(-2)*A.size(-3))
+
+    B_r = B[...,0].contiguous().view(B.size(-2)*B.size(-3)).unsqueeze(0).expand_as(A_i)
+    B_i = B[..., 1].contiguous().view(B.size(-2)*B.size(-3)).unsqueeze(0).expand_as(A_r)
+
+    C[..., 0] = A_r * B_r - A_i * B_i
+    C[..., 1] = A_r * B_i + A_i * B_r
+    return C
+
+
+def periodize(input, k):
+    return input.view(input.size(0), input.size(1),
+                      k, input.size(2) // k,
+                      k, input.size(3) // k,
+                      2).mean(4).squeeze(4).mean(2).squeeze(2)
+
+
+def modulus(input):
+    # return input.norm(2, input.dim()-1).expand_as(input).contiguous()
+    norm = input.norm(2, input.dim() - 1)
+    return torch.cat([norm, Variable(norm.data.new(norm.size()).zero_())], input.dim() - 1)
+
+
+def scattering(input, psi, phi, J):
+    M, N = input.size(-2), input.size(-1)
+    M_padded, N_padded = prepare_padding_size(M, N, J)
+    S = Variable(input.data.new(input.size(0),
+                                input.size(1),
+                                1 + 8*J + 8*8*J*(J - 1) // 2,
+                                M_padded//(2**J)-2,
+                                N_padded//(2**J)-2))
+    U_r = pad(input, J)
+    U_0_c = fft_c2c(U_r)
+
+    # First low pass filter
+    U_1_c = periodize(cdgmm(U_0_c, phi[0]), k=2**J)
+
+    U_J_r = ifft_c2r(U_1_c)
+
+    n = 0
+    S[..., n, :, :] = unpad(U_J_r)
+    n = n + 1
+
+    for n1 in range(len(psi)):
+        j1 = psi[n1]['j']
+        U_1_c = cdgmm(U_0_c, psi[n1][0])
+        if(j1 > 0):
+            U_1_c = periodize(U_1_c, k=2 ** j1)
+        U_1_c = fft_c2c(modulus(ifft_c2c(U_1_c)))
+
+        # Second low pass filter
+        U_2_c = periodize(cdgmm(U_1_c, phi[j1]), k=2**(J-j1))
+        U_J_r = ifft_c2r(U_2_c)
+        S[..., n, :, :] = unpad(U_J_r)
+        n = n + 1
+
+        for n2 in range(len(psi)):
+            j2 = psi[n2]['j']
+            if(j1 < j2):
+                U_2_c = periodize(cdgmm(U_1_c, psi[n2][j1]), k=2 ** (j2-j1))
+                U_2_c = fft_c2c(modulus(ifft_c2c(U_2_c)))
+
+                # Third low pass filter
+                U_2_c = periodize(cdgmm(U_2_c, phi[j2]), k=2 ** (J-j2))
+                U_J_r = ifft_c2r(U_2_c)
+
+                S[..., n, :, :] = unpad(U_J_r)
+                n = n + 1
+    return S
+
+
+if __name__ == '__main__':
+    M, N = 32, 32
+    J = 2
+
+    M_padded, N_padded = prepare_padding_size(M, N, J)
+
+    filters = filters_bank(M_padded, N_padded, J)
+
+    Psi = filters['psi']
+    Phi = [filters['phi'][j] for j in range(J)]
+
+    Psi, Phi = cast(Psi, Phi, torch.cuda.FloatTensor)
+
+    input = Variable(torch.randn(1, 3, 32, 32).cuda())
+    input.requires_grad = True
+
+    S = scattering(input, Psi, Phi, J)
+    S.sum().backward()
+
+    scat = Scattering(M, N, J).cuda()
+    S_scat = scat(input.data)
+
+    print scat(input.data) - S.data
