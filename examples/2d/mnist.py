35e8f3b4555826497da9a166366d163737d782d5
eugenium <eugenium@gmail.com>
2018-10-06 22:55:49 -0400

    ENH add good cifar and mnist examples
    first version .. need to improve slightly accuracy
    
    fixed
    
    1st order
    
    cifar
    
    updating based on mikes comments
    
    updating directory storing path
    
    more comments resolved
    
    deleted something
    
    minor
    
    FIX order2 setting was in build, moved to init
    
    FIX resnet example minor stuff
    
    COSM add 88% acc for cifar experiments

--- examples/2d/mnist.py
+++ examples/2d/mnist.py
@@ -1,111 +1,167 @@
-import math
-import torch
+"""
+Self-contained classification example on MNIST
+
+Based on pytorch example for MNIST
+"""
+
+
+import torch.nn as nn
 import torch.optim
-import torchnet as tnt
-from torchvision.datasets.mnist import MNIST
-from torchnet.engine import Engine
-from torch.autograd import Variable
+from torchvision import datasets, transforms
 import torch.nn.functional as F
-from scattering import Scattering2D as Scattering
+from scattering import Scattering2D
+import scattering.datasets as scattering_datasets
+import scattering
+import torch
+import argparse
+import math
 
-from scattering.datasets import get_dataset_dir
+class View(nn.Module):
+    def __init__(self, *args):
+        super(View, self).__init__()
+        self.shape = args
 
+    def forward(self, x):
+        return x.view(-1,*self.shape)
 
-def get_iterator(mode):
-    mnist_dir = get_dataset_dir("mnist", create=True)
-    ds = MNIST(root=mnist_dir, download=True, train=mode)
-    data = getattr(ds, 'train_data' if mode else 'test_data')
-    labels = getattr(ds, 'train_labels' if mode else 'test_labels')
-    tds = tnt.dataset.TensorDataset([data, labels])
-    return tds.parallel(batch_size=128, num_workers=4, shuffle=mode)
+def train(model, device, train_loader, optimizer, epoch, scat):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(scat(data))
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 50 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
 
+def test(model, device, test_loader, scat):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(scat(data))
+            test_loss += F.cross_entropy(output, target, size_average=False).item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-def conv_init(ni, no, k):
-    return torch.Tensor(no, ni, k, k).normal_(0, 2/math.sqrt(ni*k*k))
-
-
-def linear_init(ni, no):
-    return torch.Tensor(no, ni).normal_(0, 2/math.sqrt(ni))
-
-
-def f(o, params, stats, mode):
-    o = F.batch_norm(o, running_mean=stats['bn.running_mean'],
-                     running_var=stats['bn.running_var'],
-                     weight=params['bn.weight'],
-                     bias=params['bn.bias'], training=mode)
-    o = F.conv2d(o, params['conv1.weight'], params['conv1.bias'])
-    o = F.relu(o)
-    o = o.view(o.size(0), -1)
-    o = F.linear(o, params['linear2.weight'], params['linear2.bias'])
-    o = F.relu(o)
-    o = F.linear(o, params['linear3.weight'], params['linear3.bias'])
-    return o
-
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
 def main():
     """Train a simple Hybrid Scattering + CNN model on MNIST.
 
-    Scattering features are normalized by batch normalization.
-    The model achieves 99.6% testing accuracy after 10 epochs.
+        Three models are demoed:
+        'linear' - scattering + linear model
+        'mlp' - scattering + MLP
+        'cnn' - scattering + CNN
+
+        scattering 1st order can also be set by the mode
+        Scattering features are normalized by batch normalization.
+
+        scatter + linear achieves 99.15% in 15 epochs
+        scatter + cnn achieves 99.3% in 15 epochs
+
     """
-    meter_loss = tnt.meter.AverageValueMeter()
-    classerr = tnt.meter.ClassErrorMeter(accuracy=True)
+    parser = argparse.ArgumentParser(description='MNIST scattering  + hybrid examples')
+    parser.add_argument('--mode', type=int, default=2,help='scattering 1st or 2nd order')
+    parser.add_argument('--classifier', type=str, default='linear',help='classifier model')
+    args = parser.parse_args()
+    assert(args.classifier in ['linear','mlp','cnn'])
 
-    scat = Scattering(M=28, N=28, J=2).cuda()
-    K = 81
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
 
-    params = {
-        'conv1.weight':     conv_init(K, 64, 1),
-        'conv1.bias':       torch.zeros(64),
-        'bn.weight':        torch.Tensor(K).uniform_(),
-        'bn.bias':          torch.zeros(K),
-        'linear2.weight':   linear_init(64*7*7, 512),
-        'linear2.bias':     torch.zeros(512),
-        'linear3.weight':   linear_init(512, 10),
-        'linear3.bias':     torch.zeros(10),
-    }
+    if args.mode == 1:
+        scattering = Scattering2D(M=28, N=28, J=2,order2=False)
+        K = 17
+    else:
+        scattering = Scattering2D(M=28, N=28, J=2)
+        K = 81
+    if use_cuda:
+        scattering = scattering.cuda()
 
-    stats = {'bn.running_mean': torch.zeros(K).cuda(),
-             'bn.running_var': torch.ones(K).cuda()}
 
-    for k, v in params.items():
-        params[k] = Variable(v.cuda(), requires_grad=True)
 
-    def h(sample):
-        x = scat(sample[0].float().cuda().unsqueeze(1) / 255.0).squeeze(1)
-        inputs = Variable(x)
-        targets = Variable(torch.LongTensor(sample[1]).cuda())
-        o = f(inputs, params, stats, sample[2])
-        return F.cross_entropy(o, targets), o
 
-    def on_sample(state):
-        state['sample'].append(state['train'])
+    if args.classifier == 'cnn':
+        model = nn.Sequential(
+            View(K, 7, 7),
+            nn.BatchNorm2d(K),
+            nn.Conv2d(K, 64, 3,padding=1), nn.ReLU(),
+            View(64*7*7),
+            nn.Linear(64 * 7 * 7, 512), nn.ReLU(),
+            nn.Linear(512, 10)
+        ).to(device)
 
-    def on_forward(state):
-        classerr.add(state['output'].data,
-                     torch.LongTensor(state['sample'][1]))
-        meter_loss.add(state['loss'].data[0])
+    elif args.classifier == 'mlp':
+        model = nn.Sequential(
+            View(K, 7, 7),
+            nn.BatchNorm2d(K),
+            View(K*7*7),
+            nn.Linear(K*7*7, 512), nn.ReLU(),
+            nn.Linear(512, 512), nn.ReLU(),
+            nn.Linear(512, 10)
+        )
 
-    def on_end_epoch(state):
-        print('Training accuracy:', classerr.value())
+    elif args.classifier == 'linear':
+        model = nn.Sequential(
+            View(K, 7, 7),
+            nn.BatchNorm2d(K),
+            View(K * 7 * 7),
+            nn.Linear(K * 7 * 7, 10)
+        )
+    else:
+        raise ValueError('Classifier should be cnn/mlp/linear')
 
-    def on_end(state):
-        print('Training' if state['train'] else 'Testing', 'accuracy')
-        print(classerr.value())
+    model.to(device)
 
-    optimizer = torch.optim.SGD(params.values(), lr=0.01, momentum=0.9,
+    #initialize
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
+            m.weight.data.normal_(0, 2./math.sqrt(n))
+            m.bias.data.zero_()
+        if isinstance(m, nn.Linear):
+            m.weight.data.normal_(0, 2./math.sqrt(m.in_features))
+            m.bias.data.zero_()
+
+    # DataLoaders
+    if use_cuda:
+        num_workers = 4
+        pin_memory = True
+    else:
+        num_workers = None
+        pin_memory = False
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(scattering_datasets.get_dataset_dir('MNIST'), train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=128, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(scattering_datasets.get_dataset_dir('MNIST'), train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=128, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+
+    # Optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9,
                                 weight_decay=0.0005)
 
-    engine = Engine()
-    engine.hooks['on_sample'] = on_sample
-    engine.hooks['on_forward'] = on_forward
-    engine.hooks['on_end_epoch'] = on_end_epoch
-    engine.hooks['on_end'] = on_end
-    print('Training:')
-
-    engine.train(h, get_iterator(True), 10, optimizer)
-    print('Testing:')
-    engine.test(h, get_iterator(False))
+    for epoch in range(1, 16):
+        train( model, device, train_loader, optimizer, epoch, scattering)
+        test(model, device, test_loader, scattering)
 
 
 if __name__ == '__main__':
