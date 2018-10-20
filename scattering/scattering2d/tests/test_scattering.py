d51512dcf397a6b6a70fe46885f1b5acd45205b3
Edouard Oyallon <edouard.oyallon@centralesupelec.fr>
2018-10-20 22:15:10 +0200

    [REF] Switching to torch.fft + unit tests (#18)
    Switched from skcuda cufft to torch fft
    Made unit tests run
    
    * in place operation might not be possible
    
    * Removing unecessary files.
    
    * fixing typos
    
    * fixing typos
    
    * mod
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * fixing typos
    
    * Fixing fft test typo
    
    * i put back the code of mathieu - but i'm a noob, i'm not sure how to remove this commit..
    
    * removing "in place" tag for operations which are actually not
    
    * removing useless lines in unit tests
    
    * fixing comma+unit test to be belo< 1e-6
    
    * space+scat=scattering
    
    * fixing typos
    
    fixing typos
    
    fixing typos
    
    fixing typos
    
    fixing typos
    
    fixing typos
    
    * s/jit/backend
    
    * s/jit/backend
    
    * s/jit/backend
    
    * fixing a type and tensor definition
    
    * removing copy from scattering
    
    * removing again copy_
    
    * removing again copy_
    
    * TST fix test so that it reads check file in properly
    
    * FIX the rebase removed the L=8 in the function signature

--- scattering/scattering2d/tests/test_scattering.py
+++ scattering/scattering2d/tests/test_scattering.py
@@ -1,76 +1,25 @@
 """ This script will test the submodules used by the scattering module"""
 
+import os
 import torch
 from scattering.scattering2d import Scattering2D
 from scattering.scattering2d import utils as sl
 
-
-def linfnorm(x,y):
-    return torch.max(torch.abs(x-y))
-
-def test_FFTCentralFreq(gpu=False):
-    # Checked the 0 frequency
-    x = torch.FloatTensor(10, 10, 2).fill_(0)
-    x.narrow(2, 0, 1).fill_(1)
-    if gpu:
-        x = x.cuda()
-
-    a = x.sum()
-    fft = sl.Fft()
-    fft(x, inplace=True)
-    b = x[0,0,0]
-    assert (a - b).abs().max() < 1e-6
-
-def test_FFTCentralFreqBatch(gpu=False):
-    # Same for batches
-    x = torch.FloatTensor(3,10,10,2).fill_(0)
-    x.narrow(3,0,1).fill_(1)
-    if gpu:
-        x = x.cuda()
-
-    a = x.sum()
-    fft = sl.Fft()
-    fft(x, inplace=True)
-    c = x[:,0,0,0].sum()
-    assert (a - c).abs().max() < 1e-6
-
-
-def test_FFTUnormalized(gpu=False):
-    # Check for a random tensor:
-    x = torch.FloatTensor(25, 17, 3, 2).bernoulli_(0.5)
-
-    if gpu:
-        x=x.cuda()
-    else:
-        x=x.cpu()
-    x.narrow(3,1,1).fill_(0)
-
-    fft=sl.Fft()
-    y = fft(x)
-    z = fft(y, direction='C2R')
-
-    z /= 17*3 # FFTs are unnormalized
-
-
-    assert (linfnorm(x.select(3,0), z) - 0).abs().max() < 1e-6
-
-
-
-
-# Checkked the modulus
+# Checked the modulus
 def test_Modulus():
-    for jit in [True, False]:
-        modulus = sl.Modulus(jit=jit)
-        x = torch.cuda.FloatTensor(100,10,4,2).copy_(torch.rand(100,10,4,2))
+    for backend in ['pytorch', 'skcuda']:
+        modulus = sl.Modulus(backend=backend)
+        x = torch.rand(100, 10, 4, 2).cuda().float()
         y = modulus(x)
         u = torch.squeeze(torch.sqrt(torch.sum(x * x, 3)))
         v = y.narrow(3, 0, 1)
-
+        u = u.squeeze()
+        v = v.squeeze()
         assert (u - v).abs().max() < 1e-6
 
 
 def test_Periodization():
-    for jit in [True, False]:
+    for backend in ['torch', 'skcuda']:
         x = torch.rand(100, 1, 128, 128, 2).cuda().double()
         y = torch.zeros(100, 1, 8, 8, 2).cuda().double()
 
@@ -82,33 +31,35 @@ def test_Periodization():
 
         y = y / (16*16)
 
-        periodize = sl.Periodize(jit=jit)
+        periodize = sl.Periodize(backend=backend)
 
         z = periodize(x, k=16)
         assert (y - z).abs().max() < 1e-8
-
-        z = periodize(x.cpu(), k=16)
-        assert (y.cpu() - z).abs().max() < 1e-8
+        if backend == 'torch':
+            z = periodize(x.cpu(), k=16)
+            assert (y.cpu() - z).abs().max() < 1e-8
 
 
 # Check the CUBLAS routines
 def test_Cublas():
-    for jit in [True, False]:
-        x = torch.rand(100,128,128,2).cuda()
-        filter = torch.rand(128,128,2).cuda()
+    for backend in ['pytorch', 'skcuda']:
+        x = torch.rand(100, 128, 128, 2).cuda()
+        filter = torch.rand(128, 128, 2).cuda()
         filter[..., 1] = 0
-        y = torch.ones(100,128,128,2).cuda()
-        z = torch.Tensor(100,128,128,2).cuda()
+        y = torch.ones(100, 128, 128, 2).cuda()
+        z = torch.Tensor(100, 128, 128, 2).cuda()
 
         for i in range(100):
             y[i,:,:,0]=x[i,:,:,0] * filter[:,:,0]-x[i,:,:,1] * filter[:,:,1]
-            y[i, :, :, 1] = x[i, :, :, 1] * filter[:, :, 0] + x[i, :, :, 0] *filter[:, :, 1]
-        z = sl.cdgmm(x, filter, jit=jit)
+            y[i, :, :, 1] = x[i, :, :, 1] * filter[:, :, 0] + x[i, :, :, 0] * filter[:, :, 1]
+        z = sl.cdgmm(x, filter, backend=backend)
 
         assert (y-z).abs().max() < 1e-6
 
+# Check the scattering
 def test_Scattering2D():
-    data = torch.load('test_data.pt')
+    test_data_dir = os.path.dirname(__file__)
+    data = torch.load(os.path.join(test_data_dir, 'test_data.pt'))
 
     x = data['x']
     S = data['Sx']
@@ -119,33 +70,28 @@ def test_Scattering2D():
     N = x.shape[3]
 
     # First, let's check the Jit
-    scat = Scattering2D(M, N, J, pre_pad=pre_pad,jit=True)
+    scattering = Scattering2D(M, N, J, pre_pad=pre_pad, backend='skcuda')
 
-    scat.cuda()
+    scattering.cuda()
     x = x.cuda()
     S = S.cuda()
-    y = scat(x)
+    y = scattering(x)
     assert ((S - y)).abs().max() < 1e-6
 
     # Then, let's check when using pure pytorch code
-    scat = Scattering2D(M, N, J, pre_pad=pre_pad, jit=False)
+    scattering = Scattering2D(M, N, J, pre_pad=pre_pad, backend='torch')
     Sg = []
-    Sc = []
+
     for gpu in [True, False]:
         if gpu:
             x = x.cuda()
-            scat.cuda()
+            scattering.cuda()
             S = S.cuda()
-            Sg = scat(x)
+            Sg = scattering(x)
         else:
             x = x.cpu()
             S = S.cpu()
-            scat.cpu()
-            Sg = scat(x)
-            """there are huge round off errors with fftw, numpy fft, cufft...
-            and the kernels of periodization. We do not wish to play with that as it is meaningless."""
-        print((Sg - S).abs().max())
-        assert (Sg-S).abs().max() < 1e-1
-
-
+            scattering.cpu()
+            Sg = scattering(x)
+        assert (Sg - S).abs().max() < 1e-6
 
