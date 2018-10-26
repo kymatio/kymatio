41fbd01c48d549e9b978d84a1d4792cccabc5f82
edouardoyallon <edouard.oyallon@ens.fr>
2018-10-26 21:40:10 +0200

    no idea what i'm doing :dog:

--- examples/1d/plot_filters.py
+++ examples/1d/plot_filters.py
@@ -1,5 +1,5 @@
 """
-Plot of the 2D Morlet filters used for the Scattering Transform
+Plot of the 1D Morlet filters used for the Scattering Transform
 ===============================
 See :ref:`filter_bank` for more informations about the used wavelets.
 """
@@ -14,48 +14,32 @@ import scipy.fftpack as fft
 # Initial parameters of the filter bank
 # -------------------------------------
 
-M = 32
-J = 3
-L = 8
+J_support = 3
+J_scattering = 8
+Q = 7
 
-filters_set = filter_bank(M, M, J, L=L)
+phi_fft, psi1_fft, psi2_fft, _ = scattering_filter_factory(J_support, J_scattering, Q)
 
 
-###############################################################################
-# Imshow complex images
-# -------------------------------------
-# Thanks to https://stackoverflow.com/questions/17044052/mathplotlib-imshow-complex-2d-array
-from colorsys import hls_to_rgb
-def colorize(z):
-    n, m = z.shape
-    c = np.zeros((n, m, 3))
-    c[np.isinf(z)] = (1.0, 1.0, 1.0)
-    c[np.isnan(z)] = (0.5, 0.5, 0.5)
 
-    idx = ~(np.isinf(z) + np.isnan(z))
-    A = (np.angle(z[idx]) + np.pi) / (2*np.pi)
-    A = (A + 0.5) % 1.0
-    B =  1.0/(1.0+abs(z[idx])**0.3)
-    c[idx] = [hls_to_rgb(a, b, 0.8) for a,b in zip(A,B)]
-    return c
 
 fig, axs = plt.subplots(J+1, L, sharex=True, sharey=True)
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 ###############################################################################
-# Bandpass filters
+# Bandpass filters $\psi_1$
 # ----------------
 # First, we display each wavelets according to each scale and orientation.
 i=0
-for filter in filters_set['psi']:
-    f_r = filter[0][...,0].numpy()
-    f_i = filter[0][..., 1].numpy()
-    f = f_r + 1j*f_i
-    filter_c = fft.fft2(f)
+for k in len(psi1_fft):
+    #f_r = filter[0][...,0].numpy()
+    #f_i = filter[0][..., 1].numpy()
+    #f = f_r + 1j*f_i
+    #filter_c = fft.fft(f)
+    filter_c = psi1_fft[k]
     filter_c = np.fft.fftshift(filter_c)
-    axs[i // L, i % L].imshow(colorize(filter_c))
-    axs[i // L, i % L].axis('off')
+    axs[i // L, i % L].plot(filter_c)
     axs[i // L, i % L].set_title("$j = {}$ \n $\\theta={}".format(i // L, i % L))
     i = i+1
 
