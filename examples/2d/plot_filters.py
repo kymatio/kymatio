4da1f2b42ca3accce617ac7a9d9fd1619880d17d
edouardoyallon <edouard.oyallon@ens.fr>
2018-10-26 20:08:50 +0200

    done

--- examples/2d/plot_filters.py
+++ examples/2d/plot_filters.py
@@ -1,10 +1,3 @@
-"""
-Matplotlib colormaps in Nilearn
-================================
-Visualize HCP connectome workbench color maps shipped with Nilearn
-which can be used for plotting brain images on surface.
-See :ref:`surface-plotting` for surface plotting details.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 from scattering.scattering2d.filters_bank import filters_bank
@@ -13,10 +6,7 @@ import numpy as np
 from matplotlib import rc
 
 
-###########################################################################
-# Plot color maps
-# ----------------
-
+# This function displays filter bank with those parameters:
 M = 32
 J = 3
 L = 8
@@ -44,8 +34,8 @@ def colorize(z):
     c[idx] = [hls_to_rgb(a, b, 0.8) for a,b in zip(A,B)]
     return c
 
+# Display first the psi
 for filter in filters_set['psi']:
-
     f_r = filter[0][...,0].numpy()
     f_i = filter[0][..., 1].numpy()
     f = f_r + 1j*f_i
@@ -57,18 +47,18 @@ for filter in filters_set['psi']:
     i = i+1
 
 
+# Add blanks for pretty display
 for z in range(L):
     axs[i // L, i % L].axis('off')
     i = i+1
 
 
+# Display the phi
 f_r = filters_set['phi'][0][...,0].numpy()
 f_i = filters_set['phi'][0][..., 1].numpy()
 f = f_r + 1j*f_i
-
 filter_c = fft.fft2(f)
 filter_c = np.fft.fftshift(filter_c)
-
 axs[J,L//2].imshow(colorize(filter_c))#amplitude, cmap=phase, aspect='auto')
 fig.suptitle("Wavelets for each scales $j$ and angles $\\theta$ used, with the corresponding low-pass filter.\n The contrast corresponds to the amplitude and the color to the phase.", fontsize=13)
 
