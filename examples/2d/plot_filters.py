dfbe29ff194de34021e366a397cdd70c3a50cc7f
edouardoyallon <edouard.oyallon@ens.fr>
2018-10-26 19:48:36 +0200

    pyplot

--- examples/2d/plot_filters.py
+++ examples/2d/plot_filters.py
@@ -7,27 +7,68 @@ See :ref:`surface-plotting` for surface plotting details.
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from scattering.scattering2d.filter_bank import filter_bank
+from scattering.scattering2d.filters_bank import filters_bank
+import scipy.fftpack as fft
+import numpy as np
 
 
 ###########################################################################
 # Plot color maps
 # ----------------
 
-M = 128
+M = 32
 J = 3
+L = 8
 
-filters_set = filter_bank(M, M, J)
+filters_set = filters_bank(M, M, J, L=L)
+
+fig, axs = plt.subplots(J+1, L, sharex=True, sharey=True)
+
+
+i=0
+from colorsys import hls_to_rgb
+
+
+def colorize(z):
+    n,m = z.shape
+    c = np.zeros((n,m,3))
+    c[np.isinf(z)] = (1.0, 1.0, 1.0)
+    c[np.isnan(z)] = (0.5, 0.5, 0.5)
+
+    idx = ~(np.isinf(z) + np.isnan(z))
+    A = (np.angle(z[idx]) + np.pi) / (2*np.pi)
+    A = (A + 0.5) % 1.0
+    B =  1.0/(1.0+abs(z[idx])**0.3)
+    c[idx] = [hls_to_rgb(a, b, 0.8) for a,b in zip(A,B)]
+    return c
 
 for filter in filters_set['psi']:
-    filter_c = fft.fft2(filter[0])
+
+    f_r = filter[0][...,0].numpy()
+    f_i = filter[0][..., 1].numpy()
+    f = f_r + 1j*f_i
+    filter_c = fft.fft2(f)
+    filter_c = np.fft.fftshift(filter_c)
     phase = np.angle(filter_c)
     amplitude = np.absolute(filter_c)
+    #print(amplitude.shape)
+    axs[i//L,i%L].imshow(colorize(filter_c))
+    axs[i//L,i%L].axis('off')
+    #amplitude, cmap=phase, aspect='auto')
+    #plt.title('lul', fontsize=10, va='bottom', rotation=90
+    i=i+1
 
-    plt.imshow(amplitude, cmap=plt.get_cmap(cmap), aspect='auto')
-    plt.title('lul', fontsize=10, va='bottom', rotation=90)
 
-show()
+f_r = filters_set['phi'][0][...,0].numpy()
+f_i = filters_set['phi'][0][..., 1].numpy()
+f = f_r + 1j*f_i
+
+filter_c = fft.fft2(f)
+filter_c = np.fft.fftshift(filter_c)
+
+axs[J,L//2].imshow(colorize(filter_c))#amplitude, cmap=phase, aspect='auto')
+
+plt.show()
 
 """
 nmaps = len(nilearn_cmaps)
