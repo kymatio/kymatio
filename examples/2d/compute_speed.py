d8be8ed65ee105bdf5952e3bfe64c09758f0639b
edouardoyallon <edouard.oyallon@ens.fr>
2018-10-30 12:38:55 +0100

    better formatting

--- examples/2d/compute_speed.py
+++ examples/2d/compute_speed.py
@@ -4,14 +4,21 @@ import scattering.scattering2d.backend as backend
 from scattering import Scattering2D as Scattering
 
 
+
+scattering = Scattering(M=224, N=224, J=3, L=8)
+x_data = torch.randn(128, 3, 256, 256)
+
+times = 10
+elapsed_time = -1
+
 if backend.NAME == 'skcuda':
     ############################ TORCH BACKEND - FLOAT 32 -- FORWARD ##################
     print('==> Testing Float32 with PyTorch and Torch backend')
     torch.cuda.set_device(0)
     torch.backends.cudnn.benchmark = True
 
-    scattering = Scattering(M=224, N=224, J=3, L=8).cuda()
-    x_data = torch.randn(256, 3, 224, 224).cuda().float()
+    scattering.cuda()
+    x_data = x_data.cuda().float()
 
     torch.cuda.synchronize()
     t_start = time.time()
@@ -20,13 +27,6 @@ if backend.NAME == 'skcuda':
     torch.cuda.synchronize()
     elapsed_time = time.time() - t_start
 
-    print('Elapsed time: %.2f [s / %d evals]' % (elapsed_time, 10))
-    print('Hz: %.2f [hz]' % (times / 10))
-
-
-
-
-    print('skcuda backend not installed... passing...\n')
 
 
 if backend.NAME == 'torch':
@@ -34,16 +34,15 @@ if backend.NAME == 'torch':
     print('==> Testing Float32 with PyTorch and Torch backend, on CPU, forward')
     from scattering import Scattering2D as Scattering
 
-    scattering = Scattering(M=224, N=224, J=3, L=8).cpu()
-    x_data = torch.randn(256, 3, 224, 224).float()
+    scattering.cpu()
+    x_data = x_data.cpu().float()
 
     t_start = time.time()
     for i in range(10):
         scattering(x_data)
     elapsed_time = time.time() - t_start
 
-    print('Elapsed time: %.2f [s / %d evals]' % (elapsed_time, 10))
-    print('Hz: %.2f [hz]' % (times / 10))
+print('Elapsed time: %.2f [s / %d evals], avg: %.2f' % (elapsed_time, times, elapsed_time/times))
 
 
 
