91f0e0cfa0a00325aa0d45722b84f56be7ab9784
edouardoyallon <edouard.oyallon@ens.fr>
2018-10-30 20:41:02 +0100

    fix!

--- scattering/scattering3d/tests/test_scattering3d.py
+++ scattering/scattering3d/tests/test_scattering3d.py
@@ -1,8 +1,21 @@
 """ This script will test the submodules used by the scattering module"""
 import torch
+import os
 import numpy as np
 from scattering import Scattering3D
-from scattering.scattering3d import utils as sl
+from scattering.scattering3d import backend
+from scattering.scattering3d.utils import generate_weighted_sum_of_gaussians, compute_integrals
+
+
+
+if torch.cuda.is_available():
+    devices = ['gpu', 'cpu']
+else:
+    devices = ['cpu']
+
+
+
+
 
 def linfnorm(x,y):
     return torch.max(torch.abs(x-y))
@@ -20,15 +33,14 @@ def relative_difference(a, b):
 
 def test_FFT3d_central_freq_batch():
     # Checked the 0 frequency for the 3D FFT
-    for gpu in [ True, False]:
-        x = torch.FloatTensor(1, 32, 32, 32, 2).fill_(0)
-        if gpu:
+    for device in devices:
+        x = torch.zeros(1, 32, 32, 32, 2).float()
+        if device == 'gpu':
             x = x.cuda()
         a = x.sum()
-        fft3d = sl.Fft3d()
-        y = fft3d(x)
+        y = backend.fft(x)
         c = y[:,0,0,0].sum()
-        assert rerror(a,c)<1e-6
+        assert (c-a).abs().sum()<1e-6
 
 
 def test_solid_harmonic_scattering():
@@ -40,16 +52,16 @@ def test_solid_harmonic_scattering():
     M, N, O, J, L = 128, 128, 128, 1, 3
     grid = torch.from_numpy(
         np.fft.ifftshift(np.mgrid[-M//2:-M//2+M, -N//2:-N//2+N, -O//2:-O//2+O].astype('float32'), axes=(1,2,3)))
-    x = sl.generate_weighted_sum_of_gaussians(grid, centers, weights, sigma_gaussian)
-    scat = Scattering3D(M=M, N=N, O=O, J=J, L=L, sigma_0=sigma_0_wavelet)
-    args = {'integral_powers': [1]}
-    s = scat(x, order_2=False, method='integral', method_args=args)
+    x = generate_weighted_sum_of_gaussians(grid, centers, weights, sigma_gaussian)
+    scattering = Scattering3D(M=M, N=N, O=O, J=J, L=L, sigma_0=sigma_0_wavelet)
+    s = scattering(x, order_2=False, method='integral', integral_powers=[1])
 
     for j in range(J+1):
         sigma_wavelet = sigma_0_wavelet*2**j
         k = sigma_wavelet / np.sqrt(sigma_wavelet**2 + sigma_gaussian**2)
         for l in range(1, L+1):
-            assert rerror(s[0, 0, j, l],k**l)<1e-4
+            err = torch.abs(s[0, 0, j, l] - k ** l).sum()/(1e-6+s[0, 0, j, l].abs().sum())
+            assert err<1e-4
 
 def test_against_standard_computations():
     file_path = os.path.abspath(os.path.dirname(__file__))
