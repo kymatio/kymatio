464e5f7002e0fe3f31202b7b9dafffd054659d39
Louis Thiry <biboundai@homail.fr>
2018-10-30 12:23:13 +0100

    test comparision with reference version working

--- scattering/scattering3d/tests/test_scattering3d.py
+++ scattering/scattering3d/tests/test_scattering3d.py
@@ -17,11 +17,20 @@ def rerror(x,y):
         return np.sum(np.abs(x-y))/nx
 
 
+def relative_difference(a, b):
+    return np.linalg.norm((a - b) / np.maximum(np.abs(a), np.abs(b)))
+
+
 def test_first_qm7_molecules_scattering_coefs():
     file_path = os.path.abspath(os.path.dirname(__file__))
-    order_0_ref = np.load(os.path.join(file_path, 'data/order_0_ref.npy'))
-    order_1_ref = np.load(os.path.join(file_path, 'data/order_1_ref.npy'))
-    order_2_ref = np.load(os.path.join(file_path, 'data/order_2_ref.npy'))
+    order_0_ref_cpu = np.load(os.path.join(file_path, 'data/order_0_ref_cpu.npy'))
+    order_1_ref_cpu = np.load(os.path.join(file_path, 'data/order_1_ref_cpu.npy'))
+    order_2_ref_cpu = np.load(os.path.join(file_path, 'data/order_2_ref_cpu.npy'))
+
+    order_0_ref_gpu = np.load(os.path.join(file_path, 'data/order_0_ref_gpu.npy'))
+    order_1_ref_gpu = np.load(os.path.join(file_path, 'data/order_1_ref_gpu.npy'))
+    order_2_ref_gpu = np.load(os.path.join(file_path, 'data/order_2_ref_gpu.npy'))
+
     M, N, O, J, L = 192, 128, 96, 2, 2
     integral_powers = [1., 2.]
     sigma = 1.5
@@ -36,25 +45,37 @@ def test_first_qm7_molecules_scattering_coefs():
             np.mgrid[-M//2:-M//2+M, -N//2:-N//2+N, -O//2:-O//2+O].astype('float32'),
             axes=(1, 2, 3)))
 
-    for cuda in [False, True]:
-        if cuda:
-            _grid = grid.cuda()
-            _pos = pos.cuda()
-            _charges = charges.cuda()
-        else:
-            _grid = grid
-            _pos = pos
-            _charges = charges
-        first_densities = generate_weighted_sum_of_gaussians(
-                _grid, _pos, _charges, sigma, cuda=cuda)
+    grid_gpu = grid.cuda()
+    pos_gpu = pos.cuda()
+    charges_gpu = charges.cuda()
 
-        order_0 = compute_integrals(first_densities, integral_powers)
-        order_1, order_2 = scattering(first_densities, order_2=True,
-                                method='integral', integral_powers=integral_powers)
+    densities_cpu = generate_weighted_sum_of_gaussians(
+            grid, pos, charges, sigma, cuda=False)
+    order_0_cpu = compute_integrals(densities_cpu, integral_powers)
+    order_1_cpu, order_2_cpu = scattering(densities_cpu, order_2=True,
+                            method='integral', integral_powers=integral_powers)
+    order_0_diff_cpu = relative_difference(order_0_ref_cpu, order_0_cpu.numpy())
+    order_1_diff_cpu = relative_difference(order_1_ref_cpu, order_1_cpu.numpy())
+    order_2_diff_cpu = relative_difference(order_2_ref_cpu, order_2_cpu.numpy())
 
-        assert np.linalg.norm(order_0_ref - order_0.cpu().numpy()) < 1e-4
-        assert np.linalg.norm(order_1_ref - order_1.cpu().numpy()) < 1e-4
-        assert np.linalg.norm(order_2_ref - order_2.cpu().numpy()) < 1e-4
+    print('CPU', order_0_diff_cpu, order_1_diff_cpu, order_2_diff_cpu)
+    assert  order_0_diff_cpu < 1e-3, "CPU : order 0 do not match, diff={}".format(order_0_diff_cpu)
+    assert  order_1_diff_cpu < 1e-3, "CPU : order 1 do not match, diff={}".format(order_1_diff_cpu)
+    assert  order_2_diff_cpu < 1e-3, "CPU : order 2 do not match, diff={}".format(order_2_diff_cpu)
+
+    densities_gpu = generate_weighted_sum_of_gaussians(
+            grid_gpu, pos_gpu, charges_gpu, sigma, cuda=True)
+    order_0_gpu = compute_integrals(densities_gpu, integral_powers)
+    order_1_gpu, order_2_gpu = scattering(densities_gpu, order_2=True,
+                            method='integral', integral_powers=integral_powers)
+    order_0_diff_gpu = relative_difference(order_0_ref_gpu, order_0_gpu.cpu().numpy())
+    order_1_diff_gpu = relative_difference(order_1_ref_gpu, order_1_gpu.cpu().numpy())
+    order_2_diff_gpu = relative_difference(order_2_ref_gpu, order_2_gpu.cpu().numpy())
+
+    print('GPU', order_0_diff_gpu, order_1_diff_gpu, order_2_diff_gpu)
+    assert  order_0_diff_gpu < 1e-4, "GPU : order 0 do not match, diff={}".format(order_0_diff_gpu)
+    assert  order_1_diff_gpu < 1e-4, "GPU : order 1 do not match, diff={}".format(order_1_diff_gpu)
+    assert  order_2_diff_gpu < 1e-4, "GPU : order 2 do not match, diff={}".format(order_2_diff_gpu)
 
 
 def test_solid_harmonic_scattering():
