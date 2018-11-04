1db4255e8b19a322434fa2fce398b73b5757f764
Joakim Andén <janden@flatironinstitute.org>
2018-11-03 23:50:00 -0400

    MAINT+DOC Refactor and doc 1D `compute_speed.py`
    Refactored to make script more linear and commented to provide
    ok sphinx-gallery rendering.

--- examples/1d/compute_speed.py
+++ examples/1d/compute_speed.py
@@ -1,97 +1,123 @@
 """
-Benchmark the speed of the 1D scattering transform
-==================================================
+1D scattering transform benchmark
+=================================
 We compute scattering transforms for signals of length `T = 2**16`, with scale
 `J = 10` and `Q = 8` wavelets per octave. The signals are stacked into batches
 of size `batch_size = 64` and the transform is computed `10` times to get an
 average running time.
 """
 
+###############################################################################
+# Preliminaries
+# -------------
+# Since kymatio handles PyTorch arrays, we first import `torch`.
+
 import torch
+
+###############################################################################
+# To measure the running time of the implementation, we use the `time` package.
+
 import time
+
+###############################################################################
+# The performance of the implementation depends on which "backend" is used. We
+# therefore want to report the name of the backend when presenting the results.
+# Certain backends are also GPU-only, we we want to detect that before running
+# the benchmark.
+
 import scattering.scattering1d.backend as backend
+
+###############################################################################
+# Finally, we import the `Scattering1D` class that computes the scattering
+# transform.
 from scattering import Scattering1D
 
 ###############################################################################
-# Parameters for the benchmark
-# ----------------------------
+# Benchmark setup
+# --------------------
+# First, we set up some basic parameters, the signal length `T`, the number of
+# wavelets per octave `Q`, and the averaging scale, `2**J`. For a sampling rate
+# of 11025 Hz, `T = 2**16` corresponds to about 6 seconds of audio, and an
+# averaging scale of `2**10` is about 100 milliseconds, both of which are
+# typical values for these parameters in audio applications. For `Q`, 8
+# wavelets per octave ensures that we are able to resolve isolated sinusoids
+# without sacrificing too much temporal resolution.
 
 T = 2**16
-Q = 8
 J = 10
+Q = 8
+
+###############################################################################
+# To squeeze the maximum performance out of the implementation, we apply it to
+# a batch of `64` signals. Larger batch sizes do not yield increased efficiency,
+# but smaller values increases the influence of overhead on the running time.
 
 batch_size = 64
+
+###############################################################################
+# We repeat the benchmark `10` times and compute the average running time to
+# get a reasonable estimate.
 
 times = 10
 
 ###############################################################################
-# Set up the scattering object and the test data
-# ----------------------------------------------
+# Determine which devices (CPU or GPU) that are supported by the current
+# backend.
+
+if backend.NAME == 'torch':
+    devices = ['cpu', 'gpu']
+elif backend.NAME == 'skcuda':
+    devices = ['gpu']
+
+###############################################################################
+# Create the `Scattering1D` object using the given parameters and generate
+# some compatible test data with the specified batch size.
 
 scattering = Scattering1D(T, J, Q)
 
-x_data = torch.randn(batch_size, 1, T, dtype=torch.float32)
+x = torch.randn(batch_size, 1, T, dtype=torch.float32)
 
 ###############################################################################
-# Benchmark the PyTorch backend
-# -----------------------------
-# If we're using the this backend, compute scattering transforms both on CPU
-# and GPU so that we can compare performance.
+# Run the benchmark
+# -----------------
+# For each device, we need to convert the `scattering` object and the Tensor
+# `x` to the appropriate type, invoke `times` calls to the `scattering.forward`
+# and print the running times. If the benchmark is running on the GPU, we also
+# need to call `torch.cuda.synchronize()` before and after the benchmark to
+# make sure that all CUDA kernels have finished executing.
 
-if backend.NAME == 'torch':
-    devices = ['cpu']
-    if torch.cuda.is_available():
-        devices.append('gpu')
+for device in devices:
+    fmt_str = '==> Testing Float32 with {} backend, on {}, forward'
+    print(fmt_str.format(backend.NAME, device.upper()))
 
-    for device in devices:
-        fmt_str = '==> Testing Float32 with Torch backend, on {}, forward'
-        print(fmt_str.format(device.upper()))
+    if device == 'gpu':
+        scattering.cuda()
+        x = x.cuda()
+    else:
+        scattering.cpu()
+        x = x.cpu()
 
-        if device == 'gpu':
-            scattering.cuda()
-            x_data = x_data.cuda()
-        else:
-            scattering.cpu()
-            x_data = x_data.cpu()
-
-        if device == 'gpu':
-            torch.cuda.synchronize()
-
-        t_start = time.time()
-        for _ in range(times):
-            scattering(x_data)
-
-        if device == 'gpu':
-            torch.cuda.synchronize()
-
-        t_elapsed = time.time() - t_start
-
-        fmt_str = 'Elapsed time: {:2f} [s / {:d} evals], avg: {:.2f} (s/batch)'
-        print(fmt_str.format(t_elapsed, times, t_elapsed/times))
-
-###############################################################################
-# Benchmark the skcuda backend
-# -----------------------------
-# If we're using the this backend, only compute the benchmark on the GPU.
-
-if backend.NAME == 'skcuda':
-    device = 'gpu'
-
-    fmt_str = '==> Testing Float32 with Skcuda backend, on {}, forward'
-    print(fmt_str.format(device.upper()))
-
-    scattering.cuda()
-    x_data = x_data.cuda()
-
-    torch.cuda.synchronize()
+    if device == 'gpu':
+        torch.cuda.synchronize()
 
     t_start = time.time()
     for _ in range(times):
-        scattering(x_data)
+        scattering.forward(x)
 
-    torch.cuda.synchronize()
+    if device == 'gpu':
+        torch.cuda.synchronize()
 
     t_elapsed = time.time() - t_start
 
     fmt_str = 'Elapsed time: {:2f} [s / {:d} evals], avg: {:.2f} (s/batch)'
     print(fmt_str.format(t_elapsed, times, t_elapsed/times))
+
+###############################################################################
+# The resulting output should be something like
+#
+# .. code-block:: text
+#
+#   ==> Testing Float32 with torch backend, on CPU, forward
+#   Elapsed time: 27.158231 [s / 10 evals], avg: 2.72 (s/batch)
+#   ==> Testing Float32 with torch backend, on GPU, forward
+#   Elapsed time: 8.083082 [s / 10 evals], avg: 0.81 (s/batch)
