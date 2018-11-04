ff3f42705bac49f4cf8e21a4d0ed375110537c94
Joakim Andén <janden@flatironinstitute.org>
2018-11-03 22:44:16 -0400

    MAINT+DOC Refactor and document `real_signal.py`
    The 1D example has been refactored to make a linear narrative
    easier to follow. Comments have also been added to make
    `sphinx-gallery` produce a reasonable documentation page.

--- examples/1d/real_signal.py
+++ examples/1d/real_signal.py
@@ -1,63 +1,141 @@
+"""
+Compute scattering transform of a speech signal
+===============================================
+This script loads a speech signal from the free spoken digit dataset (FSDD)
+of a man pronouncing the word "zero," computes its scattering transform, and
+displays the zeroth-, first-, and second-order scattering coefficients.
+"""
+
+###############################################################################
+# Preliminaries
+# -------------
+#
+# Since kymatio handles PyTorch arrays, we first import `torch`.
+
 import torch
+
+###############################################################################
+# To handle audio file I/O, we import `os` and `scipy.io.wavfile`.
+
+import os
+import scipy.io.wavfile
+
+###############################################################################
+# We import `matplotlib` to plot the calculated scattering coefficients.
+
+import matplotlib.pyplot as plt
+
+###############################################################################
+# Finally, we import the `Scattering1D` class from the `scattering` package and
+# the `fetch_fsdd` function from `scattering.datasets`. The `Scattering1D`
+# class is what lets us calculate the scattering transform, while the
+# `fetch_fsdd` function downloads the FSDD, if needed.
+
 from scattering import Scattering1D
 from scattering.datasets import fetch_fsdd
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
-import numpy as np
-import os
 
+###############################################################################
+# Scattering setup
+# ----------------
+# First, we make download the FSDD (if not already downloaded) and read in the
+# recording `0_jackson_0.wav` of a man pronouncing the word "zero".
 
-def loadfile(path_file):
-    sr, x = wavfile.read(path_file)
-    x = np.asarray(x, dtype='float')
-    # make it mono
-    if x.ndim > 1:
-        smallest_axis = np.argmin(x.shape)
-        x = x.mean(axis=smallest_axis)
-    x = np.asarray(x, dtype='float')
-    x /= np.max(np.abs(x))
-    return sr, x
+info_dataset = fetch_fsdd(verbose=True)
 
+file_path = os.path.join(info_dataset['path_dataset'],
+                        sorted(info_dataset['files'])[0])
+_, x = scipy.io.wavfile.read(file_path)
 
-def show_signal(x, s, order0, order1, order2):
-    fig, axarr = plt.subplots(4, 1, figsize=(8, 16))
-    axarr[0].plot(x)
-    axarr[0].set_title('Original signal')
-    axarr[1].plot(s[order0][0])
-    axarr[1].set_title('Scattering Order 0')
-    axarr[2].imshow(s[order1], aspect='auto')
-    axarr[2].set_title('Scattering Order 1')
-    axarr[3].imshow(s[order2], aspect='auto')
-    axarr[3].set_title('Scattering Order 2')
-    plt.show()
+###############################################################################
+# Once the recording is in memory, we convert it to a PyTorch Tensor, normalize
+# it, and reshape it to the form `(B, C, T)`, where `B` is the batch size, `C`
+# is the number of channels, and `T` is the number of samples in the recording.
+# In our case, we have only one signal in our batch, so `B = 1`. We also have
+# a single channel, so `C = 1`. Note that `C` is almost always `1`, for input
+# Tensors as this axis indexes the different scattering coefficients.
 
+x = torch.from_numpy(x).float()
+x /= x.abs().max()
+x = x.view(1, 1, -1)
 
-if __name__ == '__main__':
-    # fetch the dataset and get the signal
-    info_dataset = fetch_fsdd(verbose=True)
-    filepath = os.path.join(info_dataset['path_dataset'],
-                            sorted(info_dataset['files'])[0])
+###############################################################################
+# We are now ready to set up the parameters for the scattering transform.
+# First, the number of samples, `T`, is given by the size of our input `x`.
+# The averaging scale is specified as a power of two, `2**J`. Here, we set
+# `J = 6` to get an averaging, or maximum, scattering scale of `2**6 = 64`
+# samples. Finally, we set the number of wavelets per octave, `Q`, to `16`.
+# This lets us resolve frequencies at a resolution of `1/16` octaves.
 
-    # Load the signal
-    sr, x = loadfile(filepath)
-    x_th = torch.from_numpy(x).float().unsqueeze(0).unsqueeze(0)
+T = x.shape[-1]
+J = 6
+Q = 16
 
-    # Prepare the scattering
-    T = x_th.shape[-1]
-    J = 6
-    Q = 16
-    scattering = Scattering1D(T, J, Q)
+###############################################################################
+# Finally, we are able to create the object which computes our scattering
+# transform, `scattering`.
 
-    # Get the metadata
-    coords = Scattering1D.compute_meta_scattering(J, Q, order2=True)
-    order0 = torch.LongTensor([0])
-    order1 = torch.LongTensor(
-        sorted([cc for cc in coords.keys() if coords[cc]['order'] == '1']))
-    order2 = torch.LongTensor(
-        sorted([cc for cc in coords.keys() if coords[cc]['order'] == '2']))
+scattering = Scattering1D(T, J, Q)
 
-    # Compute the scattering
-    s = scattering.forward(x_th).numpy()[0]
+###############################################################################
+# Compute and display the scattering coefficients
+# -----------------------------------------------
+# Computing the scattering transform of a PyTorch Tensor is achieved using the
+# `forward()` method of the `Scattering1D` class. The output is another Tensor
+# of shape `(B, C, T)`. Here, `B` is the same as for the input `x`, but `C` is
+# the number of scattering coefficient outputs, and `T` is the number of
+# samples along the time axis. This is typically much smaller than the number
+# of input samples since the scattering transform performs an average in time
+# and subsamples the result to save memory.
 
-    # show it
-    show_signal(x, s, order0.numpy(), order1.numpy(), order2.numpy())
+Sx = scattering.forward(x)
+
+###############################################################################
+# To display the scattering coefficients, we must first identify which belong
+# to each order (zeroth, first, or second). We do this by extracting the `meta`
+# information from the scattering object and constructing masks for each order.
+
+coords = Scattering1D.compute_meta_scattering(J, Q, order2=True)
+order0 = [0]
+order1 = sorted([cc for cc in coords.keys() if coords[cc]['order'] == '1'])
+order2 = sorted([cc for cc in coords.keys() if coords[cc]['order'] == '2'])
+
+###############################################################################
+# We display the results in four separate plots arranged vertically.
+
+fig, axarr = plt.subplots(4, 1, figsize=(8, 16))
+
+###############################################################################
+# First, we plot the original signal `x`. Note that we have to index it as
+# `x[0,0,:]` to convert it to a one-dimensional array and convert it to a
+# numpy array using the `numpy()` method.
+
+axarr[0].plot(x[0,0,:].numpy())
+axarr[0].set_title('Original signal')
+
+###############################################################################
+# We now plot the zeroth-order scattering coefficient, which is simply an
+# average of the original signal at the scale `2**J`.
+
+axarr[1].plot(Sx[0,order0,:].numpy())
+axarr[1].set_title('Scattering Order 0')
+
+###############################################################################
+# We then plot the first-order coefficients, which are arranged along time
+# and log-frequency.
+
+axarr[2].imshow(Sx[0,order1,:].numpy(), aspect='auto')
+axarr[2].set_title('Scattering Order 1')
+
+###############################################################################
+# Finally, we plot the second-order scattering coefficients. These are also
+# organized aling time, but has two log-frequency indices: one first-order
+# frequency and one second-order frequency. Here, both indices are mixed along
+# the vertical axis.
+
+axarr[3].imshow(Sx[0,order2,:].numpy(), aspect='auto')
+axarr[3].set_title('Scattering Order 2')
+
+###############################################################################
+# Display the plots!
+
+plt.show()
