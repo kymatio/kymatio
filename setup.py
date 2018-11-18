e0520d32ea6f33373081591fbd61f5c04f8da15a
Michael Eickenberg <michael.eickenberg@gmail.com>
2018-11-17 21:34:56 -0800

    FIX authors

--- setup.py
+++ setup.py
@@ -18,11 +18,12 @@ setup_info = dict(
     version=VERSION,
     author=('Edouard Oyallon, Eugene Belilovsky, Sergey Zagoruyko, '
             'Michael Eickenberg, Mathieu Andreux, Georgios Exarchakis, '
-            'Louis Thiry, Vincent Lostanlen'),
+            'Louis Thiry, Vincent Lostanlen, Joakim Anden, '
+            'Tomas Angles, Gabriel Huang, Roberto Leonarduzzi'),
     author_email=('edouard.oyallon@ens.fr, eugene.belilovsky@inria.fr, '
                   'sergey.zagoruyko@enpc.fr, michael.eickenberg@berkeley.edu, '
                   'mathieu.andreux@ens.fr, georgios.exarchakis@ens.fr, '
-                  'louis.thiry@ens.fr, vincent.lostanlen@ens.fr'),
+                  'louis.thiry@ens.fr, vincent.lostanlen@ens.fr, janden@flatironinstitute.org'),
     url='https://github.com/edouardoyallon/pyscatwave',
     description='Fast CPU/CUDA Scattering Transform implementation',
     long_description=long_description,
