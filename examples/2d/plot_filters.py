19e57e5146a7752d6aed1c26d34d50675e25df02
Joakim Andén <janden@flatironinstitute.org>
2018-11-18 00:08:48 -0500

    DOC Fix titles for individual gallery scripts

--- examples/2d/plot_filters.py
+++ examples/2d/plot_filters.py
@@ -1,6 +1,6 @@
 """
-Plot of the 2D Morlet filters used for the Scattering Transform
-===============================================================
+Plot the 2D wavelet filters
+===========================
 See :meth:`scattering.scattering1d.filter_bank` for more informations about the used wavelets.
 """
 
