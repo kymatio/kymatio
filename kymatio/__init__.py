9caa3f90d3ae3b8f25d1a19ae5d4974475ec430a
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-11-20 15:58:02 -0500

    ENH import version as __version__ in __init__

--- kymatio/__init__.py
+++ kymatio/__init__.py
@@ -1,12 +1,8 @@
 __all__ = [
-            'Scattering3D',
-            'Scattering2D',
             'Scattering1D'
+            'Scattering2D',
+            'Scattering3D',
             ]
-
-from .scattering2d.scattering2d import Scattering2D
-from .scattering1d.scattering1d import Scattering1D
-from .scattering3d.scattering3d import Scattering3D
 
 # Make sure that DeprecationWarning within this package always gets printed
 ### Snippet copied from sklearn.__init__
@@ -17,3 +13,12 @@ warnings.filterwarnings('always', category=DeprecationWarning,
 ### End Snippet
 
 
+# Import wavelet scattering transform submodules
+from .scattering1d.scattering1d import Scattering1D
+from .scattering2d.scattering2d import Scattering2D
+from .scattering3d.scattering3d import Scattering3D
+
+
+# Import version.py so that the string kymatio.__version__
+# contains kymatio.version.version
+from .version import version as __version__
