7a0eb6537b7eeae60e76b7b6ddc3c10d19482730
Vincent Lostanlen <vincent.lostanlen@nyu.edu>
2018-11-20 13:28:31 -0500

    MAINT update setup_info in setup.py (#171)
    * MAINT Update `setup_info` in `setup.py`
    
    A few changes:
    
    * New URL and description
    * Parse version from `version.py`
    * Unicode author names
    * Set `download_url` to the GitHub repo
    * Add `classifiers`
    * `long_description` poured from `README.md`
    * `install_requires` parsed from `requirements.txt`
    * SPDX short identifier for the license
    
    * MAINT Update e-mails in `setup.py`
    
    * ENH define REQUIREMENTS in setup.py
    
    use f.read().split('\n') rather than a CSV reader
    thanks to @eickenberg for the advice
    
    * MAINT define DISTNAME constant setup.py
    
    this mirrors scikit-learn
    NB: Kymatio is capitalized in the DISTNAME
    
    * ENH parse version.py with importlib
    
    the imp module is deprecated in Python 3
    we use importlib instead
    this recipe is adapted from
    https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
    
    * MAINT strip out gh badges from LONG_DESCRIPTION

--- setup.py
+++ setup.py
@@ -1,43 +1,81 @@
 #!/usr/bin/env python
+import csv
+import importlib
 import os
 import shutil
 import sys
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+# Constants
+DISTNAME = 'Kymatio'
+DESCRIPTION = 'Wavelet scattering transforms in Python with GPU acceleration'
+URL = 'https://kymatio.github.io'
+LICENSE = 'BSD-3-Clause'
 
-long_description = """
-Fast CPU/CUDA Scattering Transform implementation for 1D, 2D, and 3D signals
 
-CuPy/PyTorch CUDA and NumPy/PyTorch CUDA implementation
-"""
+# Parse description
+with open('README.md') as f:
+    README = f.read()
+    LONG_DESCRIPTION = '\n'.join([x for x in README if not x[:3]=='[!['])
+
+
+# Parse version.py
+kymatio_version_spec = importlib.util.spec_from_file_location(
+    'kymatio_version', 'kymatio/version.py')
+kymatio_version_module = importlib.util.module_from_spec(kymatio_version_spec)
+kymatio_version_spec.loader.exec_module(kymatio_version_module)
+VERSION = kymatio_version_module.version
+
+
+# Parse requirements.txt
+with open('requirements.txt', 'r') as f:
+    REQUIREMENTS = f.read().split('\n')
+
 
 setup_info = dict(
     # Metadata
-    name='kymatio',
+    name=DISTNAME,
     version=VERSION,
     author=('Edouard Oyallon, Eugene Belilovsky, Sergey Zagoruyko, '
             'Michael Eickenberg, Mathieu Andreux, Georgios Exarchakis, '
-            'Louis Thiry, Vincent Lostanlen, Joakim Anden, '
-            'Tomas Angles, Gabriel Huang, Roberto Leonarduzzi'),
-    author_email=('edouard.oyallon@ens.fr, eugene.belilovsky@inria.fr, '
-                  'sergey.zagoruyko@enpc.fr, michael.eickenberg@berkeley.edu, '
+            'Louis Thiry, Vincent Lostanlen, Joakim Andén, '
+            'Tomás Angles, Gabriel Huang, Roberto Leonarduzzi'),
+    author_email=('edouard.oyallon@centralesupelec.fr, belilove@iro.umontreal.ca, '
+                  'sergey.zagoruyko@inria.fr, michael.eickenberg@berkeley.edu, '
                   'mathieu.andreux@ens.fr, georgios.exarchakis@ens.fr, '
                   'louis.thiry@ens.fr, vincent.lostanlen@nyu.edu, janden@flatironinstitute.org, '
-                  'tomas.angles@ens.fr, gabriel.huang@ens.fr, roberto.leonarduzzi@ens.fr'),
-    url='https://github.com/edouardoyallon/pyscatwave',
-    description='Fast CPU/CUDA Scattering Transform implementation',
-    long_description=long_description,
-    license='BSD',
-
-    # Package info
+                  'tomas.angles@ens.fr, gabriel.huang@umontreal.ca, roberto.leonarduzzi@ens.fr'),
+    url=URL,
+    download_url='https://github.com/kymatio/kymatio/releases',
+    classifiers=['Intended Audience :: Education',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: BSD License',
+                 'Natural Language :: English',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Topic :: Multimedia :: Graphics :: 3D Modeling',
+                 'Topic :: Multimedia :: Sound/Audio :: Analysis',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                 'Topic :: Scientific/Engineering :: Chemistry',
+                 'Topic :: Scientific/Engineering :: Image Recognition',
+                 'Topic :: Scientific/Engineering :: Information Analysis',
+                 'Topic :: Scientific/Engineering :: Mathematics',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 ],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    license=LICENSE,
     packages=find_packages(exclude=('test',)),
-
+    install_requires=REQUIREMENTS,
     zip_safe=True,
-
-    install_requires=[
-        'torch'
-    ]
 )
 
 setup(**setup_info)
