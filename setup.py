705ddab2329ed5cb9f5f4216b5e5f2fa1fa59275
Vincent Lostanlen <vincent.lostanlen@nyu.edu>
2018-11-19 19:36:26 -0500

    MAINT Update `setup_info` in `setup.py`
    A few changes:
    
    * New URL and description
    * Parse version from `version.py`
    * Unicode author names
    * Set `download_url` to the GitHub repo
    * Add `classifiers`
    * `long_description` poured from `README.md`
    * `install_requires` parsed from `requirements.txt`
    * SPDX short identifier for the license

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
+import csv
+import imp
 import os
 import shutil
 import sys
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+DESCRIPTION = 'Wavelet scattering transforms in Python with GPU acceleration'
+VERSION = imp.load_source('kymatio.version', 'kymatio/version.py').version
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+URL = 'https://kymatio.github.io'
+LICENSE = 'BSD-3-Clause'
 
-long_description = """
-Fast CPU/CUDA Scattering Transform implementation for 1D, 2D, and 3D signals
 
-CuPy/PyTorch CUDA and NumPy/PyTorch CUDA implementation
-"""
+# Parse requirements.txt
+with open('requirements.txt', 'r') as f:
+    REQUIREMENTS = [row[0] for row in csv.reader(f, delimiter='\n')]
+
 
 setup_info = dict(
     # Metadata
@@ -18,26 +25,44 @@ setup_info = dict(
     version=VERSION,
     author=('Edouard Oyallon, Eugene Belilovsky, Sergey Zagoruyko, '
             'Michael Eickenberg, Mathieu Andreux, Georgios Exarchakis, '
-            'Louis Thiry, Vincent Lostanlen, Joakim Anden, '
-            'Tomas Angles, Gabriel Huang, Roberto Leonarduzzi'),
+            'Louis Thiry, Vincent Lostanlen, Joakim Andén, '
+            'Tomás Angles, Gabriel Huang, Roberto Leonarduzzi'),
     author_email=('edouard.oyallon@ens.fr, eugene.belilovsky@inria.fr, '
                   'sergey.zagoruyko@enpc.fr, michael.eickenberg@berkeley.edu, '
                   'mathieu.andreux@ens.fr, georgios.exarchakis@ens.fr, '
                   'louis.thiry@ens.fr, vincent.lostanlen@nyu.edu, janden@flatironinstitute.org, '
                   'tomas.angles@ens.fr, gabriel.huang@ens.fr, roberto.leonarduzzi@ens.fr'),
-    url='https://github.com/edouardoyallon/pyscatwave',
-    description='Fast CPU/CUDA Scattering Transform implementation',
-    long_description=long_description,
-    license='BSD',
-
-    # Package info
+    url=URL,
+    download_url='https://github.com/kymatio/kymatio/releases',
+    classifiers=['Intended Audience :: Education',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: BSD License',
+                 'Natural Language :: English',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Topic :: Multimedia :: Graphics :: 3D Modeling',
+                 'Topic :: Multimedia :: Sound/Audio :: Analysis',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                 'Topic :: Scientific/Engineering :: Chemistry',
+                 'Topic :: Scientific/Engineering :: Image Recognition',
+                 'Topic :: Scientific/Engineering :: Information Analysis',
+                 'Topic :: Scientific/Engineering :: Mathematics',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 ],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    license=LICENSE,
     packages=find_packages(exclude=('test',)),
-
+    install_requires=REQUIREMENTS,
     zip_safe=True,
-
-    install_requires=[
-        'torch'
-    ]
 )
 
 setup(**setup_info)
