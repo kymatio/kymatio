5fa3643d01514679c53beda156dd019d87aad328
Joakim Andén <janden@flatironinstitute.org>
2018-11-21 10:39:51 -0500

    MAINT Fix naming in `mnist.py`
    Should have `scattering`, not `scat`.

--- examples/2d/mnist.py
+++ examples/2d/mnist.py
@@ -25,12 +25,12 @@ class View(nn.Module):
     def forward(self, x):
         return x.view(-1,*self.shape)
 
-def train(model, device, train_loader, optimizer, epoch, scat):
+def train(model, device, train_loader, optimizer, epoch, scattering):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
-        output = model(scat(data))
+        output = model(scattering(data))
         loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
@@ -39,14 +39,14 @@ def train(model, device, train_loader, optimizer, epoch, scat):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
-def test(model, device, test_loader, scat):
+def test(model, device, test_loader, scattering):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            output = model(scat(data))
+            output = model(scattering(data))
             test_loss += F.cross_entropy(output, target, size_average=False).item() # sum up batch loss
             pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
