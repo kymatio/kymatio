8ccc9b83c117e317ee25c6a14627ae6887815747
edouardoyallon <edouard.oyallon@ens.fr>
2018-11-23 14:27:56 +0100

    Removing the warning

--- examples/2d/mnist.py
+++ examples/2d/mnist.py
@@ -47,7 +47,7 @@ def test(model, device, test_loader, scattering):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(scattering(data))
-            test_loss += F.cross_entropy(output, target, size_average=False).item() # sum up batch loss
+            test_loss += F.cross_entropy(output, target, reduction='sum').item() # sum up batch loss
             pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
