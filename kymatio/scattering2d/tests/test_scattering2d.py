cef1fd14107f9c026020087c8161bc34efadcfcd
Michael Eickenberg <michael.eickenberg@gmail.com>
2018-12-16 19:16:50 +0000

    API+TST changed 2D output order
    TST reordered coefficients in the scattering2D test

--- kymatio/scattering2d/tests/test_scattering2d.py
+++ kymatio/scattering2d/tests/test_scattering2d.py
@@ -1,6 +1,7 @@
 """ This script will test the submodules used by the scattering module"""
 
 import os
+import numpy as np
 import torch
 import pytest
 from kymatio.scattering2d import Scattering2D
@@ -139,6 +140,27 @@ def test_Cublas():
 
                 assert (y - z).abs().max() < 1e-6
 
+
+def reorder_coefficients_from_interleaved(J, L):
+    # helper function to obtain positions of order0, order1, order2 from interleaved
+    order0, order1, order2 = [], [], []
+    n_order0, n_order1, n_order2 = 1, J * L, L ** 2 * J * (J - 1) // 2
+    n = 0
+    order0.append(n)
+    for j1 in range(J):
+        for l1 in range(L):
+            n += 1
+            order1.append(n)
+            for j2 in range(j1 + 1, J):
+                for l2 in range(L):
+                    n += 1
+                    order2.append(n)
+    assert len(order0) == n_order0
+    assert len(order1) == n_order1
+    assert len(order2) == n_order2
+    return order0, order1, order2
+
+
 # Check the scattering
 # FYI: access the two different tests in here by setting envs
 # KYMATIO_BACKEND=skcuda and KYMATIO_BACKEND=torch
@@ -149,6 +171,14 @@ def test_Scattering2D():
     x = data['x']
     S = data['Sx']
     J = data['J']
+
+    # we need to reorder S from interleaved (how it's saved) to o0, o1, o2
+    # (which is how it's now computed)
+
+    o0, o1, o2 = reorder_coefficients_from_interleaved(J, L=8)
+    reorder = torch.from_numpy(np.concatenate((o0, o1, o2)))
+    S = S[..., reorder, :, :]
+
     pre_pad = data['pre_pad']
 
     M = x.shape[2]
