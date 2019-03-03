035d7e32e73ce2d2e422b56f07488f8f7740211e
Joakim Andén <janden@flatironinstitute.org>
2019-03-03 15:13:33 -0500

    TST Manually test `ModulusStable.backward` (1D)
    Although this function is already tested through PyTorch's autograd
    functionality, it doesn't count towards code coverage since the call goes
    through the PyTorch C extensions. Instead, we call `backward` manually to
    ensure that it works properly and is included in the code coverage.
    
    Fixes #300.

--- kymatio/scattering1d/tests/test_utils.py
+++ kymatio/scattering1d/tests/test_utils.py
@@ -105,6 +105,19 @@ def test_modulus(random_state=42):
     diff = x.grad - x_grad
     assert torch.max(torch.abs(diff)) <= 1e-7
 
+    # Manually check `forward`/`backward` using fake context object. This
+    # ensures that `backward` is included in code coverage since going through
+    # the PyTorch C extension seems to not play well with `coverage.py`.
+    class FakeContext:
+        def save_for_backward(self, *args):
+            self.saved_tensors = args
+
+    ctx = FakeContext()
+    y = backend.ModulusStable.forward(ctx, x)
+    y_grad = torch.ones_like(y)
+    x_grad_manual = backend.ModulusStable.backward(ctx, y_grad)
+    assert (x_grad_manual - x_grad).abs().max() < 1e-7
+
     # Test the differentiation with a vector made of zeros
     x0 = torch.zeros(100, 4, 128, 2, requires_grad=True)
     if force_gpu:
