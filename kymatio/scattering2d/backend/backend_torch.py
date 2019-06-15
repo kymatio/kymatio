FIX fft normalization bug

kymatio/scattering2d/backend/backend_torch.py
@@ -161,12 +161,24 @@ def fft(input, direction='C2C', inverse=False):
         raise (RuntimeError('Tensors must be contiguous!'))
 
     if direction == 'C2R':
-        output = torch.irfft(input, 2, normalized=False, onesided=False)*input.size(-2)*input.size(-3)
+        output = torch.irfft(input, 2, normalized=True, onesided=False)
     elif direction == 'C2C':
         if inverse:
-            output = torch.ifft(input, 2, normalized=False)*input.size(-2)*input.size(-3)
+            output = torch.ifft(input, 2, normalized=True)
         else:
-            output = torch.fft(input, 2, normalized=False)
+            output = torch.fft(input, 2, normalized=True)
+
+    # j = 1
+    # if direction == 'C2R':
+    #     input *= input.size(-2) * input.size(-3) // 2 ** (2 * j)
+    #     output = torch.irfft(input, 2, normalized=False, onesided=False)
+    # elif direction == 'C2C':
+    #     if inverse:
+    #         input *= input.size(-2) * input.size(-3) // 2 ** (2 * j)
+    #         output = torch.ifft(input, 2, normalized=False)
+    #     else:
+    #         output = torch.fft(input, 2, normalized=False)
+    #     output /= input.size(-2) * input.size(-3) // 2 ** (2 * j)
 
     return output
 