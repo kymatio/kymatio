bee6fbb248ab6dd18ea7a77496b627e88817706c
edouardoyallon <edouard.oyallon@ens.fr>
2019-07-22 23:43:56 +0200

    backend

--- kymatio/scattering2d/backend/__init__.py
+++ kymatio/scattering2d/backend/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ['torch_backend', 'torch_skcuda_backend']
+__all__ = ['torch_backend', 'torch_skcuda_backend', 'numpy_backend']
 
 try:
     from .torch_backend import backend as torch_backend
     from .torch_skcuda_backend import backend as torch_skcuda_backend
+    from .numpy_backend import backend as numpy_backend
 except:
     pass
