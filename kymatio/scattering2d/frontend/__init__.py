58048009a8a4c1c90f4711a172758a3463023eb8
edouardoyallon <edouard.oyallon@ens.fr>
2019-08-14 17:02:17 +0200

    frontend

--- kymatio/scattering2d/frontend/__init__.py
+++ kymatio/scattering2d/frontend/__init__.py
@@ -1,6 +1,23 @@
-__all__ = ['Scattering2DTorch', 'Scattering2DNumpy', 'Scattering2DTensorflow']
+__all__ = []
+
+try:
+    from .torch_frontend import Scattering2DTorch
+    __all__.append('Scattering2DTorch')
+except:
+    pass
+
+try:
+    from .tensorflow_frontend import Scattering2DTensorflow
+    __all__.append('Scattering2DTensorflow')
+except:
+    pass
+
+try:
+    from .numpy_frontend import Scattering2DNumpy
+    __all__.append('Scattering2DNumpy')
+except:
+    pass
 
 
-from .torch_frontend import Scattering2DTorch
-from .tensorflow_frontend import Scattering2DTensorflow
-from .numpy_frontend import Scattering2DNumpy
+
+
