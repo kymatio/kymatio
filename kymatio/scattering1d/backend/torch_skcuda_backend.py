3763605b519642f70b3d011ee458fafa4d182771
edouardoyallon <edouard.oyallon@ens.fr>
2019-08-20 12:46:18 +0100

    implementing torch skcuda backend

--- kymatio/scattering1d/backend/torch_skcuda_backend.py
+++ kymatio/scattering1d/backend/torch_skcuda_backend.py
@@ -1,14 +1,11 @@
 # Authors: Edouard Oyallon, Joakim Anden
 
-import numpy as np
 import torch
-import torch.nn.functional as F
-from torch.autograd import Function
 import cupy
 from collections import namedtuple
 from string import Template
 
-NAME = 'skcuda'
+NAME = 'torch_skcuda'
 
 @cupy.util.memoize(for_each_device=True)
 def load_kernel(kernel_name, code, **kwargs):
@@ -219,145 +216,20 @@ def subsample_fourier(x, k):
     """
     return subsamplefourier(x,k)
 
-def pad_1d(x, pad_left, pad_right, mode='constant', value=0.):
-    """Pad real 1D tensors
 
-    1D implementation of the padding function for real PyTorch tensors.
+from torch_backend import ifft1d_c2c, fft1d_c2c, real, unpad, pad, pad_1d, finalize, ModulusStable
 
-    Parameters
-    ----------
-    x : tensor
-        Three-dimensional input tensor with the third axis being the one to
-        be padded.
-    pad_left : int
-        Amount to add on the left of the tensor (at the beginning of the
-        temporal axis).
-    pad_right : int
-        amount to add on the right of the tensor (at the end of the temporal
-        axis).
-    mode : string, optional
-        Padding mode. Options include 'constant' and 'reflect'. See the
-        PyTorch API for other options.  Defaults to 'constant'.
-    value : float, optional
-        If mode == 'constant', value to input within the padding. Defaults to
-        0.
+backend = namedtuple('backend', ['name', 'modulus_complex', 'subsample_fourier', 'real', 'unpad', 'fft1d_c2c',\
+                                 'ifft1d_c2c', 'finalize'])
 
-    Returns
-    -------
-    res : tensor
-        The tensor passed along the third dimension.
-    """
-    if (pad_left >= x.shape[-1]) or (pad_right >= x.shape[-1]):
-        if mode == 'reflect':
-            raise ValueError('Indefinite padding size (larger than tensor).')
-    res = F.pad(x.unsqueeze(2),
-                (pad_left, pad_right, 0, 0),
-                mode=mode, value=value).squeeze(2)
-    return res
-
-def pad(x, pad_left=0, pad_right=0, to_complex=True):
-    """Pad real 1D tensors and map to complex
-
-    Padding which allows to simultaneously pad in a reflection fashion and map
-    to complex if necessary.
-
-    Parameters
-    ----------
-    x : tensor
-        Three-dimensional input tensor with the third axis being the one to
-        be padded.
-    pad_left : int
-        Amount to add on the left of the tensor (at the beginning of the
-        temporal axis).
-    pad_right : int
-        amount to add on the right of the tensor (at the end of the temporal
-        axis).
-    to_complex : boolean, optional
-        Whether to map the resulting padded tensor to a complex type (seen
-        as a real number). Defaults to True.
-
-    Returns
-    -------
-    output : tensor
-        A padded signal, possibly transformed into a four-dimensional tensor
-        with the last axis of size 2 if to_complex is True (this axis
-        corresponds to the real and imaginary parts).
-    """
-    output = pad_1d(x, pad_left, pad_right, mode='reflect')
-    if to_complex:
-        output = torch.stack((output, torch.zeros_like(output)), dim=-1)
-    return output
-
-def unpad(x, i0, i1):
-    """Unpad real 1D tensor
-
-    Slices the input tensor at indices between i0 and i1 along the last axis.
-
-    Parameters
-    ----------
-    x : tensor
-        Input tensor with least one axis.
-    i0 : int
-        Start of original signal before padding.
-    i1 : int
-        End of original signal before padding.
-
-    Returns
-    -------
-    x_unpadded : tensor
-        The tensor x[..., i0:i1].
-    """
-    return x[..., i0:i1]
-
-def real(x):
-    """Real part of complex tensor
-
-    Takes the real part of a complex tensor, where the last axis corresponds
-    to the real and imaginary parts.
-
-    Parameters
-    ----------
-    x : tensor
-        A complex tensor (that is, whose last dimension is equal to 2).
-
-    Returns
-    -------
-    x_real : tensor
-        The tensor x[..., 0] which is interpreted as the real part of x.
-    """
-    return x[..., 0]
-
-def fft1d_c2c(x):
-    """Compute the 1D FFT of a complex signal
-
-    Input
-    -----
-    x : tensor
-        A tensor of size (..., T, 2), where x[..., 0] is the real part and
-        x[..., 1] is the imaginary part.
-
-    Returns
-    -------
-    x_f : tensor
-        A tensor of the same size as x containing its Fourier transform in the
-        standard PyTorch FFT ordering.
-    """
-    return torch.fft(x, signal_ndim=1)
-
-def ifft1d_c2c(x):
-    """Compute the normalized 1D inverse FFT of a complex signal
-
-    Input
-    -----
-    x_f : tensor
-        A tensor of size (..., T, 2), where x_f[..., 0] is the real part and
-        x[..., 1] is the imaginary part. The frequencies are assumed to be in
-        the standard PyTorch FFT ordering.
-
-    Returns
-    -------
-    x : tensor
-        A tensor of the same size of x_f containing the normalized inverse
-        Fourier transform of x_f.
-    """
-    return torch.ifft(x, signal_ndim=1)
+backend.name = 'torch_skcuda'
+backend.modulus_complex = modulus_complex
+backend.ModulusStable = ModulusStable
+backend.subsample_fourier = subsample_fourier
+backend.real = real
+backend.unpad = unpad
+backend.pad = pad
+backend.pad_1d = pad_1d
+backend.fft1d_c2c = fft1d_c2c
+backend.ifft1d_c2c = ifft1d_c2c
+backend.finalize = finalize
