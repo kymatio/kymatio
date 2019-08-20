6f1aea58fc0a5f178a892f8e0f8a65cb8e0d19ff
Joakim Andén <janden@flatironinstitute.org>
2019-08-19 22:07:37 -0400

    TST Make 1D meta filter work again
    To slice torch arrays, we need to be using torch arrays as indices. Since
    `meta()` now returns numpy arrays, using them to slice torch arrays has a
    different behavior (in certain versions of torch). To make sure we get the
    desired behavior, we have to cast to torch before slicing.

--- kymatio/scattering1d/tests/test_torch_scattering1d.py
+++ kymatio/scattering1d/tests/test_torch_scattering1d.py
@@ -66,7 +66,7 @@ def test_simple_scatterings(device, backend, random_state=42):
         x2.to(device)
         s2 = scattering(x2)
 
-        assert(s2[:,meta['order'] != 1,:].abs().max() < 1e-2)
+        assert(s2[:,torch.from_numpy(meta['order']) != 1,:].abs().max() < 1e-2)
 
 @pytest.mark.parametrize("device", devices)
 @pytest.mark.parametrize("backend", backends)
