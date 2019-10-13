87c22cbf57899e96bbe1da983c366b5f22c8a2cf
edouardoyallon <edouard.oyallon@ens.fr>
2019-10-13 19:03:50 +0200

    setting a higher non determinist tolerance

--- kymatio/scattering2d/tests/test_torch_scattering2d.py
+++ kymatio/scattering2d/tests/test_torch_scattering2d.py
@@ -376,4 +376,4 @@ class TestScattering2DTorch:
         else:
             scattering = Scattering2D(2, shape=(8, 8), backend=backend, frontend='torch').double().to(device)
             x = torch.rand(2, 1, 8, 8).double().to(device).requires_grad_()
-            gradcheck(scattering, x)
+            gradcheck(scattering, x, nondet_tol=1e-5)
