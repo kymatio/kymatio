1389a718609a8459004705fc3186c3e5e20d245b
chaudhm <chaudhm@wwu.edu>
2019-10-20 17:57:50 -0700

    ENH Import Scattering classes from sub-packages
    Instead of importing the Scattering classes directly from the modules that
    define then (such as `kymatio.scattering2d.scattering2d`), import them from
    their corresponding sub-packages (such as `kymatio.scattering2d`). This is
    because the former only works pre-frontends, while the latter allows for a more
    flexible definition through `kymatio.scattering2d.__init__`).

--- kymatio/__init__.py
+++ kymatio/__init__.py
@@ -2,6 +2,7 @@
 ### Snippet copied from sklearn.__init__
 import warnings
 import re
+
 warnings.filterwarnings('always', category=DeprecationWarning,
                         module=r'^{0}.*'.format(re.escape(__name__)))
 ### End Snippet
@@ -12,8 +13,8 @@ __all__ = [
             'HarmonicScattering3D'
             ]
 
-from .scattering1d.scattering1d import Scattering1D
-from .scattering2d.scattering2d import Scattering2D
-from .scattering3d.scattering3d import HarmonicScattering3D
+from .scattering1d import Scattering1D
+from .scattering2d import Scattering2D
+from .scattering3d import HarmonicScattering3D
 
 from .version import version as __version__
