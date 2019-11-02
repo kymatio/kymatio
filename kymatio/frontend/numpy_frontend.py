8a317106606169347f7566d3c58589ef0f76649b
chaudhm <chaudhm@wwu.edu>
2019-11-01 19:39:09 -0700

    incoprated new front end scheme
    after squash fixes
    
    squash fixes

--- kymatio/frontend/numpy_frontend.py
+++ kymatio/frontend/numpy_frontend.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 class ScatteringNumPy(object):
    def __init__(self):
@@ -16,30 +15,3 @@ class ScatteringNumPy(object):
        """ Returns the logging message when the frontend is deployed."""
         raise NotImplementedError
 
-=======
-from abc import ABCMeta, abstractmethod
-
-class ScatteringNumpy(object, metaclass=ABCMeta):
-   def __init__(self):
-       super(ScatteringNumpy, self).__init__()
-
-   @abstractmethod
-   def build(self):
-       """ Defines elementary routines."""
-
-   @abstractmethod
-   def scattering(self, x):
-       """ This function should call the functional scattering."""
-
-   def __call__(self, x):
-       return self.scattering(x)
-
-   @abstractmethod
-   def loginfo(self):
-       """
-       Returns the logging message when the frontend is deployed.
-       -------
-
-       """
-       
->>>>>>> e801b80... adding all basic files
