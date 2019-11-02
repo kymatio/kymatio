51dd3b81bff619944d32a7974db22c9797c24132
chaudhm <chaudhm@wwu.edu>
2019-11-02 15:24:41 -0700

    identation changes

--- kymatio/frontend/numpy_frontend.py
+++ kymatio/frontend/numpy_frontend.py
@@ -1,13 +1,13 @@
 
 class ScatteringNumPy(object):
-   def __init__(self):
-       super(ScatteringNumPy, self).__init__()
-
-   def scattering(self, x):
-       """ This function should compute the scattering transform."""
+    def __init__(self):
+        super(ScatteringNumPy, self).__init__()
+        
+    def scattering(self, x):
+        """ This function should compute the scattering transform."""
         raise NotImplementedError
 
-   def __call__(self, x):
-       """ This function should call the functional scattering."""
-       return self.scattering(x)
+    def __call__(self, x):
+        """ This function should call the functional scattering."""
+        return self.scattering(x)
 
