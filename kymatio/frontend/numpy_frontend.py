c221711c6768fbf91c89467c5aba7ced73ab73db
chaudhm <chaudhm@wwu.edu>
2019-11-07 00:31:25 -0500

    ENH Add parent frontend classes
    These are:
    - `ScatteringBase`, the parent class for all frontends,
    - `ScatteringNumPy`, the parent class for the NumPy frontends,
    - `ScatteringTensorFlow`, the parent class for the TF frontends, and,
    - 'ScatteringTorch', the parent class for the Torch frontends.
    
    Right now, these provide some minimal shared functionality related to
    calling conventions in the different frameworks. More generally, they
    provide a location for frontend-specific code that is independent of the
    dimension of the transform.

---
+++ kymatio/frontend/numpy_frontend.py
@@ -0,0 +1,13 @@
+class ScatteringNumPy(object):
+    def __init__(self):
+        super(ScatteringNumPy, self).__init__()
+        self.frontend_name = 'numpy'
+
+    def scattering(self, x):
+        """ This function should compute the scattering transform."""
+        raise NotImplementedError
+
+    def __call__(self, x):
+        """ This function provides a standard NumPy calling interface to the
+        scattering computation."""
+        return self.scattering(x)
