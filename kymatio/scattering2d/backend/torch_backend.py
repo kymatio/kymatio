e6fccabc56a63beb91207d6dffb2c3fb5a9b7659
chaudhm <chaudhm@wwu.edu>
2019-11-25 23:00:52 -0500

    API Introduce 2D frontend (torch)
    So far, only for PyTorch. Note that this commit doesn't change any of the unit
    tests to keep things simple. As a result, this breaks the 2D tests that rely on
    the old backend framework being present.
    
    We also remove backend-specific 2D tests. This will make the 2D tests
    run for the new frontend, since the Scattering2D interface hasn't
    changed. We keep these for now just to make sure we're backwards
    compatible (since the default frontend is still the PyTorch one), but
    once we're comfortable that the new, frontend-specific tests cover this,
    we should remove these since they are redundant.

---
+++ kymatio/scattering2d/backend/torch_backend.py
@@ -0,0 +1,336 @@
+# Authors: Edouard Oyallon, Sergey Zagoruyko
+
+import torch
+from torch.nn import ReflectionPad2d
+from collections import namedtuple
+
+BACKEND_NAME = 'torch'
+
+def iscomplex(x):
+    return x.shape[-1] == 2
+
+def isreal(x):
+    return x.shape[-1] == 1
+
+class Pad(object):
+    def __init__(self, pad_size, input_size, pre_pad=False):
+        """Padding which allows to simultaneously pad in a reflection fashion
+            and map to complex.
+
+            Parameters
+            ----------
+            pad_size : list of 4 integers
+                Size of padding to apply [top, bottom, left, right].
+            input_size : list of 2 integers
+                size of the original signal [height, width].
+            pre_pad : boolean, optional
+                If set to true, then there is no padding, one simply adds the imaginary part.
+
+        """
+        self.pre_pad = pre_pad
+        self.pad_size = pad_size
+        self.input_size = input_size
+
+        self.build()
+
+    def build(self):
+        """Builds the padding module.
+
+            Attributes
+            ----------
+            padding_module : ReflectionPad2d
+                Pads the input tensor using the reflection of the input
+                boundary.
+
+        """
+        pad_size_tmp = list(self.pad_size)
+
+        # This handles the case where the padding is equal to the image size
+        if pad_size_tmp[0] == self.input_size[0]:
+            pad_size_tmp[0] -= 1
+            pad_size_tmp[1] -= 1
+        if pad_size_tmp[2] == self.input_size[1]:
+            pad_size_tmp[2] -= 1
+            pad_size_tmp[3] -= 1
+        # Pytorch expects its padding as [left, right, top, bottom]
+        self.padding_module = ReflectionPad2d([pad_size_tmp[2], pad_size_tmp[3],
+                                               pad_size_tmp[0], pad_size_tmp[1]])
+
+    def __call__(self, x):
+        """Applies padding and maps to complex.
+
+            Parameters
+            ----------
+            x : tensor
+                Real tensor input to be padded and sent to complex domain.
+
+            Returns
+            -------
+            output : tensor
+                Complex torch tensor that has been padded.
+
+        """
+        batch_shape = x.shape[:-2]
+        signal_shape = x.shape[-2:]
+        x = x.reshape((-1, 1) + signal_shape)
+        if not self.pre_pad:
+            x = self.padding_module(x)
+            if self.pad_size[0] == self.input_size[0]:
+                x = torch.cat([x[:, :, 1, :].unsqueeze(2), x, x[:, :, x.shape[2] - 2, :].unsqueeze(2)], 2)
+            if self.pad_size[2] == self.input_size[1]:
+                x = torch.cat([x[:, :, :, 1].unsqueeze(3), x, x[:, :, :, x.shape[3] - 2].unsqueeze(3)], 3)
+
+        output = x.new_zeros(x.shape + (2,))
+        output[..., 0] = x
+        output = output.reshape(batch_shape + output.shape[-3:])
+        return output
+
+def unpad(in_):
+    """Unpads input.
+
+        Slices the input tensor at indices between 1:-1.
+
+        Parameters
+        ----------
+        in_ : tensor
+            Input tensor.
+
+        Returns
+        -------
+        in_[..., 1:-1, 1:-1] : tensor
+            Output tensor.  Unpadded input.
+
+    """
+    return torch.unsqueeze(in_[..., 1:-1, 1:-1], -3)
+
+class SubsampleFourier(object):
+    """Subsampling of a 2D image performed in the Fourier domain
+
+        Subsampling in the spatial domain amounts to periodization
+        in the Fourier domain, hence the formula.
+
+        Parameters
+        ----------
+        x : tensor
+            Input tensor with at least 5 dimensions, the last being the real
+            and imaginary parts. Ideally, the last dimension should be a power
+            of 2 to avoid errors.
+        k : int
+            Integer such that x is subsampled by 2**k along the spatial variables.
+
+        Returns
+        -------
+        out : tensor
+            Tensor such that its Fourier transform is the Fourier
+            transform of a subsampled version of x, i.e. in
+            F^{-1}(out)[u1, u2] = F^{-1}(x)[u1 * (2**k), u2 * (2**k)].
+
+    """
+    def __call__(self, x, k):
+        if not x.is_contiguous():
+            raise RuntimeError('Input should be contiguous.')
+        batch_shape = x.shape[:-3]
+        signal_shape = x.shape[-3:]
+        x = x.view((-1,) + signal_shape)
+        y = x.view(-1,
+                       k, x.shape[1] // k,
+                       k, x.shape[2] // k,
+                       2)
+
+        out = y.mean(3, keepdim=False).mean(1, keepdim=False)
+        out = out.reshape(batch_shape + out.shape[-3:])
+        return out
+
+class Modulus(object):
+    """This class implements a modulus transform for complex numbers.
+
+        Usage
+        -----
+        modulus = Modulus()
+        x_mod = modulus(x)
+
+        Parameters
+        ---------
+        x : tensor
+            Complex torch tensor.
+
+        Returns
+        -------
+        output : tensor
+            A tensor with the same dimensions as x, such that output[..., 0]
+            contains the complex modulus of x, while output[..., 1] = 0.
+
+    """
+    def __call__(self, x):
+        norm = torch.zeros_like(x)
+        norm[...,0] = (x[...,0]*x[...,0] +
+                       x[...,1]*x[...,1]).sqrt()
+        return norm
+
+def fft(x, direction='C2C', inverse=False):
+    """Interface with torch FFT routines for 2D signals.
+
+        Example
+        -------
+        x = torch.randn(128, 32, 32, 2)
+        x_fft = fft(x)
+        x_ifft = fft(x, inverse=True)
+
+        Parameters
+        ----------
+        x : tensor
+            Complex input for the FFT.
+        direction : string
+            'C2R' for complex to real, 'C2C' for complex to complex.
+        inverse : bool
+            True for computing the inverse FFT.
+            NB : If direction is equal to 'C2R', then an error is raised.
+
+        Raises
+        ------
+        RuntimeError
+            In the event that we are going from complex to real and not doing
+            the inverse FFT or in the event x is not contiguous.
+        TypeError
+            In the event that x does not have a final dimension 2 i.e. not
+            complex.
+
+        Returns
+        -------
+        output : tensor
+            Result of FFT or IFFT.
+
+    """
+    if direction == 'C2R':
+        if not inverse:
+            raise RuntimeError('C2R mode can only be done with an inverse FFT.')
+
+    if not iscomplex(x):
+        raise TypeError('The input should be complex (e.g. last dimension is 2).')
+
+    if not x.is_contiguous():
+        raise RuntimeError('Tensors must be contiguous.')
+
+    if direction == 'C2R':
+        output = torch.irfft(x, 2, normalized=False, onesided=False) * x.shape[-2] * x.shape[-3]
+    elif direction == 'C2C':
+        if inverse:
+            output = torch.ifft(x, 2, normalized=False) * x.shape[-2] * x.shape[-3]
+        else:
+            output = torch.fft(x, 2, normalized=False)
+
+    return output
+
+def cdgmm(A, B, inplace=False):
+    """Complex pointwise multiplication.
+
+        Complex pointwise multiplication between (batched) tensor A and tensor B.
+
+        Parameters
+        ----------
+        A : tensor
+            A is a complex tensor of size (B, C, M, N, 2).
+        B : tensor
+            B is a complex tensor of size (M, N, 2) or real tensor of (M, N, 1).
+        inplace : boolean, optional
+            If set to True, all the operations are performed in place.
+
+        Raises
+        ------
+        RuntimeError
+            In the event that the filter B is not a 3-tensor with a last
+            dimension of size 1 or 2, or A and B are not compatible for
+            multiplication.
+        TypeError
+            In the event that A is not complex, or B does not have a final
+            dimension of 1 or 2, or A and B are not of the same dtype, or if
+            A and B are not on the same device.
+
+        Returns
+        -------
+        C : tensor
+            Output tensor of size (B, C, M, N, 2) such that:
+            C[b, c, m, n, :] = A[b, c, m, n, :] * B[m, n, :].
+
+    """
+    if not iscomplex(A):
+        raise TypeError('The input must be complex, indicated by a last '
+                        'dimension of size 2.')
+
+    if B.ndimension() != 3:
+        raise RuntimeError('The filter must be a 3-tensor, with a last '
+                           'dimension of size 1 or 2 to indicate it is real '
+                           'or complex, respectively.')
+
+    if not iscomplex(B) and not isreal(B):
+        raise TypeError('The filter must be complex or real, indicated by a '
+                        'last dimension of size 2 or 1, respectively.')
+
+    if A.shape[-3:-1] != B.shape[-3:-1]:
+        raise RuntimeError('The filters are not compatible for multiplication.')
+
+    if A.dtype is not B.dtype:
+        raise TypeError('A and B must be of the same dtype.')
+
+    if B.device.type == 'cuda':
+        if A.device.type == 'cuda':
+            if A.device.index != B.device.index:
+                raise TypeError('A and B must be on the same GPU.')
+        else:
+            raise TypeError('A must be on GPU.')
+
+    if B.device.type == 'cpu':
+        if A.device.type == 'cuda':
+            raise TypeError('A must be on CPU.')
+
+    if isreal(B):
+        if inplace:
+            return A.mul_(B)
+        else:
+            return A * B
+    else:
+        C = A.new(A.shape)
+
+        A_r = A[..., 0].contiguous().view(-1, A.shape[-2]*A.shape[-3])
+        A_i = A[..., 1].contiguous().view(-1, A.shape[-2]*A.shape[-3])
+
+        B_r = B[...,0].contiguous().view(B.shape[-2]*B.shape[-3]).unsqueeze(0).expand_as(A_i)
+        B_i = B[..., 1].contiguous().view(B.shape[-2]*B.shape[-3]).unsqueeze(0).expand_as(A_r)
+
+        C[..., 0].view(-1, C.shape[-2]*C.shape[-3])[:] = A_r * B_r - A_i * B_i
+        C[..., 1].view(-1, C.shape[-2]*C.shape[-3])[:] = A_r * B_i + A_i * B_r
+
+        return C if not inplace else A.copy_(C)
+
+def finalize(s0, s1, s2):
+    """Concatenate scattering of different orders.
+
+        Parameters
+        ----------
+        s0 : tensor
+            Tensor which contains the zeroth order scattering coefficents.
+        s1 : tensor
+            Tensor which contains the first order scattering coefficents.
+        s2 : tensor
+            Tensor which contains the second order scattering coefficents.
+
+        Returns
+        -------
+        s : tensor
+            Final output. Scattering transform.
+
+    """
+    if len(s2) > 0:
+        return torch.cat([torch.cat(s0, -3), torch.cat(s1, -3), torch.cat(s2, -3)], -3)
+    else:
+        return torch.cat([torch.cat(s0, -3), torch.cat(s1, -3)], -3)
+
+backend = namedtuple('backend', ['name', 'cdgmm', 'modulus', 'subsample_fourier', 'fft', 'Pad', 'unpad', 'finalize'])
+backend.name = 'torch'
+backend.cdgmm = cdgmm
+backend.modulus = Modulus()
+backend.subsample_fourier = SubsampleFourier()
+backend.fft = fft
+backend.Pad = Pad
+backend.unpad = unpad
+backend.finalize = finalize
---
+++ kymatio/scattering2d/frontend/base_frontend.py
@@ -0,0 +1,108 @@
+from ...frontend.base_frontend import ScatteringBase
+
+from ..filter_bank import filter_bank
+from ..utils import compute_padding
+
+
+class Scattering2DBase(ScatteringBase):
+    """ Main module implementing the scattering transform in 2D.
+        The scattering transform computes two wavelet transform followed
+        by modulus non-linearity.
+        It can be summarized as::
+
+            S_J x = [S_J^0 x, S_J^1 x, S_J^2 x]
+
+        for::
+
+            S_J^0 x = x * phi_J
+            S_J^1 x = [|x * psi^1_lambda| * phi_J]_lambda
+            S_J^2 x = [||x * psi^1_lambda| * psi^2_mu| * phi_J]_{lambda, mu}
+
+        where * denotes the convolution (in space), phi_J is a lowpass
+        filter, psi^1_lambda is a family of bandpass
+        filters and psi^2_mu is another family of bandpass filters.
+        Only Morlet filters are used in this implementation.
+        Convolutions are efficiently performed in the Fourier domain.
+
+        Example
+        -------
+            # 1) Define a Scattering2D object as:
+                s = Scattering2D_torch(J, shape=(M, N))
+            #    where (M, N) is the image size and 2**J the scale of the scattering
+            # 2) Forward on an input Tensor x of shape B x M x N,
+            #     where B is the batch size.
+                result_s = s(x)
+
+        Parameters
+        ----------
+        J : int
+            Log-2 of the scattering scale.
+        shape : tuple of ints
+            Spatial support (M, N) of the input.
+        L : int, optional
+            Number of angles used for the wavelet transform. Defaults to `8`.
+        max_order : int, optional
+            The maximum order of scattering coefficients to compute. Must be either
+            `1` or `2`. Defaults to `2`.
+        pre_pad : boolean, optional
+            Controls the padding: if set to False, a symmetric padding is applied
+            on the signal. If set to True, the software will assume the signal was
+            padded externally. Defaults to `False`.
+        backend : object, optional
+            Controls the backend which is combined with the frontend.
+
+        Attributes
+        ----------
+        J : int
+            Log-2 of the scattering scale.
+        shape : tuple of int
+            Spatial support (M, N) of the input.
+        L : int, optional
+            Number of angles used for the wavelet transform.
+        max_order : int, optional
+            The maximum order of scattering coefficients to compute.
+            Must be either equal to `1` or `2`.
+        pre_pad : boolean
+            Controls the padding: if set to False, a symmetric padding is applied
+            on the signal. If set to True, the software will assume the signal was
+            padded externally.
+        Psi : dictionary
+            Contains the wavelets filters at all resolutions. See
+            filter_bank.filter_bank for an exact description.
+        Phi : dictionary
+            Contains the low-pass filters at all resolutions. See
+            filter_bank.filter_bank for an exact description.
+        M_padded, N_padded : int
+             Spatial support of the padded input.
+
+        Notes
+        -----
+        The design of the filters is optimized for the value L = 8.
+
+        pre_pad is particularly useful when cropping bigger images because
+        this does not introduce border effects inherent to padding.
+        """
+
+    def __init__(self, J, shape, L=8, max_order=2, pre_pad=False, backend=None):
+        super(Scattering2DBase, self).__init__()
+        self.pre_pad = pre_pad
+        self.L = L
+        self.backend = backend
+        self.J = J
+        self.shape = shape
+        self.max_order = max_order
+
+    def build(self):
+        self.M, self.N = self.shape
+
+        if 2 ** self.J > self.M or 2 ** self.J > self.N:
+            raise RuntimeError('The smallest dimension should be larger than 2^J.')
+        self.M_padded, self.N_padded = compute_padding(self.M, self.N, self.J)
+        # pads equally on a given side if the amount of padding to add is an even number of pixels, otherwise it adds an extra pixel
+        self.pad = self.backend.Pad([(self.M_padded - self.M) // 2, (self.M_padded - self.M+1) // 2, (self.N_padded - self.N) // 2,
+                                (self.N_padded - self.N + 1) // 2], [self.M, self.N], pre_pad=self.pre_pad)
+        self.unpad = self.backend.unpad
+
+    def create_filters(self):
+        filters = filter_bank(self.M_padded, self.N_padded, self.J, self.L)
+        self.phi, self.psi = filters['phi'], filters['psi']
