9c883226cf98c9a0bc75529a9291d8eb6752b3e7
edouardoyallon <edouard.oyallon@ens.fr>
2019-11-25 22:49:52 -0500

    ENH Error messages in 2D torch backend loading
    Make these more specific (and, it is hoped, more useful).

--- kymatio/frontend/base_frontend.py
+++ kymatio/frontend/base_frontend.py
@@ -16,11 +16,22 @@ class ScatteringBase():
     def _instantiate_backend(self, import_string):
         """ This function should instantiate the backend to be used if not already
         specified"""
+
+        # Either the user entered a string, in which case we load the corresponding backend.
         if isinstance(self.backend, str):
-            self.backend = importlib.import_module(import_string + self.backend \
-                    + "_backend", 'backend').backend
-        elif not self.backend.name.startswith(self.frontend_name):
-            raise RuntimeError('This backend is not supported.')
+            if self.backend.startswith(self.frontend_name):
+                try:
+                    self.backend = importlib.import_module(import_string + self.backend + "_backend", 'backend').backend
+                except ImportError:
+                    raise ImportError('Backend ' + self.backend + ' not found!')
+            else:
+                raise ImportError('The backend ' + self.backend + ' can not be called from the frontend ' +
+                                   self.frontend_name + '.')
+        # Either the user passed a backend object, in which case we perform a compatibility check.
+        else:
+            if not self.backend.name.startswith(self.frontend_name):
+                raise ImportError('The backend ' + self.backend.name + ' is not supported by the frontend ' +
+                                   self.frontend_name + '.')
 
     def create_filters(self):
         """ This function should run a filterbank function that
