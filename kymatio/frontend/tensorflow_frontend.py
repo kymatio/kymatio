a6ce59253c08093f326b8510179573bc86613f4c
Joakim Andén <janden@flatironinstitute.org>
2019-12-11 11:37:38 -0500

    REF Remove version check in TF frontend

--- kymatio/frontend/tensorflow_frontend.py
+++ kymatio/frontend/tensorflow_frontend.py
@@ -2,11 +2,6 @@ import tensorflow as tf
 from packaging import version
 
 
-assert (version.parse(tf.__version__) >= version.parse('2.0.0a0'),
-        'Current TensorFlow version is ' + str(tf.__version__) + '. '
-        'Please upgrade TensorFlow to 2.0.0a0 or later.')
-
-
 class ScatteringTensorFlow(tf.Module):
     def __init__(self, name):
         super(ScatteringTensorFlow, self).__init__(name=name)
