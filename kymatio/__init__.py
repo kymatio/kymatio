a1fa057d5ad9fd232ba4c48683efc323b27ced92
edouardoyallon <edouard.oyallon@ens.fr>
2020-01-23 16:26:13 -0500

    FIX Make sure PendingDeprecationWarning is shown

--- kymatio/__init__.py
+++ kymatio/__init__.py
@@ -5,6 +5,8 @@ import re
 
 warnings.filterwarnings('always', category=DeprecationWarning,
                         module=r'^{0}.*'.format(re.escape(__name__)))
+warnings.filterwarnings('always', category=PendingDeprecationWarning,
+                        module=r'^{0}.*'.format(re.escape(__name__)))
 ### End Snippet
 
 __all__ = [
