88d31218bf83e75905dff11f1d704b7f9c2afd52
edouardoyallon <edouard.oyallon@ens.fr>
2020-01-24 06:49:52 -0500

    ENH Simplify wrapper classes
    Move the basic wrapping code from `Scattering1D` and `Scattering2D` into
    the (new) `Scattering` class. Also enable new syntactic sugar imports
    like
    
    ```
    from kymatio.tensorflow import Scattering2D
    S = Scattering2D(J=J, shape=shape)
    ```
    
    which doesn't require us to specify the `frontend` kwarg.

---
+++ kymatio/tensorflow.py
@@ -0,0 +1,4 @@
+from .scattering1d.frontend.tensorflow_frontend import ScatteringTensorFlow1D as Scattering1D
+from .scattering2d.frontend.tensorflow_frontend import ScatteringTensorFlow2D as Scattering2D
+
+__all__ = ['Scattering1D', 'Scattering2D']
---
+++ kymatio/torch.py
@@ -0,0 +1,4 @@
+from .scattering1d.frontend.torch_frontend import ScatteringTorch1D as Scattering1D
+from .scattering2d.frontend.torch_frontend import ScatteringTorch2D as Scattering2D
+
+__all__ = ['Scattering1D', 'Scattering2D']
