fd65d1609dcfbe426cc158198825f87bf56f0694
Joakim Andén <janden@flatironinstitute.org>
2020-01-25 16:21:23 -0500

    REF Import identical backend functions from torch

--- kymatio/scattering3d/backend/torch_skcuda_backend.py
+++ kymatio/scattering3d/backend/torch_skcuda_backend.py
@@ -11,46 +11,6 @@ def _iscomplex(input):
     return input.shape[-1] == 2
 
 
-def complex_modulus(input_array):
-    modulus = torch.zeros_like(input_array)
-    modulus[..., 0] += torch.sqrt((input_array ** 2).sum(-1))
-    return modulus
-
-
-def fft(input, inverse=False):
-    """Interface with torch FFT routines for 3D signals.
-        fft of a 3d signal
-        Example
-        -------
-        x = torch.randn(128, 32, 32, 32, 2)
-
-        x_fft = fft(x)
-        x_ifft = fft(x, inverse=True)
-        Parameters
-        ----------
-        x : tensor
-            Complex input for the FFT.
-        inverse : bool
-            True for computing the inverse FFT.
-
-        Raises
-        ------
-        TypeError
-            In the event that x does not have a final dimension 2 i.e. not
-            complex.
-
-        Returns
-        -------
-        output : tensor
-            Result of FFT or IFFT.
-    """
-    if not _iscomplex(input):
-        raise (TypeError('The input should be complex (e.g. last dimension is 2)'))
-    if inverse:
-        return torch.ifft(input, 3)
-    return torch.fft(input, 3)
-
-
 def cdgmm3d(A, B, inplace=False):
     """Complex pointwise multiplication.
 
@@ -117,6 +77,8 @@ def cdgmm3d(A, B, inplace=False):
     return C
 
 
+from .torch_backend import complex_modulus
+from .torch_backend import fft
 from .torch_backend import modulus_rotation
 from .torch_backend import subsample
 from .torch_backend import compute_integrals
