0c32ce225013252c8a0af3aed67628a7122c5189
eickenberg <eickenberg@users.noreply.github.com>
2020-01-25 16:54:15 -0500

    FIX docstring and remove newline

--- kymatio/backend/tensorflow_backend.py
+++ kymatio/backend/tensorflow_backend.py
@@ -8,18 +8,20 @@ def _isreal(x):
     return x.dtype == np.float32 or x.dtype == np.float64
 
 class Modulus(object):
-    """
-        This class implements a modulus transform for complex numbers.
-        Usage
-        -----
-        modulus = Modulus()
-        x_mod = modulus(x)
-        Parameters
-        ---------
-        x: input complex tensor.
-        Returns
-        -------
-        output: a real tensor equal to the modulus of x.
+    """This class implements a modulus transform for complex numbers.
+
+    Parameters
+    ----------
+    x: input complex tensor.
+
+    Returns
+    ----------
+    output: a real tensor equal to the modulus of x.
+
+    Usage
+    ----------
+    modulus = Modulus()
+    x_mod = modulus(x)
     """
     def __call__(self, x):
         norm = tf.abs(x)
