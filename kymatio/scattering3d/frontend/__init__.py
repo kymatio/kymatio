e1231307d2d1123d4976aed776fb3f22c493218b
Joakim Andén <janden@flatironinstitute.org>
2020-01-25 17:00:01 -0500

    REF Use Scattering for boilerplate

--- kymatio/scattering3d/frontend/__init__.py
+++ kymatio/scattering3d/frontend/__init__.py
@@ -1,36 +1,11 @@
-import logging
-import warnings
-import importlib
+from ...frontend import Scattering
 
 
-class HarmonicScattering3D(object):
+class HarmonicScattering3D(Scattering):
     def __init__(self, *args, **kwargs):
-        frontend_suffixes = {'torch' : 'Torch', 'numpy' : 'NumPy', 'tensorflow'
-                : 'TensorFlow'}
-        if 'frontend' not in kwargs:
-            warnings.warn("Torch frontend is currently the default, but NumPy will become the default in the next"
-                          " version.", PendingDeprecationWarning)
-            frontend = 'torch'
-        else:
-            frontend = kwargs['frontend'].lower()
-            kwargs.pop('frontend')
-
-        try:
-            module = importlib.import_module('kymatio.scattering3d.frontend.' + frontend + '_frontend')
-
-            # Create frontend-specific class name by inserting frontend name
-            # after `Scattering`.
-            frontend = frontend_suffixes[frontend]
-
-            class_name = self.__class__.__name__
-            class_name = (class_name[:-2] + frontend + class_name[-2:])
-
-            self.__class__ = getattr(module, class_name)
-            self.__init__(*args, **kwargs)
-        except Exception as e:
-            raise e from RuntimeError('\nThe frontend \'' + frontend + '\' could not be correctly imported.')
-
-        logging.info('The hardmonic 3D frontend ' + frontend + ' was imported.')
+        super().__init__(name='harmonic 3D',
+                         class_name='scattering3d',
+                         *args, **kwargs)
 
 
 __all__ = ['HarmonicScattering3D']
