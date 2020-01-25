ba309e448463fc8a996ff483222c59df0baa9f1a
Joakim Andén <janden@flatironinstitute.org>
2020-01-25 17:37:29 -0500

    DOC Change 3D doc back to actual 3D doc
    Was 2D doc for some reason.

--- kymatio/scattering3d/frontend/base_frontend.py
+++ kymatio/scattering3d/frontend/base_frontend.py
@@ -3,83 +3,48 @@ from ..filter_bank import solid_harmonic_filter_bank, gaussian_filter_bank
 
 
 class ScatteringBase3D(ScatteringBase):
-    """ Abstract module implementing the scattering transform in 2D. Actual
-        implementations of the Scattering2D inherits from its methods and
-        attributes. The scattering transform computes two wavelet transform
-        followed by modulus non-linearity. It can be summarized as::
+    """3D Solid Harmonic scattering .
 
-            S_J x = [S_J^0 x, S_J^1 x, S_J^2 x]
+    This class implements solid harmonic scattering on an input 3D image.
+    For details see https://arxiv.org/abs/1805.00571.
 
-        for::
+    Instantiates and initializes a 3d solid harmonic scattering object.
 
-            S_J^0 x = x * phi_J
-            S_J^1 x = [|x * psi^1_lambda| * phi_J]_lambda
-            S_J^2 x = [||x * psi^1_lambda| * psi^2_mu| * phi_J]_{lambda, mu}
+    Parameters
+    ----------
+    J: int
+        number of scales
+    shape: tuple of int
+        shape (M, N, O) of the input signal
+    L: int, optional
+        Number of l values. Defaults to 3.
+    sigma_0: float, optional
+        Bandwidth of mother wavelet. Defaults to 1.
+    max_order: int, optional
+        The maximum order of scattering coefficients to compute. Must be
+        either 1 or 2. Defaults to 2.
+    rotation_covariant: bool, optional
+        if set to True the first order moduli take the form:
 
-        where * denotes the convolution (in space), phi_J is a lowpass
-        filter, psi^1_lambda is a family of bandpass
-        filters and psi^2_mu is another family of bandpass filters.
-        Only Morlet filters are used in this implementation.
-        Convolutions are efficiently performed in the Fourier domain.
+        $\\sqrt{\\sum_m (x \\star \\psi_{j,l,m})^2)}$
 
-        Example
-        -------
-            # 1) Define a Scattering2D object as:
-                s = Scattering2D(J, shape=(M, N))
-            #    where (M, N) is the image size and 2**J the scale of the scattering
-            # 2) Forward on an input Tensor x of shape B x M x N,
-            #     where B is the batch size.
-                result_s = s(x)
+        if set to False the first order moduli take the form:
 
-        Parameters
-        ----------
-        J : int
-            Log-2 of the scattering scale.
-        shape : tuple of ints
-            Spatial support (M, N) of the input.
-        L : int, optional
-            Number of angles used for the wavelet transform. Defaults to `8`.
-        max_order : int, optional
-            The maximum order of scattering coefficients to compute. Must be either
-            `1` or `2`. Defaults to `2`.
-        pre_pad : boolean, optional
-            Controls the padding: if set to False, a symmetric padding is applied
-            on the signal. If set to True, the software will assume the signal was
-            padded externally. Defaults to `False`.
-        backend : object, optional
-            Controls the backend which is combined with the frontend.
+        $x \\star \\psi_{j,l,m}$
 
-        Attributes
-        ----------
-        J : int
-            Log-2 of the scattering scale.
-        shape : tuple of int
-            Spatial support (M, N) of the input.
-        L : int, optional
-            Number of angles used for the wavelet transform.
-        max_order : int, optional
-            The maximum order of scattering coefficients to compute.
-            Must be either equal to `1` or `2`.
-        pre_pad : boolean
-            Controls the padding: if set to False, a symmetric padding is applied
-            on the signal. If set to True, the software will assume the signal was
-            padded externally.
-        Psi : dictionary
-            Contains the wavelets filters at all resolutions. See
-            filter_bank.filter_bank for an exact description.
-        Phi : dictionary
-            Contains the low-pass filters at all resolutions. See
-            filter_bank.filter_bank for an exact description.
-        M_padded, N_padded : int
-             Spatial support of the padded input.
+        The second order moduli change analogously
+        Defaut: True
+    method: string, optional
+        specifies the method for obtaining scattering coefficients
+        ("standard","local","integral"). Default: "standard"
+    points: array-like, optional
+        List of locations in which to sample wavelet moduli. Used when
+        method == 'local'
+    integral_powers: array-like
+        List of exponents to the power of which moduli are raised before
+        integration. Used with method == 'standard', method == 'integral'
 
-        Notes
-        -----
-        The design of the filters is optimized for the value L = 8.
-
-        pre_pad is particularly useful when cropping bigger images because
-        this does not introduce border effects inherent to padding.
-        """
+    """
     def __init__(self, J, shape, L=3, sigma_0=1, max_order=2,
                  rotation_covariant=True, method='standard', points=None,
                  integral_powers=(0.5, 1., 2.), backend=None):
