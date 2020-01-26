f7952011442e78dec699081de12173d931e5e63f
eugenium <eugenium@github.com>
2020-01-26 18:59:47 -0500

    adding sklearn transformer and example

---
+++ kymatio/tests/test_qutils.py
@@ -0,0 +1,42 @@
+import os
+import torch
+
+from kymatio.utils import ScatteringTransformer
+from kymatio.scattering2d import Scattering2D
+import io
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+from sklearn.utils.estimator_checks import check_estimator
+
+def test_sklearn_transformer():
+    test_data_dir = os.path.join(os.path.dirname(__file__), 
+                                    "..", "scattering2d",
+                                    "tests")
+    with open(os.path.join(test_data_dir, 'test_data_2d.npz'), 'rb') as f:
+        buffer = io.BytesIO(f.read())
+        data = np.load(buffer)
+
+
+    x = torch.from_numpy(data['x'])
+    J = data['J']
+
+    S = Scattering2D(J, x.shape[2:],frontend='torch')
+    Sx = S.forward(x)
+
+    x_raveled = x.reshape(x.shape[0], -1).detach().cpu().numpy()
+    Sx_raveled = Sx.reshape(x.shape[0], -1).detach().cpu().numpy()
+
+
+    st = ScatteringTransformer(S, x[0].shape,'torch').fit()
+
+    t = st.transform(x_raveled)
+    assert_array_almost_equal(Sx_raveled, t)
+
+    # Check numpy
+    S = Scattering2D(J, x.shape[2:], frontend='numpy')
+    st = ScatteringTransformer(S, x[0].shape, 'numpy').fit()
+
+    t = st.transform(x_raveled)
+
+    assert_array_almost_equal(Sx_raveled, t)
+
---
+++ kymatio/utils.py
@@ -0,0 +1,57 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+import torch
+
+
+class ScatteringTransformer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, S, signal_shape, J, frontend='numpy'):
+        """Creates an object that is compatible with the scikit-learn API
+        and implements the .transform method.
+        
+        Parameters
+        ==========
+
+        S: an instance of Scattering1D, Scattering2D or Scattering3D
+            This instance is called by the transformer
+
+        signal_shape: tuple of ints
+            The shape of one sample. Scikit-learn convention is to work with
+            2D matrices only, of shape (n_samples, n_features). Data is
+            delivered in this way and has to be reshaped before transform
+
+        Output
+        ======
+        Y: ndarray, shape (n_samples, n_scattering_features)
+            The scattering coefficients, raveled in C order
+        """
+
+        self.scattering = S
+        self.signal_shape = signal_shape
+        self.frontend = frontend
+
+        assert frontend in ['numpy','torch']
+
+    def fit(self, X=None, y=None):
+        # no fitting necessary
+        return self
+
+    def predict(self, X):
+        X_reshaped = X.reshape((-1,) + self.signal_shape)
+        if self.frontend is 'torch':
+            X_reshaped = torch.from_numpy(X_reshaped)
+            if hasattr(self.scattering, "device"):
+                X_reshaped = X_reshaped.to(self.scattering.device)
+
+        transformed = self.scattering(X_reshaped)
+
+        if self.frontend is 'torch':
+            output = transformed.detach().cpu().numpy().reshape(X.shape[0], -1)
+        else:
+            output = transformed.reshape(X.shape[0], -1)
+
+        return output
+
+    transform = predict
+
+
