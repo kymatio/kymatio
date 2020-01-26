0ed3eaa859b7b3553625b03c0f140031ebb5c754
Edouard Oyallon <edouard.oyallon@lip6.fr>
2020-01-25 21:24:10 -0500

    ENH Pytorch 3D Back/Frontend
    * initial commit for the 3D backendfrontend in torch
    
    * 3D is here
    
    * MAINT changed all instaces of size to shape
    
    * STY changed and added puncutation to error messages
    
    * MAINT changed all instances of size to shape
    
    * STY removed backslash
    
    * MAINT removed import method using unknown globals and locals, use import module now
    
    * STY removed and added blank space
    
    * STY removed and added blank lines. changed imports
    
    * MAINT changed all instances of size to shape
    
    * MAINT removed useless backend instaniation
    
    * MAINT remove use of locals
    
    * STY added and removed blank lines
    
    * TST Fix 3D standard test
    
    * ENH Do proper batch reshaping in 3D
    
    * ENH Use concatenate backend primitive in 3D torch
    
    This way, we do one big concatenate at the end and therefore do not need
    the intermediate calls to `aggregate` and `finalize`.
    
    * TST Remove old 3D tests
    
    * FIX Make new concatenate work for skcuda
    
    * TST Fix 3D torch test for GPU
    
    * REF Remove remnants of finalize and aggregate
    
    * STY Fix backend tuple creation
    
    * FIX Make torch_skcuda backend work again
    
    * STY Clean up core 3D code
    
    * ENH Remove to_complex from skcuda backend
    
    * REF Import identical backend functions from torch
    
    * DOC Capitalize CPU in error messages
    
    * REF Remove unneeded calls in 3D torch init
    
    * STY+REF Address minor comments from PR
    
    * REF Use Scattering for boilerplate
    
    * DOC No rights reserved
    
    * DOC Change 3D doc back to actual 3D doc
    
    Was 2D doc for some reason.
    
    * REF+STY Addressing some of Michael's comments
    
    And others.
    
    Co-authored-by: MuawizChaudhary <39755015+MuawizChaudhary@users.noreply.github.com>
    Co-authored-by: Joakim Andén <janden@flatironinstitute.org>

--- kymatio/scattering3d/utils.py
+++ kymatio/scattering3d/utils.py
@@ -1,97 +1,54 @@
-"""Author: Louis Thiry, All rights reserved, 2018."""
-import torch
+"""Author: Louis Thiry"""
 import numpy as np
 import warnings
 
 
-def generate_weighted_sum_of_gaussians(grid, positions, weights, sigma,
-                                       cuda=False):
+def generate_weighted_sum_of_gaussians(grid, positions, weights, sigma):
     """
         Computes sum of 3D Gaussians centered at given positions and weighted
         with the given weights.
-
         Parameters
         ----------
-        grid : torch tensor
+        grid : numpy array
             numerical grid, size (3, M, N, O)
-        positions: torch tensor
+        positions: numpy array
             positions of the Gaussians, size (B, N_gaussians, 3)
             B batch_size, N_gaussians number or gaussians
-        positions: torch tensor
+        weights: numpy array
             weights of the Gaussians, size (B, N_gaussians)
             zero weights are assumed to be at the end since if a weight is zero
             all weights after are ignored
         sigma : float
             width parameter of the Gaussian
-        cuda: boolean, optional
-            if True, computations are done on CUDA GPU
-
         Returns
         -------
-        signals : torch tensor
+        signals : numpy array
             numpy array of size (B, M, N, O)
             B is the batch_size, M, N, O are the size of the signal
     """
-    _, M, N, O = grid.size()
-    signals = torch.zeros(positions.size(0), M, N, O)
-    if cuda:
-        signals = signals.cuda()
+    _, M, N, O = grid.shape
+    signals = np.zeros((positions.shape[0], M, N, O))
 
-    for i_signal in range(positions.size(0)):
-        n_points = positions[i_signal].size(0)
+    for i_signal in range(positions.shape[0]):
+        n_points = positions[i_signal].shape[0]
         for i_point in range(n_points):
             if weights[i_signal, i_point] == 0:
                 break
             weight = weights[i_signal, i_point]
             center = positions[i_signal, i_point]
-            signals[i_signal] += weight * torch.exp(
+            signals[i_signal] = np.add(signals[i_signal] , weight * np.exp(
                 -0.5 * ((grid[0] - center[0]) ** 2 +
                         (grid[1] - center[1]) ** 2 +
-                        (grid[2] - center[2]) ** 2) / sigma**2)
+                        (grid[2] - center[2]) ** 2) / sigma**2))
     return signals / ((2 * np.pi) ** 1.5 * sigma ** 3)
-
-
-def subsample(input_array, j):
-    return input_array[..., ::2 ** j, ::2 ** j, ::2 ** j, :].contiguous()
-
-
-
-def compute_integrals(input_array, integral_powers):
-    """
-        Computes integrals of the input_array to the given powers.
-
-        Parameters
-        ----------
-        input_array: torch tensor
-            size (B, M, N, O), B batch_size, M, N, O spatial dims
-
-        integral_powers: list
-            list of P positive floats containing the p values used to
-            compute the integrals of the input_array to the power p (l_p norms)
-
-        Returns
-        -------
-        integrals: torch tensor
-            tensor of size (B, P) containing the integrals of the input_array
-            to the powers p (l_p norms)
-
-    """
-    integrals = torch.zeros(input_array.size(0), len(integral_powers), 1)
-    for i_q, q in enumerate(integral_powers):
-        integrals[:, i_q, 0] = (input_array ** q).view(
-                                        input_array.size(0), -1).sum(1).cpu()
-    return integrals
-
 
 def get_3d_angles(cartesian_grid):
     """
         Given a cartesian grid, computes the spherical coord angles (theta, phi).
-
         Parameters
         ----------
         cartesian_grid: numpy array
             4D array of shape (3, M, N, O)
-
         Returns
         -------
         polar: numpy array
@@ -114,20 +71,16 @@ def double_factorial(i):
 def sqrt(x):
     """
         Compute the square root of an array
-
         This suppresses any warnings due to invalid input, unless the array is
         real and has negative values. This fixes the erroneous warnings
         introduced by an Intel SVM bug for large single-precision arrays. For
         more information, see:
-
             https://github.com/numpy/numpy/issues/11448
             https://github.com/ContinuumIO/anaconda-issues/issues/9129
-
         Parameters
         ----------
         x : numpy array
             An array for which we would like to compute the square root.
-
         Returns
         -------
         y : numpy array
@@ -148,7 +101,6 @@ def _apply_filters(filters, fn):
         ----------
         filters: a filter bank
         fn: a function to apply on the parameters
-
         Returns
         -------
         filters: the filters modified
