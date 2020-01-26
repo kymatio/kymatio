aa6e4a0654ee8bd57389f1dd4be3856ae033da1b
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2020-01-25 22:11:51 -0500

    TST Normalize benchmarks
    Make sure they have the same number of samples and therefore give
    comparable running times. Fixes #359.

--- benchmarks/benchmarks/common.py
+++ benchmarks/benchmarks/common.py
@@ -1,2 +1,9 @@
 import torch
+
+
+# Make sure we have the same random numbers for each run.
 torch.manual_seed(0)
+
+# This constant corresponds to the total number of samples per batch. It is
+# shared across all benchmarks of scattering: 1D, 2D, and 3D.
+SCATTERING_BENCHMARK_SIZE = 2 ** 21
