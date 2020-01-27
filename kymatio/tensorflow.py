0b7b9badb95d00ae18253023537afe362547bb9c
edouardoyallon <edouard.oyallon@ens.fr>
2020-01-26 21:41:51 -0500

    DOC Update the documentation
    Rewrite narrative and include the new templated docs in the API
    reference.

--- kymatio/tensorflow.py
+++ kymatio/tensorflow.py
@@ -3,4 +3,13 @@ from .scattering2d.frontend.tensorflow_frontend import ScatteringTensorFlow2D as
 from .scattering3d.frontend.tensorflow_frontend \
         import HarmonicScatteringTensorFlow3D as HarmonicScattering3D
 
+Scattering1D.__module__ = 'kymatio.tensorflow'
+Scattering1D.__name__ = 'Scattering1D'
+
+Scattering2D.__module__ = 'kymatio.tensorflow'
+Scattering2D.__name__ = 'Scattering2D'
+
+HarmonicScattering3D.__module__ = 'kymatio.tensorflow'
+HarmonicScattering3D.__name__ = 'HarmonicScattering3D'
+
 __all__ = ['Scattering1D', 'Scattering2D', 'HarmonicScattering3D']
