85e43680514d6d78cbcfd446dfe71b9edbe50856
chaudhm <chaudhm@wwu.edu>
2020-01-27 11:51:33 -0800

    STY white space changes

--- kymatio/tests/test_utils.py
+++ kymatio/tests/test_utils.py
@@ -16,16 +16,14 @@ def test_sklearn_transformer():
         buffer = io.BytesIO(f.read())
         data = np.load(buffer)
 
-
     x = torch.from_numpy(data['x'])
     J = data['J']
 
-    S = Scattering2D(J, x.shape[2:],frontend='torch')
+    S = Scattering2D(J, x.shape[2:], frontend='torch')
     Sx = S.forward(x)
 
     x_raveled = x.reshape(x.shape[0], -1).detach().cpu().numpy()
     Sx_raveled = Sx.reshape(x.shape[0], -1).detach().cpu().numpy()
-
 
     st = ScatteringTransformer(S, x[0].shape,'torch').fit()
 
@@ -39,4 +37,3 @@ def test_sklearn_transformer():
     t = st.transform(x_raveled)
 
     assert_array_almost_equal(Sx_raveled, t)
-
