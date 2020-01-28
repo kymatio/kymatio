d4a183eee72c1b29390be3a6d4ae6c241a2baf47
Joakim Andén <janden@flatironinstitute.org>
2020-01-27 22:12:05 -0500

    ENH Make 3D frontends aliases available

--- kymatio/numpy.py
+++ kymatio/numpy.py
@@ -1,4 +1,6 @@
 from .scattering1d.frontend.numpy_frontend import ScatteringNumPy1D as Scattering1D
 from .scattering2d.frontend.numpy_frontend import ScatteringNumPy2D as Scattering2D
+from .scattering3d.frontend.numpy_frontend \
+        import HarmonicScatteringNumPy3D as HarmonicScattering3D
 
-__all__ = ['Scattering1D', 'Scattering2D']
+__all__ = ['Scattering1D', 'Scattering2D', 'HarmonicScattering3D']
--- kymatio/torch.py
+++ kymatio/torch.py
@@ -1,4 +1,6 @@
 from .scattering1d.frontend.torch_frontend import ScatteringTorch1D as Scattering1D
 from .scattering2d.frontend.torch_frontend import ScatteringTorch2D as Scattering2D
+from .scattering3d.frontend.torch_frontend \
+        import HarmonicScatteringTorch3D as HarmonicScattering3D
 
-__all__ = ['Scattering1D', 'Scattering2D']
+__all__ = ['Scattering1D', 'Scattering2D', 'HarmonicScattering3D']
