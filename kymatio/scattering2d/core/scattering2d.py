2040a14c867021352ef1d3759c692df6b56f0891
Joakim Andén <janden@flatironinstitute.org>
2020-01-28 16:27:33 -0500

    API Change list output to list of dicts in 2D
    Instead of list of arrays.

--- kymatio/scattering2d/core/scattering2d.py
+++ kymatio/scattering2d/core/scattering2d.py
@@ -22,10 +22,14 @@ def scattering2d(x, pad, unpad, backend, J, L, phi, psi, max_order, vectorize=Tr
     S_0 = fft(U_1_c, 'C2R', inverse=True)
     S_0 = unpad(S_0)
 
-    out_S_0.append(S_0)
+    out_S_0.append({'coef': S_0,
+                    'j': (),
+                    'theta': ()})
 
     for n1 in range(len(psi)):
         j1 = psi[n1]['j']
+        theta1 = psi[n1]['theta']
+
         U_1_c = cdgmm(U_0_c, psi[n1][0])
         if j1 > 0:
             U_1_c = subsample_fourier(U_1_c, k=2 ** j1)
@@ -40,14 +44,19 @@ def scattering2d(x, pad, unpad, backend, J, L, phi, psi, max_order, vectorize=Tr
         S_1_r = fft(S_1_c, 'C2R', inverse=True)
         S_1_r = unpad(S_1_r)
 
-        out_S_1.append(S_1_r)
+        out_S_1.append({'coef': S_1_r,
+                        'j': (j1,),
+                        'theta': (theta1,)})
 
         if max_order < 2:
             continue
         for n2 in range(len(psi)):
             j2 = psi[n2]['j']
+            theta2 = psi[n2]['theta']
+
             if j2 <= j1:
                 continue
+
             U_2_c = cdgmm(U_1_c, psi[n2][j1])
             U_2_c = subsample_fourier(U_2_c, k=2 ** (j2 - j1))
             U_2_c = fft(U_2_c, 'C2C', inverse=True)
@@ -61,7 +70,9 @@ def scattering2d(x, pad, unpad, backend, J, L, phi, psi, max_order, vectorize=Tr
             S_2_r = fft(S_2_c, 'C2R', inverse=True)
             S_2_r = unpad(S_2_r)
 
-            out_S_2.append(S_2_r)
+            out_S_2.append({'coef': S_2_r,
+                            'j': (j1, j2),
+                            'theta': (theta1, theta2)})
 
     out_S = []
     out_S.extend(out_S_0)
@@ -69,7 +80,7 @@ def scattering2d(x, pad, unpad, backend, J, L, phi, psi, max_order, vectorize=Tr
     out_S.extend(out_S_2)
 
     if vectorize:
-        out_S = concatenate(out_S)
+        out_S = concatenate([x['coef'] for x in out_S])
 
     return out_S
 
