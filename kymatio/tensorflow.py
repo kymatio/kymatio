23fe1f13c1105e699ffb63956c8858d14864f57e
Joakim Andén <janden@flatironinstitute.org>
2020-01-27 22:12:05 -0500

    ENH Make 3D frontends aliases available

--- kymatio/tensorflow.py
+++ kymatio/tensorflow.py
@@ -1,4 +1,6 @@
 from .scattering1d.frontend.tensorflow_frontend import ScatteringTensorFlow1D as Scattering1D
 from .scattering2d.frontend.tensorflow_frontend import ScatteringTensorFlow2D as Scattering2D
+from .scattering3d.frontend.tensorflow_frontend \
+        import HarmonicScatteringTensorFlow3D as HarmonicScattering3D
 
-__all__ = ['Scattering1D', 'Scattering2D']
+__all__ = ['Scattering1D', 'Scattering2D', 'HarmonicScattering3D']
