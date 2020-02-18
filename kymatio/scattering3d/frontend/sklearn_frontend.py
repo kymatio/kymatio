7e7c96e822cf108430f82736eaff348b62e06b41
Joakim Andén <janden@flatironinstitute.org>
2020-02-18 14:56:27 -0500

    REF+DOC Move sklearn frontends out of root module
    Instead, place these in the respective frontend directories of 1D, 2D
    and 3D. We also hook these frontends into the documentation template
    framework.

---
+++ kymatio/scattering3d/frontend/sklearn_frontend.py
@@ -0,0 +1,16 @@
+from kymatio.frontend.sklearn_frontend import ScatteringTransformerMixin
+
+from kymatio.numpy import HarmonicScattering3D as HarmonicScatteringNumPy3D
+
+
+# NOTE: Order in base classes matters here, since we want the sklearn-specific
+# documentation parameters to take precedence over NP.
+class HarmonicScatteringTransformer3D(ScatteringTransformerMixin,
+                                      HarmonicScatteringNumPy3D):
+    pass
+
+
+HarmonicScatteringTransformer3D._document()
+
+
+__all__ = ['HarmonicScatteringTransformer3D']
