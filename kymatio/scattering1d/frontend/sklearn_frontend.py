e40b2220585ddbfeff23e295c463636f5462fa5d
Joakim Andén <janden@flatironinstitute.org>
2020-02-18 14:56:27 -0500

    REF+DOC Move sklearn frontends out of root module
    Instead, place these in the respective frontend directories of 1D, 2D
    and 3D. We also hook these frontends into the documentation template
    framework.

---
+++ kymatio/scattering1d/frontend/sklearn_frontend.py
@@ -0,0 +1,15 @@
+from kymatio.frontend.sklearn_frontend import ScatteringTransformerMixin
+
+from kymatio.numpy import Scattering1D as ScatteringNumPy1D
+
+
+# NOTE: Order in base classes matters here, since we want the sklearn-specific
+# documentation parameters to take precedence over NP.
+class ScatteringTransformer1D(ScatteringTransformerMixin, ScatteringNumPy1D):
+    pass
+
+
+ScatteringTransformer1D._document()
+
+
+__all__ = ['ScatteringTransformer1D']
--- kymatio/sklearn.py
+++ kymatio/sklearn.py
@@ -1,45 +1,17 @@
-from sklearn.base import BaseEstimator, TransformerMixin
+from kymatio.scattering1d.frontend.sklearn_frontend \
+    import ScatteringTransformer1D as Scattering1D
+from kymatio.scattering2d.frontend.sklearn_frontend \
+    import ScatteringTransformer2D as Scattering2D
+from kymatio.scattering3d.frontend.sklearn_frontend \
+    import HarmonicScatteringTransformer3D as HarmonicScattering3D
 
-from kymatio.numpy import Scattering1D as ScatteringNumPy1D
-from kymatio.numpy import Scattering2D as ScatteringNumPy2D
-from kymatio.numpy import HarmonicScattering3D as HarmonicScatteringNumPy3D
+Scattering1D.__module__ = 'kymatio.sklearn'
+Scattering1D.__name__ = 'Scattering1D'
 
+Scattering2D.__module__ = 'kymatio.sklearn'
+Scattering2D.__name__ = 'Scattering2D'
 
-class ScatteringTransformerMixin(BaseEstimator, TransformerMixin):
-    def fit(self, x=None, y=None):
-        # No fitting necessary.
-        return self
-
-    def predict(self, x):
-        n_samples = x.shape[0]
-
-        x = x.reshape((-1,) + self.shape)
-
-        Sx = self.scattering(x)
-
-        Sx = Sx.reshape(n_samples, -1)
-
-        return Sx
-
-    transform = predict
-
-
-class ScatteringTransformer1D(ScatteringNumPy1D, ScatteringTransformerMixin):
-    pass
-
-
-class ScatteringTransformer2D(ScatteringNumPy2D, ScatteringTransformerMixin):
-    pass
-
-
-class HarmonicScatteringTransformer3D(HarmonicScatteringNumPy3D,
-                                      ScatteringTransformerMixin):
-    pass
-
-
-Scattering1D = ScatteringTransformer1D
-Scattering2D = ScatteringTransformer2D
-HarmonicScattering3D = HarmonicScatteringTransformer3D
-
+HarmonicScattering3D.__module__ = 'kymatio.sklearn'
+HarmonicScattering3D.__name__ = 'HarmonicScattering3D'
 
 __all__ = ['Scattering1D', 'Scattering2D', 'HarmonicScattering3D']
