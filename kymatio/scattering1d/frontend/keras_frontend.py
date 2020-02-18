2cb862392494b29926e4d59d851c78f9341b859d
Joakim Andén <janden@flatironinstitute.org>
2020-02-18 12:07:26 -0500

    REF+DOC Make Keras frontends inherit from base
    In addition to the Keras frontend superclass, we also inherit from the
    1D and 2D bases to get access to their documentation template. Note that
    we do not use their filter-creation functionality (since this is done by
    the wrapped `ScatteringTensorFlow*D` objects).

--- kymatio/scattering1d/frontend/keras_frontend.py
+++ kymatio/scattering1d/frontend/keras_frontend.py
@@ -1,26 +1,23 @@
+from kymatio.frontend.keras_frontend import ScatteringKeras
+from kymatio.scattering1d.frontend.base_frontend import ScatteringBase1D
+
 from kymatio.tensorflow import Scattering1D as ScatteringTensorFlow1D
 
-from tensorflow.keras.layers import Layer
 from tensorflow.python.framework import tensor_shape
 
 
-class ScatteringKeras1D(Layer):
+class ScatteringKeras1D(ScatteringKeras, ScatteringBase1D):
     def __init__(self, J, Q=1, max_order=2, oversampling=0):
-        super(ScatteringKeras1D, self).__init__()
-        self.J = J
-        self.Q = Q
-        self.max_order = max_order
-        self.oversampling = oversampling
+        ScatteringKeras.__init__(self)
+        ScatteringBase1D.__init__(self, J, None, Q, max_order, True,
+                oversampling, True, 'array', None)
 
     def build(self, input_shape):
         shape = tuple(tensor_shape.TensorShape(input_shape).as_list()[-1:])
         self.S = ScatteringTensorFlow1D(J=self.J, shape=shape,
                                         Q=self.Q, max_order=self.max_order,
                                         oversampling=self.oversampling)
-        super(ScatteringKeras1D, self).build(input_shape)
-
-    def call(self, x):
-        return self.S(x)
+        ScatteringKeras.build(self, input_shape)
 
     def compute_output_shape(self, input_shape):
         input_shape = tensor_shape.TensorShape(input_shape).as_list()
@@ -29,3 +26,6 @@ class ScatteringKeras1D(Layer):
         ln = self.S.ind_end[k0] - self.S.ind_start[k0]
         output_shape = [input_shape[0], nc, ln]
         return tensor_shape.TensorShape(output_shape)
+
+
+ScatteringKeras1D._document()
