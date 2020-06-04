a1b9107fb0b0434e0a87140219614dba812a2fe1
chaudhm <chaudhm@wwu.edu>
2020-06-04 09:11:39 -0700

    MAINT revmoed useless fft2

--- kymatio/scattering2d/utils.py
+++ kymatio/scattering2d/utils.py
@@ -22,8 +22,3 @@ def compute_padding(M, N, J):
     N_padded = ((N + 2 ** J) // 2 ** J + 1) * 2 ** J
 
     return M_padded, N_padded
-
-def fft2(x):
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', FutureWarning)
-        return scipy.fftpack.fft2(x)
