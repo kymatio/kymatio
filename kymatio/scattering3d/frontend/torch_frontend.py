7f309a481e27d20163cad0e3215767e521019f11
chaudhm <chaudhm@wwu.edu>
2020-06-09 15:29:13 -0700

    DOC removed unneeded docstrings

--- kymatio/scattering3d/frontend/torch_frontend.py
+++ kymatio/scattering3d/frontend/torch_frontend.py
@@ -8,40 +8,6 @@ from .base_frontend import ScatteringBase3D
 
 
 class HarmonicScatteringTorch3D(ScatteringTorch, ScatteringBase3D):
-    """3D Solid Harmonic scattering .
-    This class implements solid harmonic scattering on an input 3D image.
-    For details see https://arxiv.org/abs/1805.00571.
-    Instantiates and initializes a 3d solid harmonic scattering object.
-    Parameters
-    ----------
-    J: int
-        number of scales
-    shape: tuple of int
-        shape (M, N, O) of the input signal
-    L: int, optional
-        Number of l values. Defaults to 3.
-    sigma_0: float, optional
-        Bandwidth of mother wavelet. Defaults to 1.
-    max_order: int, optional
-        The maximum order of scattering coefficients to compute. Must be
-        either 1 or 2. Defaults to 2.
-    rotation_covariant: bool, optional
-        if set to True the first order moduli take the form:
-        $\\sqrt{\\sum_m (x \\star \\psi_{j,l,m})^2)}$
-        if set to False the first order moduli take the form:
-        $x \\star \\psi_{j,l,m}$
-        The second order moduli change analogously
-        Defaut: True
-    method: string, optional
-        specifies the method for obtaining scattering coefficients
-        ("standard","local","integral"). Default: "standard"
-    points: array-like, optional
-        List of locations in which to sample wavelet moduli. Used when
-        method == 'local'
-    integral_powers: array-like
-        List of exponents to the power of which moduli are raised before
-        integration. Used with method == 'standard', method == 'integral'
-    """
     def __init__(self, J, shape, L=3, sigma_0=1, max_order=2, rotation_covariant=True, method='integral', points=None,
                  integral_powers=(0.5, 1., 2.), backend='torch'):
         ScatteringTorch.__init__(self)
@@ -73,21 +39,6 @@ class HarmonicScatteringTorch3D(ScatteringTorch, ScatteringBase3D):
         self.register_buffer('tensor_gaussian_filter', self.gaussian_filters)
 
     def scattering(self, input_array):
-        """
-            The forward pass of 3D solid harmonic scattering
-            Parameters
-            ----------
-            input_array: torch tensor
-                input of size (batchsize, M, N, O)
-            Returns
-            -------
-            output: tuple | torch tensor
-                if max_order is 1 it returns a torch tensor with the
-                first order scattering coefficients
-                if max_order is 2 it returns a torch tensor with the
-                first and second order scattering coefficients,
-                concatenated along the feature axis
-            """
         if not torch.is_tensor(input_array):
             raise TypeError(
                 'The input should be a torch.cuda.FloatTensor, '
