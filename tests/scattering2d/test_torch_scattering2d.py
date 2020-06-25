9b4659b39417e153a25134aaba4cf06aaf623dd8
chaudhm <chaudhm@wwu.edu>
2020-06-25 14:29:32 -0700

    TST added FFT tests

--- tests/scattering2d/test_torch_scattering2d.py
+++ tests/scattering2d/test_torch_scattering2d.py
@@ -352,13 +352,47 @@ class TestFFT:
     def test_fft_exceptions(self, backend_device):
         backend, device = backend_device
 
+        x = torch.randn(4, 4, 2)
+        x = x.to(device)
+        with pytest.raises(TypeError) as record:
+            backend.rfft(x)
+        assert 'real' in record.value.args[0]
+
+        x = torch.randn(4, 4, 1)
+        x = x.to(device)
+        with pytest.raises(TypeError) as record:
+            backend.ifft(x)
+        assert 'complex' in record.value.args[0]
        
-        x = torch.randn(4, 4)
+        x = torch.randn(4, 4, 1)
+        x = x.to(device)
+        with pytest.raises(TypeError) as record:
+            backend.irfft(x)
+        assert 'complex' in record.value.args[0]
+       
+       
+        x = torch.randn(4, 4, 1)
         x = x.to(device)
         y = x[::2, ::2]
 
         with pytest.raises(RuntimeError) as record:
             backend.rfft(y)
+        assert 'must be contiguous' in record.value.args[0]
+        
+        x = torch.randn(4, 4, 2)
+        x = x.to(device)
+        y = x[::2, ::2]
+
+        with pytest.raises(RuntimeError) as record:
+            backend.ifft(y)
+        assert 'must be contiguous' in record.value.args[0]
+
+        x = torch.randn(4, 4, 2)
+        x = x.to(device)
+        y = x[::2, ::2]
+
+        with pytest.raises(RuntimeError) as record:
+            backend.irfft(y)
         assert 'must be contiguous' in record.value.args[0]
 
 
