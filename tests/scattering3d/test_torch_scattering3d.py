26dd8dce144e6bb9965c2999c07023f51310ff04
chaudhm <chaudhm@wwu.edu>
2020-06-25 14:35:10 -0700

    TST added fft tests

--- tests/scattering3d/test_torch_scattering3d.py
+++ tests/scattering3d/test_torch_scattering3d.py
@@ -54,12 +54,30 @@ def test_FFT3d_central_freq_batch(device, backend):
 @pytest.mark.parametrize("device", devices)
 @pytest.mark.parametrize("backend", backends)
 def test_fft3d_error(backend, device):
-    x = torch.randn(4, 4, 1)
+    x = torch.randn(1, 4, 4, 4, 2)
+    with pytest.raises(TypeError) as record:
+        backend.rfft(x)
+    assert 'real' in record.value.args[0]
+
+    x = torch.randn(1, 4, 4, 4, 1)
+    with pytest.raises(TypeError) as record:
+        backend.ifft(x)
+    assert 'complex' in record.value.args[0]
+
+    x = torch.randn(4, 4, 4, 1)
     x = x.to(device)
-    y = x[::2, ::2]
+    y = x[::2, ::2, ::2]
 
     with pytest.raises(RuntimeError) as record:
         backend.rfft(y)
+    assert 'must be contiguous' in record.value.args[0]
+
+    x = torch.randn(4, 4, 4, 2)
+    x = x.to(device)
+    y = x[::2, ::2, ::2]
+
+    with pytest.raises(RuntimeError) as record:
+        backend.ifft(y)
     assert 'must be contiguous' in record.value.args[0]
 
 
