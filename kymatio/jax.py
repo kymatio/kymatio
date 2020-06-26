1899668e8655572a25a0e35cd241a61e38ce410d
chaudhm <chaudhm@wwu.edu>
2020-06-26 10:39:49 -0700

    API Jax 3d

---
+++ kymatio/jax.py
@@ -0,0 +1,14 @@
+from .scattering1d.frontend.jax_frontend import ScatteringJax1D as Scattering1D
+from .scattering2d.frontend.jax_frontend import ScatteringJax2D as Scattering2D
+
+Scattering1D.__module__ = 'kymatio.jax'
+Scattering1D.__name__ = 'Scattering1D'
+
+
+Scattering2D.__module__ = 'kymatio.jax'
+Scattering2D.__name__ = 'Scattering2D'
+
+HarmonicScattering3D.__module__ = 'kymatio.jax'
+HarmonicScattering3D.__name__ = 'HarmonicScattering3D'
+
+__all__ = ['Scattering1D', 'Scattering2D', 'HarmonicScattering3D']
