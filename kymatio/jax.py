eba568a6d7df1991f165117eee1b3fd8d444369f
chaudhm <chaudhm@wwu.edu>
2020-06-27 13:07:57 -0700

    MAINT added import

--- kymatio/jax.py
+++ kymatio/jax.py
@@ -1,4 +1,5 @@
 from .scattering1d.frontend.jax_frontend import ScatteringJax1D as Scattering1D
+from .scattering2d.frontend.jax_frontend import ScatteringJax2D as Scattering2D
 
 Scattering1D.__module__ = 'kymatio.jax'
 Scattering1D.__name__ = 'Scattering1D'
