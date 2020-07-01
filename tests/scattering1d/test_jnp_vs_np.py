1857435a1e7084558c30ae9f356579db1d5940c0
chaudhm <chaudhm@wwu.edu>
2020-07-01 10:28:50 -0700

    TST renamed variables to be more clear

--- tests/scattering1d/test_jnp_vs_np.py
+++ tests/scattering1d/test_jnp_vs_np.py
@@ -24,225 +24,211 @@ def jnp_pad(x, pad_left, pad_right):
   
 def test_multiplication():
     # multiplication
-    a = np.random.rand(4, 1, 1024)
-    b = np.random.rand(4, 1, 1024)
-    d = np.random.rand(1024)
+    
+    real_filter = np.random.rand(1024)
+    complex_input = np.random.rand(4, 1, 1024) + 1j * np.random.rand(4, 1, 1024)
 
-    c = a + 1j*b
-    e = c*d
-    l = e
+    numpy_result = complex_input * real_filter
 
-    a = device_put(jnp.asarray(a))
-    b = device_put(jnp.asarray(b))
-    d = device_put(jnp.asarray(d))
+    real_filter = device_put(jnp.asarray(real_filter))
+    complex_input = device_put(jnp.asarray(complex_input))
 
-    c = a + 1j*b
-    e = c*d
-    assert jnp.allclose(e, l)
+    jax_result = complex_input * real_filter
+    assert np.allclose(jax_result, numpy_result)
 
 
 def test_jnp_multiplication():
     # multiplication jnp
-    a = device_put(random.normal(key, (4, 1, 1024)))
-    b = device_put(random.normal(key, (4, 1, 1024)))
-    d = device_put(random.normal(key, (1024,)))
-    c = a + 1j*b
-    e = c*d
-    l = e
-    a = np.asarray(a)
-    b = np.asarray(b)
-    d = np.asarray(d)
-    c = a + 1j*b
-    e = c*d
-    assert jnp.allclose(e, l)
+    real_filter = device_put(random.normal(key, (1024,)))
+    complex_input = device_put(random.normal(key, (4, 1, 1024)) + 1j * random.normal(key, (4, 1, 1024)))
+
+    jax_result = complex_input * real_filter
+    
+    real_filter = np.asarray(real_filter)
+    complex_input = np.asarray(complex_input)
+
+    numpy_result = complex_input * real_filter
+    assert jnp.allclose(numpy_result, jax_result)
 
 def test_real_ifft():
     #real ifft
-    a = np.random.rand(4, 1, 1024)
-    f = np.real(scipy.fftpack.ifft(a))
-    l = f
-    f = jnp.real(jnp.fft.ifft(device_put(jnp.asarray(a))))
-    assert jnp.allclose(f, l, atol=1e-6, rtol=1e-7)
+    x = np.random.rand(4, 1, 1024)
+
+    numpy_x_ifft = np.real(scipy.fftpack.ifft(x))
+
+    jax_x_ifft = jnp.real(jnp.fft.ifft(device_put(jnp.asarray(x))))
+    assert np.allclose(jax_x_ifft, numpy_x_ifft, atol=1e-6, rtol=1e-7)
 
 def test_jnp_real_ifft():
     #real ifft jnp
-    a = device_put(random.normal(key, (4, 1, 1024)))
-    f = jnp.real(jnp.fft.ifft(device_put(a)))
-    l = f
-    f = np.real(scipy.fftpack.ifft(np.asarray(a)))
-    assert jnp.allclose(f, l, atol=1e-6, rtol=1e-7)
+    x = device_put(random.normal(key, (4, 1, 1024)))
+
+    jax_x_ifft = jnp.real(jnp.fft.ifft(device_put(x)))
+
+    numpy_x_ifft = np.real(scipy.fftpack.ifft(np.asarray(x)))
+    assert jnp.allclose(numpy_x_ifft, jax_x_ifft, atol=1e-6, rtol=1e-7)
         
 def test_ifft():
     #ifft
-    a = np.random.rand(4, 1, 1024)
-    f = scipy.fftpack.ifft(a)
-    l = f
-    f = jnp.fft.ifft(device_put(jnp.asarray(a)))
-    assert jnp.allclose(f, l, atol=1e-6, rtol=1e-7)
+    x = np.random.rand(4, 1, 1024)
+
+    numpy_x_ifft = scipy.fftpack.ifft(x)
+
+    jax_x_ifft = jnp.fft.ifft(device_put(jnp.asarray(x)))
+    assert np.allclose(jax_x_ifft, numpy_x_ifft, atol=1e-6, rtol=1e-7)
 
 def test_jnp_ifft():
     #ifft jnp
-    a = device_put(random.normal(key, (4, 1, 1024)))
-    f = jnp.fft.ifft(device_put(a))
-    l = f
-    f = scipy.fftpack.ifft(np.asarray(a))
-    assert jnp.allclose(f, l, atol=1e-6, rtol=1e-7)
+    x = device_put(random.normal(key, (4, 1, 1024)))
+
+    jax_x_ifft = jnp.fft.ifft(device_put(x))
+
+    numpy_x_ifft = scipy.fftpack.ifft(np.asarray(x))
+    assert jnp.allclose(numpy_x_ifft, jax_x_ifft, atol=1e-6, rtol=1e-7)
 
 def test_fft():
     #fft
-    a = np.random.rand(4, 1, 1024)
-    b = np.random.rand(4, 1, 1024)
-    d = np.random.rand(1024)
-    c = a + 1j*b
-    e = d*c
-    f = np.fft.fft(e)
-    l = f
-    
-    a = device_put(jnp.asarray(a))
-    b = device_put(jnp.asarray(b))
-    c = a + 1j*b
-    d = device_put(jnp.asarray(d))
-    e = d*c
-    f = jnp.fft.fft(e)
-    assert jnp.allclose(f, l, atol=1e-6, rtol=1e-7)
+    real_filter = np.random.rand(1024)
+    complex_input = np.random.rand(4, 1, 1024) + 1j * np.random.rand(4, 1, 1024)
+
+    numpy_result = np.fft.fft(complex_input * real_filter)
+
+    real_filter = device_put(jnp.asarray(real_filter))
+    complex_input = device_put(jnp.asarray(complex_input))
+
+    jax_result = jnp.fft.fft(complex_input * real_filter)
+    assert np.allclose(jax_result, numpy_result, atol=1e-6, rtol=1e-4)
 
 def test_jnp_fft():
     #fft jnp
-    a = device_put(random.normal(key, (4, 1, 1024)))
-    b = device_put(random.normal(key, (4, 1, 1024)))
-    c = a + 1j*b
-    l = jnp.fft.fft(c)
-    a = np.asarray(a)
-    b = np.asarray(b)
-    c = a + 1j*b
-    f = np.fft.fft(c)
-    assert jnp.allclose(f, l, atol=1e-6, rtol=1e-7)
+    real_filter = device_put(random.normal(key, (1024,)))
+    complex_input = device_put(random.normal(key, (4, 1, 1024)) + 1j * random.normal(key, (4, 1, 1024)))
+
+    jax_result = jnp.fft.fft(complex_input * real_filter)
+    
+    real_filter = np.asarray(real_filter)
+    complex_input = np.asarray(complex_input)
+
+    numpy_result = np.fft.fft(complex_input * real_filter)
+
+    assert jnp.allclose(numpy_result, jax_result, atol=1e-6, rtol=1e-4)
 
 def test_rfft():
     #rfft
-    a = np.random.rand(4, 1, 1024)
-    l = np.fft.fft(a)
+    x = np.random.rand(4, 1, 1024)
+    numpy_result = np.fft.fft(x)
     
-    a = device_put(jnp.asarray(a))
-    f = jnp.fft.fft(a)
+    x = device_put(jnp.asarray(x))
+    jax_result = jnp.fft.fft(x)
     
-    assert jnp.allclose(f, l, atol=1e-6, rtol=1e-7)
+    assert np.allclose(jax_result, numpy_result, atol=1e-6, rtol=1e-4)
    
 def test_jnp_rfft():
     #rfft jnp
-    a = device_put(random.normal(key, (4, 1, 1024)))
-    f = jnp.fft.fft(a)
+    x = device_put(random.normal(key, (4, 1, 1024)))
+    jax_result = jnp.fft.fft(x)
     
-    a = np.asarray(a)
-    l = np.fft.fft(a)
+    x = np.asarray(x)
+    numpy_result = np.fft.fft(x)
 
-    assert jnp.allclose(f, l, atol=1e-6, rtol=1e-7)
+    assert jnp.allclose(numpy_result, jax_result, atol=1e-6, rtol=1e-4)
 
     
 def test_absolute():
     #absolute 
-    a = np.random.rand(4, 1, 1024)
-    b = np.random.rand(4, 1, 1024)
-    c = a + 1j*b
-    l = np.absolute(c)
-    a = device_put(jnp.asarray(a))
-    b = device_put(jnp.asarray(b))
-    c = a + 1j*b
-    f = jnp.absolute(c)
-    assert jnp.allclose(f, l)
+    complex_input = np.random.rand(4, 1, 1024) + 1j * np.random.rand(4, 1, 1024)
+    numpy_result = np.absolute(complex_input)
+
+    complex_input = device_put(jnp.asarray(complex_input))
+    jax_result = jnp.absolute(complex_input)
+    assert np.allclose(jax_result, numpy_result)
     
 def test_jnp_absolute():
     #absolute jnp
-    a = device_put(random.normal(key, (4, 1, 1024)))
-    b = device_put(random.normal(key, (4, 1, 1024)))
-    c = a + 1j*b
-    l = jnp.absolute(c)
-    a = np.asarray(a)
-    b = np.asarray(b)
-    c = a + 1j*b
-    f = np.absolute(c)
-    assert jnp.allclose(f, l)
+    complex_input = device_put(random.normal(key, (4, 1, 1024)) + 1j * random.normal(key, (4, 1, 1024)))
+    jax_result = jnp.absolute(complex_input)
+
+    complex_input = np.asarray(complex_input)
+    numpy_result = np.absolute(complex_input)
+    assert jnp.allclose(numpy_result, jax_result)
 
 def test_real():
     #real
-    a = np.random.rand(4, 1, 1024)
-    b = np.random.rand(4, 1, 1024)
-    c = a + 1j*b
-    l = jnp.real(c)
-    a = device_put(jnp.asarray(a))
-    b = device_put(jnp.asarray(b))
-    c = a + 1j*b
-    f = jnp.real(c)
-    assert jnp.allclose(f, l)
+    complex_input = np.random.rand(4, 1, 1024) + 1j * np.random.rand(4, 1, 1024)
+    numpy_result = jnp.real(complex_input)
+
+    complex_input = device_put(jnp.asarray(complex_input))
+    jax_result = jnp.real(complex_input)
+    assert np.allclose(jax_result, numpy_result)
 
 def test_jnp_real():
     #real jnp
-    a = device_put(random.normal(key, (4, 1, 1024)))
-    b = device_put(random.normal(key, (4, 1, 1024)))
-    c = a + 1j*b
-    l = jnp.real(c)
-    a = np.asarray(a)
-    b = np.asarray(b)
-    c = a + 1j*b
-    f = np.real(c)
-    assert jnp.allclose(f, l)
+    complex_input = device_put(random.normal(key, (4, 1, 1024)) + 1j * random.normal(key, (4, 1, 1024)))
+    jax_result = jnp.real(complex_input)
+
+    complex_input = np.asarray(complex_input)
+    numpy_result = np.real(complex_input)
+    assert jnp.allclose(numpy_result, jax_result)
     
 def test_subsample_fourier():
     #subsample fourier
-    a = np.random.rand(4, 1, 1024)
-    b = device_put(jnp.asarray(a))
-    for i in [1, 2, 4]:
-        c = subsample_fourier(a, i)
-        d = subsample_fourier(a, i)
-        assert jnp.allclose(c, d)
+    x = np.random.rand(4, 1, 1024)
+    x_jax = device_put(jnp.asarray(x))
 
-def test_jnp_subsample_fourier():       
-    #subsample fourier jnp
-    a = np.random.rand(4, 1, 1024)
-    b = device_put(jnp.asarray(a))
-    for i in [1, 2, 4]:
-        c = subsample_fourier(a, i)
-        d = subsample_fourier(a, i)
-        assert jnp.allclose(c, d)
+    for subsampling_factor in [1, 2, 4]:
+        numpy_result = subsample_fourier(x, subsampling_factor)
+        jax_result = subsample_fourier(x_jax, subsampling_factor)
+        assert np.allclose(jax_result, numpy_result)
+        assert jnp.allclose(numpy_result, jax_result)
+
 
 def test_pad_unpad():       
     #pad
-    a = np.random.rand(4, 1, 512)
-    b = np_pad(a, 256, 256)
-    c = device_put(jnp.asarray(a))
-    d = jnp_pad(c, 256, 256)
-    assert jnp.allclose(b, d)
-    b = b[..., 4: 12]
-    d = d[..., 4: 12]
-    assert jnp.allclose(b, d)
+    x = np.random.rand(4, 1, 512)
+    numpy_result = np_pad(x, 256, 256)
+
+    x = device_put(jnp.asarray(x))
+    jax_result = jnp_pad(x, 256, 256)
+
+    assert np.allclose(jax_result, numpy_result)
+
+    numpy_result = numpy_result[..., 4: 12]
+    jax_result = jax_result[..., 4: 12]
+    assert np.allclose(jax_result, numpy_result)
 
 def test_jnp_pad_unpad():       
     #pad jnp
-    a = device_put(random.normal(key, (4, 1, 512)))
-    b = jnp_pad(a, 256, 256)
-    c = np.asarray(a)
-    d = np_pad(c, 256, 256)
-    assert jnp.allclose(b, d)
-    b = b[..., 4: 12]
-    d = d[..., 4: 12]
-    assert jnp.allclose(b, d)
+    x = device_put(random.normal(key, (4, 1, 512)))
+    jax_result = jnp_pad(x, 256, 256)
+
+    x = np.asarray(x)
+    numpy_result = np_pad(x, 256, 256)
+    assert jnp.allclose(numpy_result, jax_result)
+
+    jax_result = jax_result[..., 4 : 12]
+    numpy_result = numpy_result[..., 4 : 12]
+    assert jnp.allclose(numpy_result, jax_result)
     
 def test_stack():       
     #stack
-    a = np.random.rand(4, 1, 512)
-    b = np.random.rand(4, 1, 512)
-    c = np.stack([a, b], axis=-2)
-    d = device_put(jnp.asarray(a))
-    e = device_put(jnp.asarray(b))
-    f = jnp.stack([d, e], axis=-2)
-    assert jnp.allclose(c, f)
+    array_1 = np.random.rand(4, 1, 512)
+    array_2 = np.random.rand(4, 1, 512)
+    numpy_result = np.stack([array_1, array_2], axis=-2)
+
+    array_1 = device_put(jnp.asarray(array_1))
+    array_2 = device_put(jnp.asarray(array_2))
+    jax_result = jnp.stack([array_1, array_2], axis=-2)
+
+    assert np.allclose(jax_result, numpy_result)
     
 def test_jnp_stack():       
     #stack jnp 
-    a = device_put(random.normal(key, (4, 1, 512)))
-    b = device_put(random.normal(key, (4, 1, 512)))
-    c = jnp.stack([a, b], axis=-2)
-    a = np.asarray(a)
-    b = np.asarray(b)
-    f = np.stack([a, b], axis=-2)
-    assert jnp.allclose(c, f)
+    array_1 = device_put(random.normal(key, (4, 1, 512)))
+    array_2 = device_put(random.normal(key, (4, 1, 512)))
+    jax_result = jnp.stack([array_1, array_2], axis=-2)
+
+    array_1 = np.asarray(array_1)
+    array_2 = np.asarray(array_2)
+    numpy_result = np.stack([array_1, array_2], axis=-2)
+
+    assert jnp.allclose(numpy_result, jax_result)
