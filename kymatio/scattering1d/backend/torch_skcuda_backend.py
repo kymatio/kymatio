5694246da4a8b4a4bb1f3c9492fe378688cd29df
Joakim Andén <janden@kth.se>
2021-01-13 12:41:21 +0100

    BUG Ensure compatibility with cupy-8.0
    Need to access utils with `cupy._util` instead of `cupy.util`.

--- kymatio/scattering1d/backend/torch_skcuda_backend.py
+++ kymatio/scattering1d/backend/torch_skcuda_backend.py
@@ -6,7 +6,13 @@ from ...backend.torch_backend import contiguous_check, complex_check
 
 BACKEND_NAME = 'torch_skcuda'
 
-@cupy.util.memoize(for_each_device=True)
+# As of v8, cupy.util has been renamed cupy._util.
+if hasattr(cupy, '_util'):
+    memoize = cupy._util.memoize
+else:
+    memoize = cupy.util.memoize
+
+@memoize(for_each_device=True)
 def load_kernel(kernel_name, code, **kwargs):
     code = Template(code).substitute(**kwargs)
     kernel_code = cupy.cuda.compile_with_cache(code)
