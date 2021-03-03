4a46f267cc06a7d68759d2132f7495c655efe4c7
Philip Warrick <philip.warrick>
2021-03-03 14:58:50 -0500

    clean up test_Scattering1D_filter_factory_T

--- tests/scattering1d/test_numpy_scattering1d.py
+++ tests/scattering1d/test_numpy_scattering1d.py
@@ -68,19 +68,29 @@ class TestScattering1DNumpy:
         #print('Sx1 shape: ' + str(Sx1.shape) + ' Sx2.shape: ' + str(Sx2.shape))
         assert Sx2.shape == (Sx1.shape[0], Sx1.shape[1], Sx1.shape[2]*2**(sigma_low_scale_factor))
     
+    def Scattering1D_filter_factory_T_plot(self, N, J, T, phi_f, psi1_f, msg):
+          import matplotlib.pyplot as plt
+          plot_dir = 'plots'
+          if not os.path.isdir(plot_dir):
+              os.mkdir(plot_dir)    
+
+          plt.figure()
+          plt.plot(np.arange(N)/N, phi_f[0], 'r', label=msg)
+          for psi_f in psi1_f:
+              plt.plot(np.arange(N)/N, psi_f[0], 'b')
+          plt.xlim(0, 0.5)
+          plt.xlabel(r'$\omega$', fontsize=18)
+          plt.ylabel(r'$\hat\psi_j(\omega)$', fontsize=18)
+          
+          plt.legend()
+          plt.savefig(plot_dir + '/order1filters_T' + str(T) + 'J' + str(J) +  '.pdf', bbox_inches='tight', orientation='landscape')
+    
     def test_Scattering1D_filter_factory_T(self, backend):
         """
         Constructs the scattering filters for the T parameter which controls the
         temporal extent of the low-pass sigma_log filter
         """
-        
         DO_PLOTS = False
-        if DO_PLOTS:
-          import matplotlib.pyplot as plt
-          plot_dir = 'plots'
-          if not os.path.isdir(plot_dir):
-              os.mkdir(plot_dir)    
-            
         N = 2**13
         Q = 1       
         sigma_low_scale_factor = [0, 5]        
@@ -95,22 +105,12 @@ class TestScattering1DNumpy:
                 else: 
                     default_str = ''
                 phi_f, psi1_f, psi2_f, _ = scattering_filter_factory(np.log2(N), J, Q, T)
-                msg = 'J=' + str(J) + ', T=' +str(T) + default_str + ': LP-filter width $\sigma_{low}$=' + str(phi_f['sigma']) + ' (=0.1/T)'                
+                #msg = 'J=' + str(J) + ', T=' +str(T) + default_str + ': LP-filter width $\sigma_{low}$=' + str(phi_f['sigma']) + ' (=0.1/T)'                
                 #print(msg)
                 assert(phi_f['sigma']==0.1/T)
 
                 if DO_PLOTS:
-                  plt.figure()
-                  h = plt.plot(np.arange(N)/N, phi_f[0], 'r', label=msg)
-                  for psi_f in psi1_f:
-                      plt.plot(np.arange(N)/N, psi_f[0], 'b')
-                  plt.xlim(0, 0.5)
-                  plt.xlabel(r'$\omega$', fontsize=18)
-                  plt.ylabel(r'$\hat\psi_j(\omega)$', fontsize=18)
-                  plt.title('First-order filters (Q = {})'.format(Q), fontsize=18)       
-                  
-                  plt.legend()
-                  plt.savefig(plot_dir + '/order1filters_T' + str(T) + 'J' + str(J) +  '.pdf', bbox_inches='tight', orientation='landscape')
+                    self.Scattering1D_filter_factory_T_plot(N, J, T, phi_f, psi1_f, msg)
                   
 if __name__ == '__main__':
   test = TestScattering1DNumpy()
