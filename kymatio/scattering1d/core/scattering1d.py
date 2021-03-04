1e212af273bfecfed901abc4250e259ed3e2c397
Philip Warrick <philip.warrick>
2021-03-04 13:19:44 -0500

    Distinguished sigma_low and sigma_min

--- kymatio/scattering1d/core/scattering1d.py
+++ kymatio/scattering1d/core/scattering1d.py
@@ -10,7 +10,7 @@ def scattering1d(x, pad, unpad, backend, J, T, psi1, psi2, phi, pad_left=0,
     Parameters
     ----------
     x : Tensor
-        a torch Tensor of size `(B, 1, T)` where `T` is the temporal size
+        a torch Tensor of size `(B, 1, N)` where `N` is the temporal size
     psi1 : dictionary
         a dictionary of filters (in the Fourier domain), with keys (`j`, `q`).
         `j` corresponds to the downsampling factor for
@@ -77,11 +77,10 @@ def scattering1d(x, pad, unpad, backend, J, T, psi1, psi2, phi, pad_left=0,
     # compute the Fourier transform
     U_0_hat = rfft(U_0)
 
-    #T = 2**(J-5)
-    T_log2 = math.floor(math.log2(T))
+    log2_T = math.floor(math.log2(T))
     
     # Get S0
-    k0 = max(T_log2 - oversampling, 0)
+    k0 = max(log2_T - oversampling, 0)
 
     if average:
         S_0_c = cdgmm(U_0_hat, phi[0])
@@ -100,8 +99,7 @@ def scattering1d(x, pad, unpad, backend, J, T, psi1, psi2, phi, pad_left=0,
         # Convolution + downsampling
         j1 = psi1[n1]['j']
 
-        #k1 = max(j1 - oversampling, T_log2 - oversampling)
-        k1 = min(max(j1 - oversampling, 0), T_log2 - oversampling)
+        k1 = min(max(j1 - oversampling, 0), log2_T - oversampling)
 
         assert psi1[n1]['xi'] < 0.5 / (2**k1)
         U_1_c = cdgmm(U_0_hat, psi1[n1][0])
@@ -116,7 +114,7 @@ def scattering1d(x, pad, unpad, backend, J, T, psi1, psi2, phi, pad_left=0,
 
         if average:
             # Convolve with phi_J
-            k1_J = max(T_log2 - k1 - oversampling, 0)
+            k1_J = max(log2_T - k1 - oversampling, 0)
             S_1_c = cdgmm(U_1_hat, phi[k1])
             S_1_hat = subsample_fourier(S_1_c, 2**k1_J)
             S_1_r = irfft(S_1_hat)
@@ -138,9 +136,7 @@ def scattering1d(x, pad, unpad, backend, J, T, psi1, psi2, phi, pad_left=0,
                     assert psi2[n2]['xi'] < psi1[n1]['xi']
 
                     # convolution + downsampling
-                    #k2 = max(j2 - k1 - oversampling, T_log2 - k1 -
-                    #        oversampling)
-                    k2 = min(max(j2 - k1 - oversampling, 0), T_log2 - k1 - oversampling)
+                    k2 = min(max(j2 - k1 - oversampling, 0), log2_T - k1 - oversampling)
 
                     U_2_c = cdgmm(U_1_hat, psi2[n2][k1])
                     U_2_hat = subsample_fourier(U_2_c, 2**k2)
@@ -153,7 +149,7 @@ def scattering1d(x, pad, unpad, backend, J, T, psi1, psi2, phi, pad_left=0,
                         U_2_hat = rfft(U_2_m)
 
                         # Convolve with phi_J
-                        k2_J = max(T_log2 - k2 - k1 - oversampling, 0)
+                        k2_J = max(log2_T - k2 - k1 - oversampling, 0)
 
                         S_2_c = cdgmm(U_2_hat, phi[k1 + k2])
                         S_2_hat = subsample_fourier(S_2_c, 2**k2_J)
