4272350c0d50517b930a5da642834878ec6356c5
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2021-03-04 18:02:29 +0100

    define class class TimeFrequencyScatteringEntry

--- kymatio/scattering1d/frontend/entry.py
+++ kymatio/scattering1d/frontend/entry.py
@@ -4,5 +4,10 @@ class ScatteringEntry1D(ScatteringEntry):
     def __init__(self, *args, **kwargs):
         super().__init__(name='1D', class_name='scattering1d', *args, **kwargs)
 
+class TimeFrequencyScatteringEntry(ScatteringEntry):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            name='time-frequency', class_name='timefrequency_scattering',
+            *args, **kwargs)
 
-__all__ = ['ScatteringEntry1D']
+__all__ = ['ScatteringEntry1D', 'TimeFrequencyScatteringEntry']
