d9850ae2f95d5099806d6063694523881da8bfd7
Joakim Andén <janden@kth.se>
2021-03-16 14:31:47 +0100

    TST Add GHA workflow to compiling docs and uploading
    This will run `make html` in the `doc` directory and upload the result
    to GitHub pages, which will then auto-deploy to https://www.kymat.io.
    For now, this only happens on pushes to `master`.

--- tools/push_doc.sh
+++ tools/push_doc.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
 
-# This script is adapted from the one in scikit-learn
-
-# This script is meant to be called in the "deploy" step defined in 
-# circle.yml. See https://circleci.com/docs/ for more details.
-# The behavior of the script is controlled by environment variable defined
-# in the circle.yml in the top level folder of the project.
-
 set -ex
 
-if [ -z $CIRCLE_PROJECT_USERNAME ];
-then USERNAME="kymatio-ci";
-else USERNAME=$CIRCLE_PROJECT_USERNAME;
-fi
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-DOC_REPO=${2-"kymatio.github.io"}
-GENERATED_DOC_DIR=$1
+repo_root=$(mktemp -d)
+rsync -av "${DOC_ROOT}/" "${repo_root}/"
 
-if [[ -z "$GENERATED_DOC_DIR" ]]; then
-    echo "Need to pass directory of the generated doc as argument"
-    echo "Usage: $0 <generated_doc_dir>"
-    exit 1
-fi
+pushd "${repo_root}"
 
-# Absolute path needed because we use cd further down in this script
-GENERATED_DOC_DIR=$(readlink -f $GENERATED_DOC_DIR)
+git init
+git remote add deploy "https://token:${TOKEN}@github.com/${DOC_REPO}.git"
+git checkout -b gh-pages
 
+touch .nojekyll
 
-dir=dev
+git add .
 
-MSG="Pushing the docs to $dir/ for branch: $CIRCLE_BRANCH, commit $CIRCLE_SHA1"
+msg="Pushing the docs for commit ${GITHUB_SHA} made on from ${GITHUB_REF} by ${GITHUB_ACTOR}"
 
-cd $HOME
-if [ ! -d $DOC_REPO ];
-then git clone --depth 1 "git@github.com:kymatio/"$DOC_REPO".git";
-fi
-cd $DOC_REPO
+git commit -am "${msg}"
 
-cp -R $GENERATED_DOC_DIR/* .
-git config user.email "eugenium+kymatio-ci@gmail.com"
-git config user.name $USERNAME
-git config push.default matching
-git add --all
-git commit -m "$MSG"
-git push
-echo $MSG 
+git push deploy gh-pages --force
+
+popd
+
+exit 0
