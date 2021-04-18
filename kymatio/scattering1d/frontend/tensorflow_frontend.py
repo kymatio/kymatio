13d8c72f6b64db7932a048e90bddf2340621cad2
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2021-04-18 22:32:00 +0400

    frontends

--- kymatio/scattering1d/frontend/tensorflow_frontend.py
+++ kymatio/scattering1d/frontend/tensorflow_frontend.py
@@ -8,11 +8,11 @@ from .base_frontend import ScatteringBase1D
 
 
 class ScatteringTensorFlow1D(ScatteringTensorFlow, ScatteringBase1D):
-    def __init__(self, J, shape, Q=1, max_order=2, average=True,
+    def __init__(self, J, shape, Q=1, Q2=1, max_order=2, average=True,
             oversampling=0, vectorize=True, out_type='array', backend='tensorflow',
                  name='Scattering1D'):
         ScatteringTensorFlow.__init__(self, name=name)
-        ScatteringBase1D.__init__(self, J, shape, Q, max_order, average,
+        ScatteringBase1D.__init__(self, J, shape, Q, Q2, max_order, average,
                 oversampling, vectorize, out_type, backend)
         ScatteringBase1D._instantiate_backend(self, 'kymatio.scattering1d.backend.')
         ScatteringBase1D.build(self)
@@ -49,7 +49,7 @@ class ScatteringTensorFlow1D(ScatteringTensorFlow, ScatteringBase1D):
         # treat the arguments
         if self.vectorize:
             size_scattering = precompute_size_scattering(
-                self.J, self.Q, max_order=self.max_order, detail=True)
+                self.J, self.Q, Q2=self.Q2, max_order=self.max_order, detail=True)
         else:
             size_scattering = 0
 
