1c2299c29b47e9f46eb14ded018bf5a538aa6947
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2021-05-06 23:05:49 +0400

    "support" TensorFlow

--- kymatio/scattering1d/backend/agnostic_backend.py
+++ kymatio/scattering1d/backend/agnostic_backend.py
@@ -1,10 +1,6 @@
-import numpy as np
-import torch
-
 
 def pad(x, pad_left, pad_right, pad_mode='reflect', backend_name='numpy'):
     """Backend-agnostic padding.
-
     Parameters
     ----------
     x : input tensor
@@ -19,37 +15,49 @@ def pad(x, pad_left, pad_right, pad_mode='reflect', backend_name='numpy'):
             - reflect: [3,4,3,2, 1,2,3,4, 3,2,1]
     backend_name : str
         One of: numpy, torch, tensorflow
-
     Returns
     -------
     out : type(x)
         Padded tensor.
     """
     if backend_name == 'numpy':
+        import numpy as np
         backend = np
         kw = dict(dtype=x.dtype)
     elif backend_name == 'torch':
+        import torch
         backend = torch
-        kw = dict(dtype=x.dtype, device=x.device)
+        kw = dict(dtype=x.dtype, layout=x.layout, device=x.device)
+    elif backend_name == 'tensorflow':
+        import tensorflow as tf
+        backend = tf
+        kw = dict(dtype=x.dtype)
 
     N = x.shape[-1]
     out = backend.zeros(x.shape[:-1] + (N + pad_left + pad_right,), **kw)
+    if backend_name == 'tensorflow':  # TODO
+        out = out.numpy()
     out[..., pad_left:-pad_right] = x
 
     if pad_mode == 'zero':
         out[..., :pad_left] = 0
         out[..., -pad_right:] = 0
     elif pad_mode == 'reflect':
-        xflip = _flip(x, backend_name)
+        xflip = _flip(x, backend, backend_name)
         out = _pad_reflect(x, xflip, out, pad_left, pad_right, N)
+
+    if backend_name == 'tensorflow':  # TODO
+        out = tf.constant(out)
     return out
 
 
-def _flip(x, backend_name='numpy'):
+def _flip(x, backend, backend_name='numpy'):
     if backend_name == 'numpy':
         return x[..., ::-1]
     elif backend_name == 'torch':
-        return torch.flip(x, (x.ndim - 1,))
+        return backend.flip(x, (x.ndim - 1,))
+    elif backend_name == 'tensorflow':
+        return x[..., ::-1]
 
 
 def _pad_reflect(x, xflip, out, pad_left, pad_right, N):
