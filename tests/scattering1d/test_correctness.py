695b0e0ccacb48b7d9af3e8cebf615f1a0e427bb
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2021-05-06 23:07:09 +0400

    TF pad test

--- tests/scattering1d/test_correctness.py
+++ tests/scattering1d/test_correctness.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 import scipy.signal
+import warnings
 from kymatio.numpy import Scattering1D
 from kymatio.scattering1d.backend.agnostic_backend import pad
 
@@ -49,14 +50,22 @@ def test_T():
 
 
 def _test_padding(backend_name):
+    def _arange(N):
+        if backend_name == 'tensorflow':
+            return backend.range(N)
+        return backend.arange(N)
+
     if backend_name == 'numpy':
         backend = np
     elif backend_name == 'torch':
         import torch
         backend = torch
+    elif backend_name == 'tensorflow':
+        import tensorflow as tf
+        backend = tf
 
     for N in (128, 129):  # even, odd
-        x = backend.arange(6 * N).reshape(2, 3, N)
+        x = backend.reshape(_arange(6 * N), (2, 3, N))
         for pad_factor in (1, 2, 3, 4):
             pad_left = (N // 2) * pad_factor
             pad_right = int(np.ceil(N / 4) * pad_factor)
@@ -68,9 +77,8 @@ def _test_padding(backend_name):
                               [[0, 0]] * (x.ndim - 1) + [[pad_left, pad_right]],
                               mode=pad_mode if pad_mode != 'zero' else 'constant')
 
-                if backend_name == 'torch':
-                    out1 = torch.from_numpy(out1)
-                assert backend.allclose(out0, out1), (
+                out0 = out0.numpy() if hasattr(out0, 'numpy') else out0
+                assert np.allclose(out0, out1), (
                     "{} | (N, pad_mode, pad_left, pad_right) = ({}, {}, {}, {})"
                     ).format(backend_name, N, pad_mode, pad_left, pad_right)
 
@@ -83,8 +91,18 @@ def test_pad_torch():
     try:
         import torch
     except ImportError:
+        warnings.warn("Failed to import torch")
         return
     _test_padding('torch')
+
+
+def test_pad_tensorflow():
+    try:
+        import tensorflow
+    except ImportError:
+        warnings.warn("Failed to import tensorflow")
+        return
+    _test_padding('tensorflow')
 
 
 def _l2(x):
@@ -102,5 +120,6 @@ if __name__ == '__main__':
         test_T()
         test_pad_numpy()
         test_pad_torch()
+        test_pad_tensorflow()
     else:
         pytest.main([__file__, "-s"])
