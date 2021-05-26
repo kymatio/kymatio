c5a8a86f04a397171577d996259c03b1b3d98dea
Philip Warrick <philip.warrick@perigen.com>
2021-05-26 18:07:02 -0400

    Removed extraneous

--- tests/scattering1d/test_keras_scattering1d.py
+++ tests/scattering1d/test_keras_scattering1d.py
@@ -15,46 +15,30 @@ class TestScattering1DKeras:
         a previously calculated version.
         """
         test_data_dir = os.path.dirname(__file__)
-
         with open(os.path.join(test_data_dir, 'test_data_1d.npz'), 'rb') as f:
             buffer = io.BytesIO(f.read())
             data = np.load(buffer)
-
         x = data['x']
         J = data['J']
         Q = data['Q']
         Sx0 = data['Sx']
-
         # default
         inputs0 = Input(shape=(x.shape[-1]))
         scat0 = Scattering1D(J=J, Q=Q)(inputs0)
-
         model0 = Model(inputs0, scat0)
         model0.compile(optimizer='adam',
                       loss='sparse_categorical_crossentropy',
                       metrics=['accuracy'])
         Sg0 = model0.predict(x)
         assert np.allclose(Sg0, Sx0)
-
         # adjust T
         sigma_low_scale_factor = 2
         T=2**(J-sigma_low_scale_factor)
-
         inputs1 = Input(shape=(x.shape[-1]))
         scat1 = Scattering1D(J=J, Q=Q, T=T)(inputs1)
-
         model1 = Model(inputs1, scat1)
         model1.compile(optimizer='adam',
                       loss='sparse_categorical_crossentropy',
                       metrics=['accuracy'])
         Sg1 = model1.predict(x)
-
-        # for now, just be sure that the output shape is different from default
-        # should we add this new result to the test data?
-        #print('Sx1 shape: ' + str(Sx1.shape) + ' Sx2.shape: ' + str(Sx2.shape))
         assert Sg1.shape == (Sg0.shape[0], Sg0.shape[1], Sg0.shape[2]*2**(sigma_low_scale_factor))
-
-
-if __name__ == '__main__':
-  test = TestScattering1DKeras()
-  test.test_Scattering1D_T()
