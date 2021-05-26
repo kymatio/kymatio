ea83643c8f80f86d6f88760ca58df4ee245b7c0a
Philip Warrick <philip.warrick@perigen.com>
2021-05-26 18:08:38 -0400

    class to function

--- tests/scattering1d/test_keras_scattering1d.py
+++ tests/scattering1d/test_keras_scattering1d.py
@@ -6,39 +6,36 @@ import os
 import numpy as np
 import io
 
-backends = []
-
-class TestScattering1DKeras:
-    def test_Scattering1D(self):
-        """
-        Applies scattering on a stored signal to make sure its output agrees with
-        a previously calculated version.
-        """
-        test_data_dir = os.path.dirname(__file__)
-        with open(os.path.join(test_data_dir, 'test_data_1d.npz'), 'rb') as f:
-            buffer = io.BytesIO(f.read())
-            data = np.load(buffer)
-        x = data['x']
-        J = data['J']
-        Q = data['Q']
-        Sx0 = data['Sx']
-        # default
-        inputs0 = Input(shape=(x.shape[-1]))
-        scat0 = Scattering1D(J=J, Q=Q)(inputs0)
-        model0 = Model(inputs0, scat0)
-        model0.compile(optimizer='adam',
-                      loss='sparse_categorical_crossentropy',
-                      metrics=['accuracy'])
-        Sg0 = model0.predict(x)
-        assert np.allclose(Sg0, Sx0)
-        # adjust T
-        sigma_low_scale_factor = 2
-        T=2**(J-sigma_low_scale_factor)
-        inputs1 = Input(shape=(x.shape[-1]))
-        scat1 = Scattering1D(J=J, Q=Q, T=T)(inputs1)
-        model1 = Model(inputs1, scat1)
-        model1.compile(optimizer='adam',
-                      loss='sparse_categorical_crossentropy',
-                      metrics=['accuracy'])
-        Sg1 = model1.predict(x)
-        assert Sg1.shape == (Sg0.shape[0], Sg0.shape[1], Sg0.shape[2]*2**(sigma_low_scale_factor))
+def test_Scattering1D(self):
+  """
+  Applies scattering on a stored signal to make sure its output agrees with
+  a previously calculated version.
+  """
+  test_data_dir = os.path.dirname(__file__)
+  with open(os.path.join(test_data_dir, 'test_data_1d.npz'), 'rb') as f:
+      buffer = io.BytesIO(f.read())
+      data = np.load(buffer)
+  x = data['x']
+  J = data['J']
+  Q = data['Q']
+  Sx0 = data['Sx']
+  # default
+  inputs0 = Input(shape=(x.shape[-1]))
+  scat0 = Scattering1D(J=J, Q=Q)(inputs0)
+  model0 = Model(inputs0, scat0)
+  model0.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+  Sg0 = model0.predict(x)
+  assert np.allclose(Sg0, Sx0)
+  # adjust T
+  sigma_low_scale_factor = 2
+  T=2**(J-sigma_low_scale_factor)
+  inputs1 = Input(shape=(x.shape[-1]))
+  scat1 = Scattering1D(J=J, Q=Q, T=T)(inputs1)
+  model1 = Model(inputs1, scat1)
+  model1.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+  Sg1 = model1.predict(x)
+  assert Sg1.shape == (Sg0.shape[0], Sg0.shape[1], Sg0.shape[2]*2**(sigma_low_scale_factor))
