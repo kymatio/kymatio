335b4c2c5e9883894d6d5a862dca73d892851db6
Michael Eickenberg <michael.eickenberg@gmail.com>
2021-06-29 20:53:10 -0400

    TST test for jax 1d backend

---
+++ tests/scattering1d/test_jax_backend_1d.py
@@ -0,0 +1,36 @@
+import pytest
+from kymatio import Scattering1D
+import os
+import numpy
+import jax.numpy as np
+from jax import device_put
+import io
+
+backends = []
+
+from kymatio.scattering1d.backend.jax_backend import backend
+backends.append(backend)
+
+@pytest.mark.parametrize('backend', backends)
+def test_Scattering1D(backend):
+    """
+    Applies scattering on a stored signal to make sure its output agrees with
+    a previously calculated version.
+    """
+    test_data_dir = os.path.dirname(__file__)
+
+    with open(os.path.join(test_data_dir, 'test_data_1d.npz'), 'rb') as f:
+        buffer = io.BytesIO(f.read())
+        data = numpy.load(buffer)
+
+    x = device_put(np.asarray(data['x']))
+    J = data['J']
+    Q = data['Q']
+    Sx0 = device_put(np.asarray(data['Sx']))
+
+    T = x.shape[-1]
+    scattering = Scattering1D(J, T, Q, backend=backend, frontend='jax')
+
+    Sx = scattering(x) 
+
+    assert np.allclose(Sx, Sx0, atol=1e-6, rtol=1e-7)
