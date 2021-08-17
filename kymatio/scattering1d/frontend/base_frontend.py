global averaging fixes

kymatio/scattering1d/frontend/base_frontend.py
@@ -83,7 +83,8 @@ class ScatteringBase1D(ScatteringBase):
                              "cannot exceed input length (got {} > {})".format(
                                  self.T, self.N))
         self.log2_T = math.floor(math.log2(self.T))
-        self.average_global = bool(self.T == mx)
+        self.average_global_phi = bool(self.T == mx)
+        self.average_global = bool(self.average_global_phi and self.average)
 
         # Compute the minimum support to pad (ideally)
         min_to_pad, pad_phi, pad_psi1, pad_psi2 = compute_minimum_support_to_pad(
@@ -231,6 +232,19 @@ class ScatteringBase1D(ScatteringBase):
             Will pad by at most `2**max_pad_factor` relative to `nextpow2(shape)`.
             E.g. if input length is 150, then maximum padding with
             `max_pad_factor=2` is `256 * (2**2) = 1024`.
+        average_global_phi : bool
+            True if `T == nextpow2(shape)`, i.e. `T` is maximum possible
+            and equivalent to global averaging, in which case lowpassing is
+            replaced by simple arithmetic mean.
+
+            In case of `average==False`, controls scattering logic for
+            `phi_t` pairs in JTFS.
+        average_global : bool
+            True if `average_global_phi and average_fr`. Same as
+            `average_global_phi` if `average_fr==True`.
+
+            In case of `average==False`, controls scattering logic for
+            `psi_t` pairs in JTFS.
         """
 
     _doc_attr_vectorize = \
@@ -541,7 +555,7 @@ class TimeFrequencyScatteringBase1D():
             # F is processed further in `_FrequencyScatteringBase`
             self.F = self.Q[0]
 
-        # basic params + frequential scattering object
+        # frequential scattering object ######################################
         self._shape_fr = self.get_shape_fr()
         # number of psi1 filters
         self._n_psi1_f = len(self.psi1_f)
@@ -629,8 +643,8 @@ class TimeFrequencyScatteringBase1D():
                                  self.T, self.F, self.aligned, self.out_3D,
                                  self.out_type, self.out_exclude,
                                  self.sampling_filters_fr, self.average,
-                                 self.average_global, self.oversampling,
-                                 self.r_psi, self.sc_freq)
+                                 self.average_global, self.average_global_phi,
+                                 self.oversampling, self.r_psi, self.sc_freq)
 
     @property
     def fr_attributes(self):
@@ -1123,12 +1137,22 @@ class TimeFrequencyScatteringBase1D():
     psi1_f_fr_down : list[dict]
         `psi1_f_fr_up`, but with "down" spin, forming a complementary pair.
 
-    average_fr_global : bool
+    average_fr_global_phi : bool
         True if `F == nextpow2(shape_fr_max)`, i.e. `F` is maximum possible
         and equivalent to global averaging, in which case lowpassing is replaced
         by simple arithmetic mean.
 
         If True, `sampling_phi_fr` has no effect.
+
+        In case of `average_fr==False`, controls scattering logic for
+        `phi_f` pairs.
+
+    average_fr_global : bool
+        True if `average_fr_global_phi and average_fr`. Same as
+        `average_fr_global_phi` if `average_fr==True`.
+
+        In case of `average_fr==False`, controls scattering logic for
+        `psi_f` pairs.
 
     log2_F : int
         Equal to `log2(prevpow2(F))`; is the maximum frequential subsampling
@@ -1485,7 +1509,9 @@ class _FrequencyScatteringBase(ScatteringBase):
                              "(got {} > {})".format(
                                  self.F, 2**self.shape_fr_scale_max))
         self.log2_F = math.floor(math.log2(self.F))
-        self.average_fr_global = bool(self.F == 2**self.shape_fr_scale_max)
+        self.average_fr_global_phi = bool(self.F == 2**self.shape_fr_scale_max)
+        self.average_fr_global = bool(self.average_fr_global_phi and
+                                      self.average_fr)
 
         # restrict `J_pad_fr_max` (and `J_pad_fr_max_init`) if specified by user
         err = ValueError("`max_pad_factor_fr` must be int>0, non-increasing "
@@ -1578,8 +1604,8 @@ class _FrequencyScatteringBase(ScatteringBase):
                 'shape_fr_scale_max', 'shape_fr_scale_min', 'pad_mode_fr',
                 'max_pad_factor_fr', 'unrestricted_pad_fr',
                 'max_subsample_equiv_before_phi_fr',
-                'subsample_equiv_relative_to_max_pad_init', 'average_fr_global',
-                'sampling_psi_fr', 'sampling_phi_fr',
+                'subsample_equiv_relative_to_max_pad_init',
+                'average_fr_global_phi', 'sampling_psi_fr', 'sampling_phi_fr',
                 'sigma_max_to_min_max_ratio',
                 'r_psi', 'normalize', 'sigma0', 'alpha', 'P_max', 'eps'))
 
@@ -1705,7 +1731,7 @@ class _FrequencyScatteringBase(ScatteringBase):
             j0 = self.subsample_equiv_relative_to_max_pad_init[n2]
             if j0 == -1:
                 sub = -1
-            elif self.average_fr_global:
+            elif self.average_fr_global_phi:
                 # "lowpass shape" always same, can't distort
                 sub = 999
             else:
@@ -1772,7 +1798,7 @@ class _FrequencyScatteringBase(ScatteringBase):
             shape_fr, self.J_fr, Q, self.F, pad_mode=self.pad_mode_fr,
             **self.get_params('r_psi', 'sigma0', 'alpha', 'P_max', 'eps',
                               'criterion_amplitude', 'normalize'))
-        if self.average_fr_global:
+        if self.average_fr_global_phi:
             min_to_pad = pad_psi1  # ignore phi's padding
             pad_phi = 0
         J_pad = math.ceil(np.log2(shape_fr + 2 * min_to_pad))

kymatio/visuals.py
@@ -889,10 +889,17 @@ def energy_profile_jtfs(Scx, meta, x=None, pairs=None, kind='l2', plots=True,
         - L1: `sum(abs(x))`
         - L2: `sum(abs(x)**2)` -- actually L2^2
 
+    plots : bool (default True)
+        Whether to visualize the energies.
+
+    plot_kw : kwargs
+        Will pass to `kymatio.visuals.plot()`.
+
     Returns
     -------
     energies: list[float]
         List of coefficient energies.
+
     pair_energies: dict[str: float]
         Keys are pairs, values are sums of all pair's coefficient energies.
     """
@@ -921,7 +928,44 @@ def energy_profile_jtfs(Scx, meta, x=None, pairs=None, kind='l2', plots=True,
 
 def coeff_distance_jtfs(Scx0, Scx1, meta0, meta1=None, pairs=None, kind='l2',
                         plots=True, **plot_kw):
-    # TODO doc
+    """Computes relative distance between JTFS coefficients.
+
+    Parameters
+    ----------
+    Scx0, Scx1: dict[list] / dict[np.ndarray]
+        `jtfs(x)`.
+
+    meta0: dict[dict[np.ndarray]]
+        `jtfs.meta()`.
+
+    meta1: dict[dict[np.ndarray]] / None
+        `jtfs.meta()` for `Scx1`. Configuration cannot differ in any way
+        that alters coefficient shapes.
+
+    pairs: None / list/tuple[str]
+        Computes distances for these pairs in provided order. None will compute
+        for all in default order:
+            ('S0', 'S1', 'phi_t * phi_f', 'phi_t * psi_f', 'psi_t * phi_f',
+             'psi_t * psi_f_up', 'psi_t * psi_f_down')
+
+    kind : str['l1', 'l2']
+        - L1: `sum(abs(x))`
+        - L2: `sum(abs(x)**2)` -- actually L2^2
+
+    plots : bool (default True)
+        Whether to visualize the distances.
+
+    plot_kw : kwargs
+        Will pass to `kymatio.visuals.plot()`.
+
+    Returns
+    -------
+    distances : list[float]
+        List of coefficient distances.
+
+    pair_distances : dict[str: float]
+        Keys are pairs, values are sums of all pair's coefficient distances.
+    """
     if not all(isinstance(Scx, dict) for Scx in (Scx0, Scx1)):
         raise NotImplementedError("inputs must be dict. Set "
                                   "out_type='dict:array' or 'dict:list'.")