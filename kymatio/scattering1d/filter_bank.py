e0978a98a1b06768009344b246e7e64d0333e65b
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-06-08 18:51:41 +0200

    MAINT decouple `scattering_filter_factory` from `ScatteringBase1D.build` (#842)
    * remove call to compute_minimum_support_to_pad
    
    * remove criterion_amplitude from scattering_filter_factory
    
    * remove compute_minimum_support_to_pad
    * bugfix previous commit
    (E       IndexError: tuple index out of range) due to bad phi_f shape
    * update examples/1d/plot_filters
    * remove criterion_amplitude from self
    
    * update test_Scattering1D_filter_factory_T
    
    remove fourth output argument (t_max_phi)
    
    * remove 4th output argument from scattering_filter_factory
    
    * remove an empty line
    
    requested by muawiz

--- kymatio/scattering1d/filter_bank.py
+++ kymatio/scattering1d/filter_bank.py
@@ -556,7 +556,7 @@ def calibrate_scattering_filters(J, Q, T, alpha, r_psi=math.sqrt(0.5), sigma0=0.
 
 
 def scattering_filter_factory(J_support, J_scattering, Q, T, r_psi=math.sqrt(0.5),
-                              criterion_amplitude=1e-3, normalize='l1',
+                              normalize='l1',
                               max_subsampling=None, sigma0=0.1, alpha=5.,
                               P_max=5, eps=1e-7, **kwargs):
     """
@@ -589,9 +589,6 @@ def scattering_filter_factory(J_support, J_scattering, Q, T, r_psi=math.sqrt(0.5
         Should be >0 and <1. Controls the redundancy of the filters
         (the larger r_psi, the larger the overlap between adjacent wavelets).
         Defaults to sqrt(0.5).
-    criterion_amplitude : float, optional
-        Represents the numerical error which is allowed to be lost after
-        convolution and padding. Defaults to 1e-3.
     normalize : string, optional
         Normalization convention for the filters (in the
         temporal domain). Supported values include 'l1' and 'l2'; a ValueError
@@ -643,10 +640,6 @@ def scattering_filter_factory(J_support, J_scattering, Q, T, r_psi=math.sqrt(0.5
         The keys of this dictionary are of th etype (j, n) where n is an
         integer counting the filters and j is the maximal dyadic subsampling
         which can be performed on top of this filter without aliasing.
-    t_max_phi : int
-        temporal size to use to pad the signal on the right and on the
-        left by making at most criterion_amplitude error. Assumes that the
-        temporal support of the low-pass filter is larger than all filters.
 
     Refs
     ----
@@ -733,10 +726,5 @@ def scattering_filter_factory(J_support, J_scattering, Q, T, r_psi=math.sqrt(0.5
     phi_f['sigma'] = sigma_low
     phi_f['j'] = log2_T
 
-    # compute the support size allowing to pad without boundary errors
-    # at the finest resolution  # TODO
-    t_max_phi = compute_temporal_support(
-        phi_f[0].reshape(1, -1), criterion_amplitude=criterion_amplitude)
-
     # return results
-    return phi_f, psi1_f, psi2_f, t_max_phi
+    return phi_f, psi1_f, psi2_f
