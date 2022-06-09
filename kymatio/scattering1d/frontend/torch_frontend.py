b5afb017a298fba0a1526351e23ae310a647c06a
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-06-09 19:24:27 +0200

    remove vectorize from Scattering1D
    use out_array='dict' instead !

--- kymatio/scattering1d/frontend/torch_frontend.py
+++ kymatio/scattering1d/frontend/torch_frontend.py
@@ -8,7 +8,7 @@ from .base_frontend import ScatteringBase1D
 
 class ScatteringTorch1D(ScatteringTorch, ScatteringBase1D):
     def __init__(self, J, shape, Q=1, T=None, max_order=2, average=True,
-            oversampling=0, vectorize=True, out_type='array', backend='torch'):
+            oversampling=0, out_type='array', backend='torch'):
         ScatteringTorch.__init__(self)
         ScatteringBase1D.__init__(self, J, shape, Q, T, max_order, average,
                 oversampling, vectorize, out_type, backend)
@@ -25,7 +25,6 @@ class ScatteringTorch1D(ScatteringTorch, ScatteringBase1D):
         # prepare for pytorch
         for k in self.phi_f.keys():
             if type(k) != str:
-                # view(-1, 1).repeat(1, 2) because real numbers!
                 self.phi_f[k] = torch.from_numpy(
                     self.phi_f[k]).float().view(-1, 1)
                 self.register_buffer('tensor' + str(n), self.phi_f[k])
@@ -33,7 +32,6 @@ class ScatteringTorch1D(ScatteringTorch, ScatteringBase1D):
         for psi_f in self.psi1_f:
             for sub_k in psi_f.keys():
                 if type(sub_k) != str:
-                    # view(-1, 1).repeat(1, 2) because real numbers!
                     psi_f[sub_k] = torch.from_numpy(
                         psi_f[sub_k]).float().view(-1, 1)
                     self.register_buffer('tensor' + str(n), psi_f[sub_k])
@@ -41,7 +39,6 @@ class ScatteringTorch1D(ScatteringTorch, ScatteringBase1D):
         for psi_f in self.psi2_f:
             for sub_k in psi_f.keys():
                 if type(sub_k) != str:
-                    # view(-1, 1).repeat(1, 2) because real numbers!
                     psi_f[sub_k] = torch.from_numpy(
                         psi_f[sub_k]).float().view(-1, 1)
                     self.register_buffer('tensor' + str(n), psi_f[sub_k])
@@ -70,26 +67,8 @@ class ScatteringTorch1D(ScatteringTorch, ScatteringBase1D):
                     n += 1
 
     def scattering(self, x):
-        # basic checking, should be improved
-        if len(x.shape) < 1:
-            raise ValueError(
-                'Input tensor x should have at least one axis, got {}'.format(
-                    len(x.shape)))
-
-        if not self.out_type in ('array', 'list'):
-            raise RuntimeError("The out_type must be one of 'array' or 'list'.")
-
-        if not self.average and self.out_type == 'array' and self.vectorize:
-            raise ValueError("Options average=False, out_type='array' and "
-                             "vectorize=True are mutually incompatible. "
-                             "Please set out_type to 'list' or vectorize to "
-                             "False.")
-
-        if not self.vectorize:
-            warnings.warn("The vectorize option is deprecated and will be "
-                          "removed in version 0.3. Please set "
-                          "out_type='list' for equivalent functionality.",
-                          DeprecationWarning)
+        ScatteringBase1D._check_runtime_args(self)
+        ScatteringBase1D._check_input(self, x)
 
         batch_shape = x.shape[:-1]
         signal_shape = x.shape[-1:]
@@ -100,13 +79,13 @@ class ScatteringTorch1D(ScatteringTorch, ScatteringBase1D):
 
         S = scattering1d(x, self.backend, self.psi1_f, self.psi2_f, self.phi_f,\
                          max_order=self.max_order, average=self.average, pad_left=self.pad_left, pad_right=self.pad_right,
-                        ind_start=self.ind_start, ind_end=self.ind_end, oversampling=self.oversampling, vectorize=self.vectorize, out_type=self.out_type)
+                        ind_start=self.ind_start, ind_end=self.ind_end, oversampling=self.oversampling, out_type=self.out_type)
 
-        if self.out_type == 'array' and self.vectorize:
+        if self.out_type == 'array':
             scattering_shape = S.shape[-2:]
             new_shape = batch_shape + scattering_shape
             S = S.reshape(new_shape)
-        elif self.out_type == 'array' and not self.vectorize:
+        elif self.out_type == 'dict':
             for k, v in S.items():
                 # NOTE: Have to get the shape for each one since we may have
                 # average == False.
