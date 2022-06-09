e48f19276e5c57909aa1edcf18ec061df24050b3
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-06-09 18:53:02 +0200

    avoid passing log2_T, pad, unpad to 1D core

--- kymatio/scattering1d/frontend/numpy_frontend.py
+++ kymatio/scattering1d/frontend/numpy_frontend.py
@@ -41,7 +41,7 @@ class ScatteringNumPy1D(ScatteringNumPy, ScatteringBase1D):
 
         x = x.reshape((-1, 1) + signal_shape)
 
-        S = scattering1d(x, self.backend.pad, self.backend.unpad, self.backend, self.log2_T, self.psi1_f, self.psi2_f,
+        S = scattering1d(x, self.backend, self.psi1_f, self.psi2_f,
                          self.phi_f, max_order=self.max_order, average=self.average, pad_left=self.pad_left,
                          pad_right=self.pad_right, ind_start=self.ind_start, ind_end=self.ind_end,
                          oversampling=self.oversampling, vectorize=self.vectorize, out_type=self.out_type)
