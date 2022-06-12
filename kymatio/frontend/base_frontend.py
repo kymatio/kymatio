6c316cb6aea38d6de736d04ac9e2de81f943c86e
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-06-13 00:56:17 +0200

    Document _check_filterbanks better

--- kymatio/frontend/base_frontend.py
+++ kymatio/frontend/base_frontend.py
@@ -15,11 +15,15 @@ class ScatteringBase():
         raise NotImplementedError
 
     def _check_filterbanks(psi1s, psi2s):
-        # Check that the Nyquist frequency of each filter, 2**(-j), is at least
+        # Check for absence of aliasing at the first order
+        # that the Nyquist frequency of each filter, 2**(-j), is at least
         # one octave above its frequency xi
         assert all((psi1["xi"] < 0.5/(2**psi1["j"])) for psi1 in psi1s)
 
-        # Check that (j1 < j2) implies (xi1 > xi2)
+        # Check for absence of aliasing at the second order
+        # The filter function selects the (psi1, psi2) pairs such that j1<j2.
+        # The map function evaluates whether those pairs also satisfy xi1>xi2.
+        # Hence, the "assert all" statement guarantees that (j1<j2) => (xi1>xi2)
         psi_generator = itertools.product(psi1s, psi2s)
         condition = lambda psi1_or_2: psi1_or_2[0]['j'] < psi1_or_2[1]['j']
         implication = lambda psi1_or_2: psi1_or_2[0]['xi'] > psi1_or_2[1]['xi']
