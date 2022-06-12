7e4099539c5c59eea8a76edcd5c940489e8d6cee
Muawiz Chaudhary <muawizc@gmail.com>
2022-06-11 22:03:25 -0400

    MAINT enable fast_mode of gradcheck

--- tests/scattering2d/test_torch_scattering2d.py
+++ tests/scattering2d/test_torch_scattering2d.py
@@ -245,4 +245,4 @@ class TestScatteringTorch2D:
             scattering = Scattering2D(2, shape=(8, 8), backend=backend,
                                       frontend='torch').double().to(device)
             x = torch.rand(2, 1, 8, 8).double().to(device).requires_grad_()
-            gradcheck(scattering, x, nondet_tol=1e-5)
+            gradcheck(scattering, x, nondet_tol=1e-5, fast_mode=True)
