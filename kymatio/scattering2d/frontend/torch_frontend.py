7bc3a7fdd45ced92f6f6268ede9c691881cde336
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-06-16 00:36:27 +0200

    deprecate N in 2D

--- kymatio/scattering2d/frontend/torch_frontend.py
+++ kymatio/scattering2d/frontend/torch_frontend.py
@@ -90,11 +90,11 @@ class ScatteringTorch2D(ScatteringTorch, ScatteringBase2D):
         if not input.is_contiguous():
             raise RuntimeError('Tensor must be contiguous.')
 
-        if (input.shape[-1] != self.N or input.shape[-2] != self.M) and not self.pre_pad:
-            raise RuntimeError('Tensor must be of spatial size (%i,%i).' % (self.M, self.N))
+        if (input.shape[-1] != self.shape[-1] or input.shape[-2] != self.shape[-2]) and not self.pre_pad:
+            raise RuntimeError('Tensor must be of spatial size (%i,%i).' % (self.shape[0], self.shape[1]))
 
-        if (input.shape[-1] != self.N_padded or input.shape[-2] != self.M_padded) and self.pre_pad:
-            raise RuntimeError('Padded tensor must be of spatial size (%i,%i).' % (self.M_padded, self.N_padded))
+        if (input.shape[-1] != self._N_padded or input.shape[-2] != self._M_padded) and self.pre_pad:
+            raise RuntimeError('Padded tensor must be of spatial size (%i,%i).' % (self._M_padded, self._N_padded))
 
         if not self.out_type in ('array', 'list'):
             raise RuntimeError("The out_type must be one of 'array' or 'list'.")
