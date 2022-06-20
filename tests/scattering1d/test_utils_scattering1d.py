25e0fa7915f172b849a1f95b9419cbf44282ec96
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-06-20 11:11:26 +0200

    test backwards compatibility

--- tests/scattering1d/test_utils_scattering1d.py
+++ tests/scattering1d/test_utils_scattering1d.py
@@ -1,8 +1,10 @@
+import math
 import numpy as np
 import pytest
 from kymatio import Scattering1D
 from kymatio.scattering1d.frontend.numpy_frontend import ScatteringNumPy1D
-from kymatio.scattering1d.utils import compute_border_indices, compute_padding
+from kymatio.scattering1d.utils import (compute_border_indices, compute_padding,
+    compute_params_filterbank)
 
 
 def test_compute_padding():
@@ -61,3 +63,52 @@ def test_scattering1d_frontend():
     with pytest.raises(RuntimeError) as ve:
         scattering = Scattering1D(2, shape=(10,), frontend='doesnotexist')
     assert "is not valid" in ve.value.args[0]
+
+# Check that self.meta() is compliant with the legacy implementation (see below)
+def test_meta():
+    J = 6
+    Q = (3, 1)
+    length = 1024
+    shape = (length,)
+    S = Scattering1D(J, shape, Q=Q)
+    self_meta = S.meta()
+    legacy_meta = legacy_compute_meta_scattering(S.J, S.Q, S.max_order,
+        S.r_psi, S.sigma0, S.alpha)
+
+
+def legacy_compute_meta_scattering(J, Q, max_order, r_psi, sigma0, alpha):
+    sigma_min = sigma0 / math.pow(2, J)
+    Q1, Q2 = Q
+    xi1s, sigma1s, j1s = compute_params_filterbank(sigma_min, Q1, alpha, r_psi)
+    xi2s, sigma2s, j2s = compute_params_filterbank(sigma_min, Q2, alpha, r_psi)
+
+    meta = {}
+
+    meta['order'] = [[], [], []]
+    meta['xi'] = [[], [], []]
+    meta['sigma'] = [[], [], []]
+    meta['j'] = [[], [], []]
+    meta['n'] = [[], [], []]
+    meta['key'] = [[], [], []]
+
+    meta['order'][0].append(0)
+    meta['xi'][0].append(())
+    meta['sigma'][0].append(())
+    meta['j'][0].append(())
+    meta['n'][0].append(())
+    meta['key'][0].append(())
+
+    for (n1, (xi1, sigma1, j1)) in enumerate(zip(xi1s, sigma1s, j1s)):
+        meta['order'][1].append(1)
+        meta['xi'][1].append((xi1,))
+        meta['sigma'][1].append((sigma1,))
+        meta['j'][1].append((j1,))
+        meta['n'][1].append((n1,))
+        meta['key'][1].append((n1,))
+
+        if max_order < 2:
+            continue
+
+        for (n2, (xi2, sigma2, j2)) in enumerate(zip(xi2s, sigma2s, j2s)):
+            if j2 > j1:
+                meta['order'][2].append(2)
