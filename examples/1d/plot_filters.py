cfbd1a36bbb4f3fd0d821a39772ca0a43b089ad0
Joakim Andén <janden@kth.se>
2022-06-23 18:20:25 +0200

    FIX Update examples/1d/plot_filters.py (#899)
    * FIX Update examples/1d/plot_filters.py
    
    As a consequence of #863.
    
    * FIX Update examples/2d/plot_filters
    
    Add new levels key.

--- examples/1d/plot_filters.py
+++ examples/1d/plot_filters.py
@@ -59,7 +59,7 @@ Q = (8, 1)
 # first-order wavelet filters (`psi1_f`), and the second-order filters
 # (`psi2_f`).
 
-phi_f, psi1_f, psi2_f = scattering_filter_factory(np.log2(T), J, Q, T)
+phi_f, psi1_f, psi2_f = scattering_filter_factory(T, J, Q, T)
 
 ###############################################################################
 # The `phi_f` output is a dictionary where each integer key corresponds points
