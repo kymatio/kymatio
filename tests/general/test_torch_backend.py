10ca6602a38598cc2e74001f3b0cb13761da0806
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-07-20 09:09:15 +0200

    unit test reshape in torch backend

--- tests/general/test_torch_backend.py
+++ tests/general/test_torch_backend.py
@@ -24,3 +24,31 @@ def test_modulus(random_state=42):
     y_grad = torch.ones_like(y)
     x_grad_manual = ModulusStable.backward(ctx, y_grad)
     assert torch.allclose(x_grad_manual, x_grad)
+
+
+def test_reshape():
+    backend = TorchBackend
+
+    # 1D
+    x = torch.randn(3, 5, 16)
+    y = backend.reshape_input(x, signal_shape=(16,))
+    xbis = backend.reshape_output(y, batch_shape=(3, 5), n_kept_dims=1)
+    assert backend.shape(x) == x.shape
+    assert y.shape == (15, 1, 16)
+    assert torch.allclose(x, xbis)
+
+    # 2D
+    x = torch.randn(3, 5, 16, 16)
+    y = backend.reshape_input(x, signal_shape=(16, 16))
+    xbis = backend.reshape_output(y, batch_shape=(3, 5), n_kept_dims=2)
+    assert backend.shape(x) == x.shape
+    assert y.shape == (15, 1, 16, 16)
+    assert torch.allclose(x, xbis)
+
+    # 3D
+    x = torch.randn(3, 5, 16, 16, 16)
+    y = backend.reshape_input(x, signal_shape=(16, 16, 16))
+    xbis = backend.reshape_output(y, batch_shape=(3, 5), n_kept_dims=3)
+    assert backend.shape(x) == x.shape
+    assert y.shape == (15, 1, 16, 16, 16)
+    assert torch.allclose(x, xbis)
