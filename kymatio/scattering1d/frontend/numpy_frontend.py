d211579db956b3ad580aeb4e13defd758524bf58
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-07-22 20:23:49 +0200

    `reshape_input`, `reshape_output` (#871)
    * backend shape methods
    
    * reshape_input, reshape_output in backends
    
    * try to call reshape_output only once [ci skip]
    
    * simplify PR, figure out "dict" as special case
    
    * spaces around operator [ci skip]
    
    * bugfix prev commit (concatenate dim in NP backend)
    
    * manage multiple inheritance
    
    move the NotImplementedError's of scattering to 2D and 3D 's base frontends
    
    * implement scattering(self, x) in 1D base frontend
    
    * shape(x, signal_dim) in backends
    
    * bugfix previous commit
    
    * Revert "bugfix previous commit"
    
    This reverts commit de46c9efeab7ca01c2f8a74cbf8f3202148ab1b1.
    
    * Revert "shape(x, signal_dim) in backends"
    
    This reverts commit 937b87cf1e5287ec6b83df0eccd8b5b58d3d3728.
    
    * add empty line in base_frontend 1D scattering
    
    after review by Muawiz
    
    * bugfix out_type=="list"
    
    * remove pad from core scattering1d
    
    compute padding in base frontend instead
    
    * spaces around operator
    
    * simplify reshape_input
    
    * fix 895
    
    * rename dim to axis
    
    for compatibility with NumPy Array API
    
    * update concatenate callsites
    
    * Revert "update concatenate callsites"
    
    This reverts commit 4222d10d7e15189e3fd52c45562daa30f84b9634.
    
    * Revert "rename dim to axis"
    
    This reverts commit f8cccf99118d02d4b0721c19ced144e27ba05769.
    
    * unit test reshape in torch backend
    
    * unit test reshape in NP backend
    
    * unit test reshape in TF backend
    
    * Revert "Merge branch 'pad-before-core-scattering' into backend-agnostic-reshape"
    
    This reverts commit c31ef30d6250ce9107224bdbf69cdc12a6623ac1, reversing
    changes made to a7a8f75e43bae49cc4cf3785b447bb1bb06c4a19.

--- kymatio/scattering1d/frontend/numpy_frontend.py
+++ kymatio/scattering1d/frontend/numpy_frontend.py
@@ -18,37 +18,25 @@ class ScatteringNumPy1D(ScatteringNumPy, ScatteringBase1D):
     def scattering(self, x):
         ScatteringBase1D._check_runtime_args(self)
         ScatteringBase1D._check_input(self, x)
-
-        batch_shape = x.shape[:-1]
-        signal_shape = x.shape[-1:]
-
-        x = x.reshape((-1, 1) + signal_shape)
+        x_shape = self.backend.shape(x)
+        batch_shape, signal_shape = x_shape[:-1], x_shape[-1:]
+        x = self.backend.reshape_input(x, signal_shape)
 
         S = scattering1d(x, self.backend, self.psi1_f, self.psi2_f,
                          self.phi_f, max_order=self.max_order, average=self.average, pad_left=self.pad_left,
                          pad_right=self.pad_right, ind_start=self.ind_start, ind_end=self.ind_end,
                          oversampling=self.oversampling)
 
-        if self.out_type == 'array':
-            S = self.backend.concatenate([x['coef'] for x in S])
-            scattering_shape = S.shape[-2:]
-            new_shape = batch_shape + scattering_shape
-            S = S.reshape(new_shape)
-        elif self.out_type == 'dict':
-            S = {x['n']: x['coef'] for x in S}
-            for k, v in S.items():
-                # NOTE: Have to get the shape for each one since we may have
-                # average == False.
-                scattering_shape = v.shape[-1:]
-                new_shape = batch_shape + scattering_shape
-                S[k] = v.reshape(new_shape)
-        elif self.out_type == 'list':
-            for x in S:
-                scattering_shape = x['coef'].shape[-1:]
-                new_shape = batch_shape + scattering_shape
-                x['coef'] = x['coef'].reshape(new_shape)
+        for n, path in enumerate(S):
+            S[n]['coef'] = self.backend.reshape_output(
+                path['coef'], batch_shape, n_kept_dims=1)
 
-        return S
+        if self.out_type == 'array':
+            return self.backend.concatenate([path['coef'] for path in S], dim=-2)
+        elif self.out_type == 'dict':
+            return {path['n']: path['coef'] for path in S}
+        elif self.out_type == 'list':
+            return S
 
 
 ScatteringNumPy1D._document()
