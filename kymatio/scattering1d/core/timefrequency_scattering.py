56e71620a680a7d109eb5ed1806e710d01f88e9b
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-08-13 23:43:09 +0200

    kymatio primitives in width-first
    + some control flow bugfixes
    and whitespace around ** operator

--- kymatio/scattering1d/core/timefrequency_scattering.py
+++ kymatio/scattering1d/core/timefrequency_scattering.py
@@ -1,6 +1,6 @@
 def scattering1d_widthfirst(U_0, backend, filters, oversampling, average_local):
     # compute the Fourier transform
-    U_0_hat = rfft(U_0)
+    U_0_hat = backend.rfft(U_0)
 
     # Get S0
     phi = filters[0]
@@ -8,9 +8,9 @@ def scattering1d_widthfirst(U_0, backend, filters, oversampling, average_local):
     k0 = max(log2_T - oversampling, 0)
 
     if average_local:
-        S_0_c = cdgmm(U_0_hat, phi['levels'][0])
-        S_0_hat = subsample_fourier(S_0_c, 2**k0)
-        S_0_r = irfft(S_0_hat)
+        S_0_c = backend.cdgmm(U_0_hat, phi['levels'][0])
+        S_0_hat = backend.subsample_fourier(S_0_c, 2 ** k0)
+        S_0_r = backend.irfft(S_0_hat)
         S_1_list = []
         # S_0_r and U_0 are 2D arrays indexed by (batch, time)
         yield {'coef': S_0_r, 'j': (), 'n': ()}
@@ -25,36 +25,37 @@ def scattering1d_widthfirst(U_0, backend, filters, oversampling, average_local):
         j1 = psi1[n1]['j']
         sub1_adj = min(j1, log2_T) if average_local else j1
         k1 = max(sub1_adj - oversampling, 0)
-        U_1_c = cdgmm(U_0_hat, psi1[n1]['levels'][0])
-        U_1_hat = subsample_fourier(U_1_c, 2**k1)
-        U_1_c = ifft(U_1_hat)
+        U_1_c = backend.cdgmm(U_0_hat, psi1[n1]['levels'][0])
+        U_1_hat = backend.subsample_fourier(U_1_c, 2 ** k1)
+        U_1_c = backend.ifft(U_1_hat)
 
         # Take the modulus
-        U_1_m = modulus(U_1_c)
+        U_1_m = backend.modulus(U_1_c)
+
+        U_1_hat = backend.rfft(U_1_m)
+        # Width-first algorithm: store U1 in anticipation of next layer
+        U_1_hats.append({'coef': U_1_hat, 'j': (j1,), 'n': (n1,)})
 
         if average_local:
-            U_1_hat = rfft(U_1_m)
-            # Width-first algorithm: store U1 in anticipation of next layer
-            U_1_hats.append({'coef': U_1_hat, 'j': (j1,), 'n': (n1,)})
-
             # Convolve with phi_J
             k1_J = max(log2_T - k1 - oversampling, 0)
-            S_1_c = cdgmm(U_1_hat, phi['levels'][k1])
-            S_1_hat = subsample_fourier(S_1_c, 2**k1_J)
-            S_1_r = irfft(S_1_hat)
+            S_1_c = backend.cdgmm(U_1_hat, phi['levels'][k1])
+            S_1_hat = backend.subsample_fourier(S_1_c, 2 ** k1_J)
+            S_1_r = backend.irfft(S_1_hat)
             S_1_list.append(S_1_r)
         else:
             # U_1_m is a 2D array indexed by (batch, time)
             yield {'coef': U_1_m, 'j': (j1,), 'n': (n1,)}
 
-    # Concatenate S1 paths over the penultimate dimension with shared n1.
-    # S1 is a real-valued 3D array indexed by (batch, n1, time)
-    S_1 = concatenate(S_1_list, -2)
+    if average_local:
+        # Concatenate S1 paths over the penultimate dimension with shared n1.
+        # S1 is a real-valued 3D array indexed by (batch, n1, time)
+        S_1 = backend.concatenate(S_1_list, -2)
 
-    # S1 is a stack of multiple n1 paths so we puth (-1) as placeholder.
-    # n1 ranges between 0 (included) and n1_max (excluded), which we store
-    # separately for the sake of meta() and padding/unpadding downstream.
-    yield {'coef': S_1, 'j': (-1,), 'n': (-1,), 'n1_max': len(S_1_list)}
+        # S1 is a stack of multiple n1 paths so we puth (-1) as placeholder.
+        # n1 ranges between 0 (included) and n1_max (excluded), which we store
+        # separately for the sake of meta() and padding/unpadding downstream.
+        yield {'coef': S_1, 'j': (-1,), 'n': (-1,), 'n1_max': len(S_1_list)}
 
     # Second order. Note that n2 is the outer loop (width-first algorithm)
     psi2 = filters[2]
@@ -64,20 +65,23 @@ def scattering1d_widthfirst(U_0, backend, filters, oversampling, average_local):
 
         for U_1_hat in U_1_hats:
             j1 = U_1_hat['j'][0]
+            sub1_adj = min(j1, log2_T) if average_local else j1
+            k1 = max(sub1_adj - oversampling, 0)
 
             if j2 > j1:
-                sub2_adj = min(j2, log2_T) if average else j2
+                sub2_adj = min(j2, log2_T) if average_local else j2
                 k2 = max(sub2_adj - k1 - oversampling, 0)
-                U_2_c = cdgmm(U_1_hat['coef'], psi2[n2]['levels'][k1])
-                U_2_hat = subsample_fourier(U_2_c, 2**k2)
-                U_2_c = ifft(U_2_hat)
+                U_2_c = backend.cdgmm(U_1_hat['coef'], psi2[n2]['levels'][k1])
+                U_2_hat = backend.subsample_fourier(U_2_c, 2 ** k2)
+                U_2_c = backend.ifft(U_2_hat)
                 Y_2_list.append(U_2_c)
 
-        # Concatenate over the penultimate dimension with shared n2.
-        # Y_2 is a complex-valued 3D array indexed by (batch, n1, time)
-        Y_2 = concatenate(Y_2_list, -2)
+        if len(Y_2_list) > 0:
+            # Concatenate over the penultimate dimension with shared n2.
+            # Y_2 is a complex-valued 3D array indexed by (batch, n1, time)
+            Y_2 = backend.concatenate(Y_2_list, -2)
 
-        # Y_2 is a stack of multiple n1 paths so we put (-1) as placeholder.
-        # n1 ranges between 0 (included) and n1_max (excluded), which we store
-        # separately for the sake of meta() and padding/unpadding downstream.
-        yield {'coef': Y_2, 'j': (-1, j2), 'n': (-1, n2), 'n1_max': len(Y_2_list)}
+            # Y_2 is a stack of multiple n1 paths so we put (-1) as placeholder.
+            # n1 ranges between 0 (included) and n1_max (excluded), which we store
+            # separately for the sake of meta() and padding/unpadding downstream.
+            yield {'coef': Y_2, 'j': (-1, j2), 'n': (-1, n2), 'n1_max': len(Y_2_list)}
