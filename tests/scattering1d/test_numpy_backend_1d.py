65c25933dc32ed998fcc3c07b68e9694ad7de09f
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-08-14 16:58:59 +0200

    ENH `swap_time_frequency`
    new Kymatio primitive: swap_time_frequency
    
    Co-authored-by: Muawiz Chaudhary <muawiz.chaudhary@mila.quebec>
    Co-authored-by: Cyrus Vahidi <c.vahidi@qmul.ac.uk>

--- tests/scattering1d/test_numpy_backend_1d.py
+++ tests/scattering1d/test_numpy_backend_1d.py
@@ -115,3 +115,22 @@ def test_fft():
     z_2 = backend.irfft(z)
     assert not np.iscomplexobj(z_2)
     assert np.allclose(x_r, z_2)
+
+
+def test_swap_time_frequency_1d():
+    shape = (10, 20, 3, 5)
+    shape_T = (10, 20, 5, 3)
+
+    x = np.arange(np.prod(shape)).reshape(shape) * 1j
+    x_T = backend.swap_time_frequency(x)
+    assert tuple(x_T.shape) == shape_T
+
+    x_T_T = backend.swap_time_frequency(x_T)
+    assert tuple(x_T_T.shape) == shape
+    assert x_T_T.shape == x.shape
+    assert np.all(x == x_T_T)
+
+    with pytest.raises(TypeError) as record:
+        x = np.ones(shape + (4,))
+        y = backend.swap_time_frequency(x)
+    assert 'should be complex' in record.value.args[0]
