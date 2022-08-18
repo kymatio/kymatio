FIX JTFS `average_local=False` in `scattering1d_widthfirst` (#932)
* remove average_local from scattering1d_widthfirst

* update test_scattering1d_widthfirst

* average_local=True returns S0

* check order 0 under average_local=False

* update docstring

tests/scattering1d/test_timefrequency_scattering.py
@@ -63,59 +63,46 @@ def test_scattering1d_widthfirst():
     S = Scattering1D(J, shape)
     x = torch.zeros(shape)
     x[shape[0]//2] = 1
-
-    ### average_local == False ###
-    average_local = False
-    # Width-first scattering
     x_shape = S.backend.shape(x)
     batch_shape, signal_shape = x_shape[:-1], x_shape[-1:]
     x = S.backend.reshape_input(x, signal_shape)
     U_0 = S.backend.pad(x, pad_left=S.pad_left, pad_right=S.pad_right)
-    filters = [S.phi_f, S.psi1_f, S.psi2_f]
-    U_gen = scattering1d_widthfirst(U_0, S.backend, filters, S.oversampling,
-        average_local)
-    U_width = {path['n']: path['coef'] for path in U_gen}
-
-    # Depth-first scattering
-    U_gen = scattering1d(U_0, S.backend, filters, S.oversampling, average_local)
-    U_depth = {path['n']: path['coef'] for path in U_gen}
-
-    # Check orders 0 and 1
-    skip_order2 = lambda item: (len(item[0]) < 2)
-    U_width_no_order2 = dict(filter(skip_order2, U_width.items()))
-    U_depth_no_order2 = dict(filter(skip_order2, U_depth.items()))
-    assert set(U_width_no_order2.keys()) == set(U_depth_no_order2.keys())
-    for key in U_width_no_order2:
-        assert torch.allclose(U_width_no_order2[key], U_depth_no_order2[key])
-
-    # Check order 2
-    keep_order2 = lambda item: (len(item[0]) == 2)
-    Y_width_order2 = dict(filter(keep_order2, U_width.items()))
-    U_depth_order2 = dict(filter(keep_order2, U_depth.items()))
-    for key in Y_width_order2:
-        n2 = key[-1]
-        keep_n2 = lambda item: (item[0][-1] == n2)
-        U_n2 = dict(filter(keep_n2, U_depth_order2.items()))
-        U_n2 = S.backend.concatenate([U_n2[key] for key in sorted(U_n2.keys())])
-        assert torch.allclose(S.backend.modulus(Y_width_order2[key]), U_n2)
-
-    ### average_local == True ###
-    average_local = True
 
     # Width-first
-    S_gen = scattering1d_widthfirst(U_0, S.backend, filters, S.oversampling,
-        average_local)
-    S_width = {path['n']: path['coef'] for path in S_gen}
+    filters = [S.phi_f, S.psi1_f, S.psi2_f]
+    W_gen = scattering1d_widthfirst(U_0, S.backend, filters, S.oversampling,
+        average_local=True)
+    W = {path['n']: path['coef'] for path in W_gen}
 
     # Depth-first
-    S_gen = scattering1d(U_0, S.backend, filters, S.oversampling, average_local)
+    S_gen = scattering1d(U_0, S.backend, filters, S.oversampling,
+        average_local=True)
     S1_depth = {path['n']: path['coef'] for path in S_gen if len(path['n']) > 0}
+    U_gen = scattering1d(U_0, S.backend, filters, S.oversampling,
+        average_local=False)
+    U2_depth = {path['n']: path['coef'] for path in U_gen if len(path['n']) == 2}
 
-    # Check order 1 (order 2 is unaveraged in width-first so irrelevant here)
+    # Check order 1
     keep_order1 = lambda item: (len(item[0]) == 1)
-    S1_width = dict(filter(keep_order1, S_width.items()))
+    S1_width = dict(filter(keep_order1, W.items()))
     assert len(S1_width) == 1
     S1_width = S1_width[(-1,)]
     S1_depth = S.backend.concatenate([
         S1_depth[key] for key in sorted(S1_depth.keys()) if len(key)==1])
     assert torch.allclose(S1_width, S1_depth)
+
+    # Check order 2
+    keep_order2 = lambda item: (len(item[0]) == 2)
+    Y_width_order2 = dict(filter(keep_order2, W.items()))
+    for key in Y_width_order2:
+        n2 = key[-1]
+        keep_n2 = lambda item: (item[0][-1] == n2)
+        U_n2 = dict(filter(keep_n2, U2_depth.items()))
+        U_n2 = S.backend.concatenate([U_n2[key] for key in sorted(U_n2.keys())])
+        assert torch.allclose(S.backend.modulus(Y_width_order2[key]), U_n2)
+
+    # Check order 0 under average_local=False
+    W_gen = scattering1d_widthfirst(U_0, S.backend, filters, S.oversampling,
+        average_local=False)
+    U_0_width = next(W_gen)
+    assert torch.allclose(U_0_width['coef'], U_0)