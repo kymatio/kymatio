947936afa0ebbf390034f5b4831314692ec1ef2a
Changhong Wang <changhong.wang@qmul.ac.uk>
2022-09-05 22:01:34 +0200

    sklearn frontend for jtfs

--- kymatio/scattering1d/frontend/sklearn_frontend.py
+++ kymatio/scattering1d/frontend/sklearn_frontend.py
@@ -1,14 +1,15 @@
 from ...frontend.sklearn_frontend import ScatteringTransformerMixin
 from ...numpy import Scattering1D as ScatteringNumPy1D
-
+from ...numpy import TimeFrequencyScatteringNumPy
 
 # NOTE: Order in base classes matters here, since we want the sklearn-specific
 # documentation parameters to take precedence over NP.
 class ScatteringTransformer1D(ScatteringTransformerMixin, ScatteringNumPy1D):
     pass
 
-# comments
 ScatteringTransformer1D._document()
 
+class TimeFrequencyScatteringTransformer(ScatteringTransformerMixin, TimeFrequencyScatteringNumPy):
+    pass
 
-__all__ = ['ScatteringTransformer1D']
+__all__ = ['ScatteringTransformer1D', 'TimeFrequencyScatteringTransformer']
