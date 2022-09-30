24ff064af0307591e64303742c3ccd5894ffdfd1
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-09-30 17:42:49 +0200

    add TimeFrequencyScattering to jax

--- kymatio/jax.py
+++ kymatio/jax.py
@@ -1,15 +1,22 @@
 from .scattering1d.frontend.jax_frontend import ScatteringJax1D as Scattering1D
+from .scattering1d.frontend.jax_frontend import (
+    TimeFrequencyScatteringJax as TimeFrequencyScattering,
+)
 from .scattering2d.frontend.jax_frontend import ScatteringJax2D as Scattering2D
-from .scattering3d.frontend.jax_frontend \
-        import HarmonicScatteringJax3D as HarmonicScattering3D
+from .scattering3d.frontend.jax_frontend import (
+    HarmonicScatteringJax3D as HarmonicScattering3D,
+)
 
-Scattering1D.__module__ = 'kymatio.jax'
-Scattering1D.__name__ = 'Scattering1D'
+Scattering1D.__module__ = "kymatio.jax"
+Scattering1D.__name__ = "Scattering1D"
 
-Scattering2D.__module__ = 'kymatio.jax'
-Scattering2D.__name__ = 'Scattering2D'
+TimeFrequencyScattering.__module__ = "kymatio.jax"
+TimeFrequencyScattering.__name__ = "TimeFrequencyScattering"
 
-HarmonicScattering3D.__module__ = 'kymatio.jax'
-HarmonicScattering3D.__name__ = 'HarmonicScattering3D'
+Scattering2D.__module__ = "kymatio.jax"
+Scattering2D.__name__ = "Scattering2D"
 
-__all__ = ['Scattering1D', 'Scattering2D', 'HarmonicScattering3D']
+HarmonicScattering3D.__module__ = "kymatio.jax"
+HarmonicScattering3D.__name__ = "HarmonicScattering3D"
+
+__all__ = ["Scattering1D", "Scattering2D", "HarmonicScattering3D"]
