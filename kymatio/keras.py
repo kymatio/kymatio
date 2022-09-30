07844efcc9c014ce18e63ba82ee49d9ff40d17d5
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-09-30 17:42:45 +0200

    add TimeFrequencyScattering to keras

--- kymatio/keras.py
+++ kymatio/keras.py
@@ -1,12 +1,16 @@
-from .scattering1d.frontend.keras_frontend \
-    import ScatteringKeras1D as Scattering1D
-from .scattering2d.frontend.keras_frontend \
-    import ScatteringKeras2D as Scattering2D
+from .scattering1d.frontend.keras_frontend import ScatteringKeras1D as Scattering1D
+from .scattering1d.frontend.keras_frontend import (
+    TimeFrequencyScatteringKeras as TimeFrequencyScattering,
+)
+from .scattering2d.frontend.keras_frontend import ScatteringKeras2D as Scattering2D
 
-Scattering1D.__module__ = 'kymatio.keras'
-Scattering1D.__name__ = 'Scattering1D'
+Scattering1D.__module__ = "kymatio.keras"
+Scattering1D.__name__ = "Scattering1D"
 
-Scattering2D.__module__ = 'kymatio.keras'
-Scattering2D.__name__ = 'Scattering2D'
+TimeFrequencyScattering.__module__ = "kymatio.keras"
+TimeFrequencyScattering.__name__ = "TimeFrequencyScattering"
 
-__all__ = ['Scattering1D', 'Scattering2D']
+Scattering2D.__module__ = "kymatio.keras"
+Scattering2D.__name__ = "Scattering2D"
+
+__all__ = ["Scattering1D", "Scattering2D"]
