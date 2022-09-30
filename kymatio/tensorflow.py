869b7ddf14c39d7ad6598e174105326cd099551e
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-09-30 17:39:47 +0200

    add TimeFrequencyScattering to TF frontend

--- kymatio/tensorflow.py
+++ kymatio/tensorflow.py
@@ -1,15 +1,31 @@
-from .scattering1d.frontend.tensorflow_frontend import ScatteringTensorFlow1D as Scattering1D
-from .scattering2d.frontend.tensorflow_frontend import ScatteringTensorFlow2D as Scattering2D
-from .scattering3d.frontend.tensorflow_frontend \
-        import HarmonicScatteringTensorFlow3D as HarmonicScattering3D
+from .scattering1d.frontend.tensorflow_frontend import (
+    ScatteringTensorFlow1D as Scattering1D,
+)
+from .scattering1d.frontend.tensorflow_frontend import (
+    TimeFrequencyScatteringTensorFlow as TimeFrequencyScattering,
+)
+from .scattering2d.frontend.tensorflow_frontend import (
+    ScatteringTensorFlow2D as Scattering2D,
+)
+from .scattering3d.frontend.tensorflow_frontend import (
+    HarmonicScatteringTensorFlow3D as HarmonicScattering3D,
+)
 
-Scattering1D.__module__ = 'kymatio.tensorflow'
-Scattering1D.__name__ = 'Scattering1D'
+Scattering1D.__module__ = "kymatio.tensorflow"
+Scattering1D.__name__ = "Scattering1D"
 
-Scattering2D.__module__ = 'kymatio.tensorflow'
-Scattering2D.__name__ = 'Scattering2D'
+TimeFrequencyScattering.__module__ = "kymatio.tensorflow"
+TimeFrequencyScattering.__name__ = "TimeFrequencyScattering"
 
-HarmonicScattering3D.__module__ = 'kymatio.tensorflow'
-HarmonicScattering3D.__name__ = 'HarmonicScattering3D'
+Scattering2D.__module__ = "kymatio.tensorflow"
+Scattering2D.__name__ = "Scattering2D"
 
-__all__ = ['Scattering1D', 'Scattering2D', 'HarmonicScattering3D']
+HarmonicScattering3D.__module__ = "kymatio.tensorflow"
+HarmonicScattering3D.__name__ = "HarmonicScattering3D"
+
+__all__ = [
+    "Scattering1D",
+    "TimeFrequencyScattering",
+    "Scattering2D",
+    "HarmonicScattering3D",
+]
