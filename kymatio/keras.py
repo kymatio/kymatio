4a29834235e696d7d020b9fafa3222ec5971246a
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-10-05 10:49:00 +0200

    MAINT export `TimeFrequencyScattering` in all frontends (#967)
    * add TimeFrequencyScattering to torch frontend
    
    * bis
    
    * add TimeFrequencyScattering to TF frontend
    
    * add TimeFrequencyScattering to sklearn
    
    * add TimeFrequencyScattering to numpy
    
    * add TimeFrequencyScattering to keras
    
    * add TimeFrequencyScattering to jax
    
    * import TimeFrequencyScatteringTransformer in sklearn.py
    
    * bis

--- kymatio/keras.py
+++ kymatio/keras.py
@@ -1,12 +1,10 @@
-from .scattering1d.frontend.keras_frontend \
-    import ScatteringKeras1D as Scattering1D
-from .scattering2d.frontend.keras_frontend \
-    import ScatteringKeras2D as Scattering2D
+from .scattering1d.frontend.keras_frontend import ScatteringKeras1D as Scattering1D
+from .scattering2d.frontend.keras_frontend import ScatteringKeras2D as Scattering2D
 
-Scattering1D.__module__ = 'kymatio.keras'
-Scattering1D.__name__ = 'Scattering1D'
+Scattering1D.__module__ = "kymatio.keras"
+Scattering1D.__name__ = "Scattering1D"
 
-Scattering2D.__module__ = 'kymatio.keras'
-Scattering2D.__name__ = 'Scattering2D'
+Scattering2D.__module__ = "kymatio.keras"
+Scattering2D.__name__ = "Scattering2D"
 
-__all__ = ['Scattering1D', 'Scattering2D']
+__all__ = ["Scattering1D", "Scattering2D"]
--- kymatio/numpy.py
+++ kymatio/numpy.py
@@ -1,19 +1,27 @@
 from .scattering1d.frontend.numpy_frontend import ScatteringNumPy1D as Scattering1D
-from .scattering1d.frontend.numpy_frontend import TimeFrequencyScatteringNumPy as TimeFrequencyScattering
+from .scattering1d.frontend.numpy_frontend import (
+    TimeFrequencyScatteringNumPy as TimeFrequencyScattering,
+)
 from .scattering2d.frontend.numpy_frontend import ScatteringNumPy2D as Scattering2D
-from .scattering3d.frontend.numpy_frontend \
-        import HarmonicScatteringNumPy3D as HarmonicScattering3D
+from .scattering3d.frontend.numpy_frontend import (
+    HarmonicScatteringNumPy3D as HarmonicScattering3D,
+)
 
-Scattering1D.__module__ = 'kymatio.numpy'
-Scattering1D.__name__ = 'Scattering1D'
+Scattering1D.__module__ = "kymatio.numpy"
+Scattering1D.__name__ = "Scattering1D"
 
-TimeFrequencyScattering.__module__ = 'kymatio.numpy'
-TimeFrequencyScattering.__name__ = 'TimeFrequencyScattering'
+TimeFrequencyScattering.__module__ = "kymatio.numpy"
+TimeFrequencyScattering.__name__ = "TimeFrequencyScattering"
 
-Scattering2D.__module__ = 'kymatio.numpy'
-Scattering2D.__name__ = 'Scattering2D'
+Scattering2D.__module__ = "kymatio.numpy"
+Scattering2D.__name__ = "Scattering2D"
 
-HarmonicScattering3D.__module__ = 'kymatio.numpy'
-HarmonicScattering3D.__name__ = 'HarmonicScattering3D'
+HarmonicScattering3D.__module__ = "kymatio.numpy"
+HarmonicScattering3D.__name__ = "HarmonicScattering3D"
 
-__all__ = ['Scattering1D', 'TimeFrequencyScattering', 'Scattering2D', 'HarmonicScattering3D']
+__all__ = [
+    "Scattering1D",
+    "TimeFrequencyScattering",
+    "Scattering2D",
+    "HarmonicScattering3D",
+]
