52833385632010af50772461a846f0e4976ee343
Cyrus <cyrusvahidi@gmail.com>
2022-10-31 12:57:15 +0000

    remove jtfs jax export until frontend is implemented

--- kymatio/jax.py
+++ kymatio/jax.py
@@ -1,7 +1,4 @@
 from .scattering1d.frontend.jax_frontend import ScatteringJax1D as Scattering1D
-from .scattering1d.frontend.jax_frontend import (
-    TimeFrequencyScatteringJax as TimeFrequencyScattering,
-)
 from .scattering2d.frontend.jax_frontend import ScatteringJax2D as Scattering2D
 from .scattering3d.frontend.jax_frontend import (
     HarmonicScatteringJax3D as HarmonicScattering3D,
@@ -9,9 +6,6 @@ from .scattering3d.frontend.jax_frontend import (
 
 Scattering1D.__module__ = "kymatio.jax"
 Scattering1D.__name__ = "Scattering1D"
-
-TimeFrequencyScattering.__module__ = "kymatio.jax"
-TimeFrequencyScattering.__name__ = "TimeFrequencyScattering"
 
 Scattering2D.__module__ = "kymatio.jax"
 Scattering2D.__name__ = "Scattering2D"
