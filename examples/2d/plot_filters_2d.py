a554a2a527ce0a235b18be4a1f0e135e8dc5dc70
Joakim Andén <janden@kth.se>
2023-03-13 15:51:11 +0100

    DOC Clean up plot_filters_2d

--- examples/2d/plot_filters_2d.py
+++ examples/2d/plot_filters_2d.py
@@ -1,86 +1,90 @@
 """
 Plot the 2D wavelet filters
 ===========================
-See :meth:`kymatio.scattering2d.filter_bank` for more informations about the used wavelets.
+See :meth:`kymatio.scattering2d.filter_bank` for more information about the wavelets.
 """
 
-from colorsys import hls_to_rgb
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+
 from kymatio.scattering2d.filter_bank import filter_bank
-from scipy.fft import fft2
 
 
 ###############################################################################
 # Initial parameters of the filter bank
 # -------------------------------------
+# Images are of shape `(M, M) = (32, 32)`, maximum wavelet scale is
+# `2 ** J = 2 ** 3 = 8` and number of orientations is `L = 8`.
+
 M = 32
 J = 3
 L = 8
+
+# Calculate the filters (in the frequency domain).
 filters_set = filter_bank(M, M, J, L=L)
 
 ###############################################################################
 # Imshow complex images
 # ---------------------
-# Thanks to https://stackoverflow.com/questions/17044052/mathplotlib-imshow-complex-2d-array
+# Since the wavelets are complex, we need to colorize them to show them
+# properly (see
+# https://stackoverflow.com/questions/17044052/mathplotlib-imshow-complex-2d-array)
 
+from colorsys import hls_to_rgb
 
-def colorize(z):
-    n, m = z.shape
-    c = np.zeros((n, m, 3))
-    c[np.isinf(z)] = (1.0, 1.0, 1.0)
-    c[np.isnan(z)] = (0.5, 0.5, 0.5)
+def colorize(im):
+    # Normalize input to have largest magnitude one.
+    im = im / np.max(np.abs(im))
 
-    idx = ~(np.isinf(z) + np.isnan(z))
-    A = (np.angle(z[idx]) + np.pi) / (2*np.pi)
+    # Use complex phase to determine hue (between zero and one).
+    A = (np.angle(im) + np.pi) / (2 * np.pi)
     A = (A + 0.5) % 1.0
-    B = 1.0/(1.0 + abs(z[idx])**0.3)
-    c[idx] = [hls_to_rgb(a, b, 0.8) for a, b in zip(A, B)]
-    return c
+
+    # Use magnitude to determine lightness.
+    B = 1.0 / (1.0 + np.abs(im) ** 0.3)
+
+    # Put everything together and convert to RGB.
+    A = A.flatten()
+    B = B.flatten()
+
+    im_c = np.array([hls_to_rgb(a, b, 0.8) for a, b in zip(A, B)])
+    im_c = im_c.reshape(im.shape + (3,))
+
+    return im_c
 
 ###############################################################################
 # Bandpass filters
 # ----------------
-# First, we display each wavelet according to its scale and orientation.
-fig, axs = plt.subplots(J, L, sharex=True, sharey=True)
-fig.set_figheight(6)
-fig.set_figwidth(6)
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-i = 0
-for filter in filters_set['psi']:
-    f = filter["levels"][0]
-    filter_c = fft2(f)
-    filter_c = np.fft.fftshift(filter_c)
-    axs[i // L, i % L].imshow(colorize(filter_c))
-    axs[i // L, i % L].axis('off')
-    axs[i // L, i % L].set_title(
-        "$j = {}$ \n $\\theta={}$".format(i // L, i % L))
-    i = i+1
+# Display each wavelet according to its scale and orientation. Color (hue)
+# denotes complex phase, while lightness denotes inverse magnitude.
 
-fig.suptitle((r"Wavelets for each scales $j$ and angles $\theta$ used."
-              "\nColor saturation and color hue respectively denote complex "
-              "magnitude and complex phase."), fontsize=13)
-fig.show()
+fig, axs = plt.subplots(J, L, sharex=True, sharey=True)
+
+for k, psi_f in enumerate(filters_set['psi']):
+    psi_f = psi_f["levels"][0]
+    psi = np.fft.ifft2(psi_f)
+    psi = np.fft.fftshift(psi)
+
+    j = k // L
+    theta = k % L
+
+    axs[j, theta].imshow(colorize(psi))
+    axs[j, theta].axis('off')
+    axs[j, theta].set_title(f"$j = {j}$ \n $\\theta={theta}$")
+
+fig.suptitle("Wavelets for each scale $j$ and angle $\\theta$")
+plt.show()
 
 ###############################################################################
 # Lowpass filter
 # --------------
-# We finally display the low-pass filter.
-plt.figure()
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.axis('off')
-plt.set_cmap('gray_r')
+# Now display the low-pass filter.
 
-f = filters_set['phi']["levels"][0]
+phi_f = filters_set['phi']["levels"][0]
 
-filter_c = fft2(f)
-filter_c = np.fft.fftshift(filter_c)
-plt.suptitle(("The corresponding low-pass filter, also known as scaling "
-              "function.\nColor saturation and color hue respectively denote "
-              "complex magnitude and complex phase"), fontsize=13)
-filter_c = np.abs(filter_c)
-plt.imshow(filter_c)
+phi = np.fft.ifft2(phi_f)
+phi = np.fft.fftshift(phi)
+plt.suptitle("The corresponding low-pass filter (scaling function)")
 
+plt.imshow(np.abs(phi), cmap="gray_r")
 plt.show()
