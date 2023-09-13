180c8f1335d564d5bd604dd1ca8abbe73e802a57
vivianwhite <vivcat980@gmail.com>
2023-09-13 13:37:03 -0400

    MAINT fixed scheduler initialization

--- examples/2d/cifar_small_sample.py
+++ examples/2d/cifar_small_sample.py
@@ -229,7 +229,7 @@ def main():
                                         weight_decay=0.0005)
     M = args.learning_schedule_multi
     drops = [60*M,120*M,160*M]
-    scheduler = torch.optim.MultiStepLR(optimizer, drops, gamma=0.2)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, drops, gamma=0.2)
     for epoch in range(0, 200*M):
         train(model, device, train_loader, optimizer, epoch+1, scattering)
         scheduler.step()
