c11593a79f1193d271ca43e32aa2d1249adce5db
Muawiz Chaudhary <muawizc@gmail.com>
2025-01-23 18:43:02 -0500

    API adding very barebones keras timefreqscatter. This code likely does not run.

--- kymatio/scattering1d/frontend/keras_frontend.py
+++ kymatio/scattering1d/frontend/keras_frontend.py
@@ -1,8 +1,9 @@
 from ...frontend.keras_frontend import ScatteringKeras
-from ...scattering1d.frontend.base_frontend import ScatteringBase1D
+from ...scattering1d.frontend.base_frontend import ScatteringBase1D, TimeFrequencyScatteringBase
 
 from kymatio.tensorflow import Scattering1D as ScatteringTensorFlow1D
 
+from kymatio.tensorflow import TimeFrequencyScattering as TimeFrequencyScatteringTensorFlow
 from tensorflow.python.framework import tensor_shape
 
 
@@ -36,4 +37,63 @@ class ScatteringKeras1D(ScatteringKeras, ScatteringBase1D):
         keys = ["J", "Q", "max_order", "oversampling"]
         return {key: getattr(self, key) for key in keys}
 
+
+class TimeFrequencyScatteringKeras(ScatteringKeras, TimeFrequencyScatteringBase):
+    def __init__(self, 
+        J,
+        J_fr,
+        Q,
+        shape,
+        T=None,
+        stride=None,
+        Q_fr=1,
+        F=None,
+        stride_fr=None,
+        out_type="array",
+        backend="tensorflow",):
+
+        ScatteringKeras.__init__(self)
+
+        self.J=J,
+        self.J_fr=J_fr,
+        self.Q=Q,
+        self.shape=shape,
+        self.T=T,
+        self.stride=stride,
+        self.Q_fr=Q_fr,
+        self.F=F,
+        self.stride_fr=stride_fr,
+        self.out_type=out_type,
+        self.backend=backend,
+
+    def build(self, input_shape):
+        shape = tuple(tensor_shape.TensorShape(input_shape).as_list()[-1:])
+        self.S = TimeFrequencyScatteringTensorFlow(
+        J=self.J,
+        J_fr=self.J_fr,
+        Q=self.Q,
+        shape=self.shape,
+        T=self.T,
+        stride=self.stride,
+        Q_fr=self.Q_fr,
+        F=self.F,
+        stride_fr=self.stride_fr)
+        ScatteringKeras.build(self, input_shape)
+
+    #TODO: how do we implement this without #839 implemented?
+    #right now uses ScatteringKeras1D calculation
+    def compute_output_shape(self, input_shape):
+        input_shape = tensor_shape.TensorShape(input_shape).as_list()
+        nc = self.S.output_size()
+        k0 = max(self.J - self._oversampling, 0)
+        ln = self.S.ind_end[k0] - self.S.ind_start[k0]
+        output_shape = [input_shape[0], nc, ln]
+        return tensor_shape.TensorShape(output_shape)
+
+    def get_config(self):
+        keys = ["J", "J_fr", "Q", "max_order", "Q_fr"]
+        return {key: getattr(self, key) for key in keys}
+
+
 ScatteringKeras1D._document()
+TimeFrequencyScatteringKeras._document()
